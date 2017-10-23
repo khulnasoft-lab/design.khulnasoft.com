3ec7f3a1187f0fa69e8d9a839bf32fe5b3461d53
Simon Knox <psimyn@gmail.com>
2017-10-23 21:23:29 +0300

    add scss loader
    also bootstrap-vue

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, '../')
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  }
+}
