513d610e90053da79109c99e4feac214d634debd
tauriedavis <taurie@gitlab.com>
2017-12-18 10:32:17 -0800

    Implements scss structure. Adds variables for colors, measures, and framework. Adds center mixin. Adds typography and spacing.

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ Vue.component = function(name, definition) {
 Vue.use(BootstrapVue)
 Vue.component = originalVueComponent
 
-import '../assets/stylesheets/main.scss';
+import '../assets/stylesheets/app.scss';
 
 const files = [
   {
@@ -36,29 +36,29 @@ const files = [
   },
 ];
 
-files.forEach(({name, modules}) => {
-  modules.keys().forEach(fileName => {
-    const htmlContent = modules(fileName);
+// files.forEach(({name, modules}) => {
+//   modules.keys().forEach(fileName => {
+//     const htmlContent = modules(fileName);
 
-    const dummyElement = document.createElement('div');
-    dummyElement.innerHTML = htmlContent;
-    document.body.appendChild(dummyElement);
-    const title = dummyElement.querySelector('h1');
+//     const dummyElement = document.createElement('div');
+//     dummyElement.innerHTML = htmlContent;
+//     document.body.appendChild(dummyElement);
+//     const title = dummyElement.querySelector('h1');
 
-    const codeBlocks = dummyElement.querySelectorAll('pre > code');
-    codeBlocks.forEach(block => {
-      block.classList.add('language-html');
-      const preElement = block.parentNode;
-      preElement.insertAdjacentHTML('afterend', '<div>');
-      const el = preElement.nextSibling;
-      const vueComponent = new Vue({
-        el,
-        template: `
-          <div class="component-preview">${block.innerText}</div>
-        `,
-      });
-    });
-    Prism.highlightAll();
-  });
-});
+//     const codeBlocks = dummyElement.querySelectorAll('pre > code');
+//     codeBlocks.forEach(block => {
+//       block.classList.add('language-html');
+//       const preElement = block.parentNode;
+//       preElement.insertAdjacentHTML('afterend', '<div>');
+//       const el = preElement.nextSibling;
+//       const vueComponent = new Vue({
+//         el,
+//         template: `
+//           <div class="component-preview">${block.innerText}</div>
+//         `,
+//       });
+//     });
+//     Prism.highlightAll();
+//   });
+// });
 
