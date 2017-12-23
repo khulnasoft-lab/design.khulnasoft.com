b7aa9836d95b49095a9d1ee8efcaf901e6af0a34
tauriedavis <taurie@gitlab.com>
2017-12-23 12:40:13 -0800

    Add first persona and necessary text styles

--- pages/getting-started/personas.vue
+++ pages/getting-started/personas.vue
@@ -1,3 +1,89 @@
-<template>
-  <h1>Todo: Add personas</h1>
-</template>
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="page">
+      <!-- add sidebar -->
+      <div class="content limited md m-t-7 m-b-7">
+        <h1>Personas</h1>
+        <div class="persona">
+          <h2>Nazim Ramesh</h2>
+
+          <div class="persona__demographics">
+          </div>
+          <div class="persona__motivations">
+            <h2>Motivations</h2>
+            <a href="">test</a>
+            <p>
+              Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wishlist of features, Nazim began comparing source control tools. A search for “self-hosted Git server repository management” returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:
+            </p>
+            <blockquote>
+              <p>
+                “I wanted them to switch away from SVN. I needed a server application to manage repositories. The common tools that were around just didn’t meet the requirements. Most of them were too simple or plain...GitLab provided all the required features. Also costs had to be low, since we don’t have a big budget for those things...the Community Edition was perfect in this regard.”
+              </p>
+            </blockquote>
+            <p>
+              In his role as a full-stack web developer, Nazim could recommend products that he would like the engineering team to use, but final approval lay with his line manager, Mike, VP of Engineering. Nazim recalls that he was met with reluctance from his colleagues when he raised moving to Git and using GitLab.
+            </p>
+            <blockquote>
+              <p>
+                “The biggest challenge...why should we change anything at all from the status quo? We needed to switch from SVN to Git. They knew they needed to learn Git and a Git workflow...using Git was scary to my colleagues...they thought it was more complex than SVN to use.”
+              </p>
+            </blockquote>
+            <p>
+              Undeterred, Nazim decided to migrate a couple of projects across to GitLab.
+            </p>
+            <blockquote>
+              <p>
+                “Old SVN users couldn’t see the benefits of Git at first. It took a month or two to convince them.”
+              </p>
+            </blockquote>
+            <p>
+              Slowly, by showing his colleagues how easy it was to use Git, the majority of the team’s projects were migrated to GitLab.
+            </p>
+            <p>
+              The engineering team have been using GitLab CE for around 2 years now. Nazim credits himself as being entirely responsible for his company’s decision to move to GitLab.
+            </p>
+          </div>
+          <div class="persona__frustrations">
+            <h2>Frustrations</h2>
+            <h3>Adoption to GitLab has been slow</h3>
+            <p>
+              Not only has the engineering team had to get to grips with Git, they’ve also had to adapt to using GitLab. Due to lack of training and existing skills in other tools, the full feature set of GitLab CE is not being utilised. Nazim sold GitLab to his manager as an ‘all in one’ tool which would replace multiple tools used within the company, thus saving costs. Nazim hasn’t had the time to integrate the legacy tools to GitLab and he’s struggling to convince his peers to change their habits.
+            </p>
+            <h3>Missing Features</h3>
+            <p>
+              Nazim’s company want GitLab to be able to do everything. There isn’t a large budget for software, so they’re selective about what tools are implemented. It needs to add real value to the company. In order for GitLab to be widely adopted and to meet the requirements of different roles within the company, it needs a host of features. When an individual within Nazim’s company wants to know if GitLab has a specific feature or does a particular thing, Nazim is the person to ask. He becomes the point of contact to investigate, build or sometimes just raise the feature request. Nazim gets frustrated when GitLab isn’t able to do what he or his colleagues need it to do.
+            </p>
+            <h3>Regressions and bugs</h3>
+            <p>
+              Nazim often has to calm down his colleagues, when a release contains regressions or new bugs. As he puts it “every new version adds something awesome, but breaks something”. He feels that “old issues for "minor" annoyances get quickly buried in the mass of open issues and linger for a very long time. More generally, I have the feeling that GitLab focus on adding new functionalities, but overlook a bunch of annoying minor regressions or introduced bugs.” Due to limited resource and expertise within the team, not only is it difficult to remain up-to-date with the frequent release cycle, it’s also counterproductive to fix workflows every month.
+            </p>
+            <h3>Uses too much RAM and CPU</h3>
+            <p>
+              <blockquote>
+                <p>
+                  “Memory usages mean that if we host it from a cloud based host like AWS, we spend almost as much on the instance as what we would pay GitHub”
+                </p>
+              </blockquote>
+            </p>
+            <h3>UI/UX</h3>
+            <p>
+              GitLab’s interface initially attracted Nazim when he was comparing version control software. He thought it would help his less technical colleagues to adapt to using Git and perhaps, GitLab could be rolled out to other areas of the business, beyond engineering. However, using GitLab’s interface daily has left him frustrated at the lack of personalisation / control over his user experience. He’s also regularly lost in a maze of navigation. Whilst he acknowledges that GitLab listens to its users and that the interface is improving, he becomes annoyed when the changes are too progressive. “Too frequent UI changes. Most of them tend to turn out great after a few cycles of fixes, but the frequency is still far too high for me to feel comfortable to always stay on the current release.”
+            </p>
+          </div>
+          <div class="persona__goals">
+            <h2>Goals</h2>
+            <ul>
+              <li>To convince his colleagues to fully adopt GitLab CE, thus improving workflow and collaboration.</li>
+              <li>To use a feature rich version control platform that covers all stages of the development lifecycle, in order to reduce dependencies on other tools.</li>
+              <li>To use an intuitive and stable product, so he can spend more time on his core job responsibilities and less time bug-fixing, guiding colleagues, etc.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="../bundle.js"></script>
+  </body>
+</html>
