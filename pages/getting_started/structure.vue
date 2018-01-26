cf5ec140cd4eb6eb6b980d49006dda2ccf12af65
tauriedavis <taurie@gitlab.com>
2018-01-26 13:57:49 -0800

    Add type scale mixin and use to calculate ui,md, and issuable md typography

--- pages/getting_started/structure.vue
+++ pages/getting_started/structure.vue
@@ -4,4 +4,3 @@
     <p>Structure relates to the structure of our design system. Documentation regarding how and why it is organized the way it is. This should also include documentation on our atomic design pattern used while building out components.</p>
   </div>
 </template>
-
--- pages/styles/typography.vue
+++ pages/styles/typography.vue
@@ -1,6 +1,73 @@
 <template>
-  <div>
-    <h1>TODO</h1>
+  <div class="content limited">
+    <div class="structure m-t-7 m-b-7">
+      <h1 class="display">UI Typography h1 Display</h1>
+      <h1>UI Typography h1</h1>
+      <h2>Demographics</h2>
+      <h3>Motivations</h3>
+      <h4>Regressions and bugs</h4>
+      <h5>Goals</h5>
+      <h6>Minor Goals</h6>
+      <p><a href="">Link test</a> Nazim works for a software development company</p>
+      <p class="small"><a href="">Link test</a> Nazim works for a software development company</p>
+      <h1>UI Typography h1</h1>
+      <p>Nazim works for a software development company</p>
+      <h2>Demographics</h2>
+      <p>Nazim works for a software development company</p>
+      <h3>Motivations</h3>
+      <p>Nazim works for a software development company</p>
+      <h4>Regressions and bugs</h4>
+      <p>Nazim works for a software development company</p>
+      <h5>Goals</h5>
+      <p>Nazim works for a software development company</p>
+      <h6>Minor Goals</h6>
+      <p>Nazim works for a software development company</p>
+    </div>
+
+    <div class="structure md m-t-7 m-b-7">
+      <h1>Markdown Typography h1</h1>
+      <h2>Demographics</h2>
+      <h3>Motivations</h3>
+      <h4>Regressions and bugs</h4>
+      <h5>Goals</h5>
+      <h6>Minor Goals</h6>
+      <p><a href="">Link test</a> Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <h1>Markdown Typography h1</h1>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <h2>Demographics</h2>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <h3>Motivations</h3>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <h4>Regressions and bugs</h4>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <h5>Goals</h5>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <h6>Minor Goals</h6>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+    </div>
+
+    <div class="structure md issuable m-t-7 m-b-7">
+      <h1>Issuable Markdown Typography h1</h1>
+      <h2>Demographics</h2>
+      <h3>Motivations</h3>
+      <h4>Regressions and bugs</h4>
+      <h5>Goals</h5>
+      <h6>Minor Goals</h6>
+      <p><a href="">Link test</a> Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <h1>Issuable Markdown Typography h1</h1>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <h2>Demographics</h2>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <h3>Motivations</h3>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <h4>Regressions and bugs</h4>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <h5>Goals</h5>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+      <h6>Minor Goals</h6>
+      <p>Nazim works for a software development company which currently hires around 80 people. When Nazim first joined the company, the engineering team were using Subversion (SVN) as their primary form of source control. However, Nazim felt SVN was not flexible enough to work with many feature branches and noticed that developers with less experience of source control struggled with the central-repository nature of SVN. Armed with a wish list of features, Nazim began comparing source control tools. A search for "self-hosted Git server repository management" returned GitLab. In his own words, Nazim explains why he wanted the engineering team to start using GitLab:</p>
+    </div>
   </div>
 </template>
-
