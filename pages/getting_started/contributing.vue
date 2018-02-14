4b753063f4abb0aa652015ed471369b4df57a24b
tauriedavis <taurie@gitlab.com>
2018-02-13 17:13:51 -0800

    Add documentation regarding contributing an issue or merge request

--- pages/getting_started/contributing.vue
+++ pages/getting_started/contributing.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>Todo: Add contributing documentation</h1>
+  <div class="content limited m-t-7 m-b-7">
+    <div class="contributing md">
+      <div class="page-header p-b-5 m-b-6">
+        <h1>Contributing</h1>
+      </div>
+      <p>Thank you for your interest in contributing to the GitLab Design System! We welcome all feedback regarding designs, guidelines, and implementation. The following details how to contribute in an easy and efficient way.</p>
+
+      <h3>Contribute an issue</h3>
+      <p>If you an interested in contributing to our design system, the first step is ensuring that an issue exists. There are many applications that an issue can have, including:</p>
+      <ul>
+        <li><p>Discussing the implementation of a new idea</p></li>
+        <li><p>Submitting feature proposals</p></li>
+        <li><p>Asking questions</p></li>
+        <li><p>Reporting bugs and malfunction</p></li>
+        <li><p>Obtaining support</p></li>
+        <li><p>Elaborating new code implementations</p></li>
+      </ul>
+      <p>If you were unable to find a related issue in our <a href="https://gitlab.com/gitlab-org/design.gitlab.com/issues" target="_blank">issue tracker</a>, begin by <a href="https://gitlab.com/gitlab-org/design.gitlab.com/issues/new" target="_blank">creating a new one</a>. For changes that affect visual designs or user experiences, it can be helpful to include a mockup.</p>
+      <h5>UX proposals</h5>
+      <p>If your proposal involves a new guideline or UX paradigm, ping a <a href="https://about.gitlab.com/team/" target="_blank">UX maintainer</a> of this project to review and provide feedback.</p>
+      <h5>Frontend proposals</h5>
+      <p>If your proposal involves a change to frontend implementation, ping a <a href="https://about.gitlab.com/team/" target="_blank">frontend maintainer</a> of this project to review and provide feedback.</p>
+
+      <h3>Contribute a merge request</h3>
+      <p>The first step towards creating a merge request is ensuring that an issue exists for the changes you want to implement. This opens the conversation and allows feedback to happen early, preventing risks such as duplicated or unnecessary work.</p>
+    </div>
+  </div>
 </template>
