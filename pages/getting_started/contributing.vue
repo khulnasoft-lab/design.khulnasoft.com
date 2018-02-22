de130ae0ef7cdc80fcd70aa0e5244e6df5f539d8
tauriedavis <taurie@gitlab.com>
2018-02-22 13:37:01 -0800

    Write docmentation regarding merge requests

--- pages/getting_started/contributing.vue
+++ pages/getting_started/contributing.vue
@@ -9,21 +9,30 @@
       <h3>Contribute an issue</h3>
       <p>If you an interested in contributing to our design system, the first step is ensuring that an issue exists. There are many applications that an issue can have, including:</p>
       <ul>
-        <li><p>Discussing the implementation of a new idea</p></li>
-        <li><p>Submitting feature proposals</p></li>
-        <li><p>Asking questions</p></li>
-        <li><p>Reporting bugs and malfunction</p></li>
-        <li><p>Obtaining support</p></li>
-        <li><p>Elaborating new code implementations</p></li>
+        <li>Discussing the implementation of a new idea</li>
+        <li>Submitting feature proposals</li>
+        <li>Asking questions</li>
+        <li>Reporting bugs and malfunction</li>
+        <li>Obtaining support</li>
+        <li>Elaborating new code implementations</li>
       </ul>
       <p>If you were unable to find a related issue in our <a href="https://gitlab.com/gitlab-org/design.gitlab.com/issues" target="_blank">issue tracker</a>, begin by <a href="https://gitlab.com/gitlab-org/design.gitlab.com/issues/new" target="_blank">creating a new one</a>. For changes that affect visual designs or user experiences, it can be helpful to include a mockup.</p>
       <h5>UX proposals</h5>
-      <p>If your proposal involves a new guideline or UX paradigm, ping a <a href="https://about.gitlab.com/team/" target="_blank">UX maintainer</a> of this project to review and provide feedback.</p>
+      <p>If your proposal involves a new guideline or UX paradigm, ping a member of the <a href="https://about.gitlab.com/team/" target="_blank">UX team</a> to review and provide feedback.</p>
       <h5>Frontend proposals</h5>
-      <p>If your proposal involves a change to frontend implementation, ping a <a href="https://about.gitlab.com/team/" target="_blank">frontend maintainer</a> of this project to review and provide feedback.</p>
+      <p>If your proposal involves a change to frontend implementation, ping a <a href="https://about.gitlab.com/team/" target="_blank">frontend maintainer</a> to review and provide feedback.</p>
 
       <h3>Contribute a merge request</h3>
-      <p>The first step towards creating a merge request is ensuring that an issue exists for the changes you want to implement. This opens the conversation and allows feedback to happen early, preventing risks such as duplicated or unnecessary work.</p>
+      <p>For changes to documentation or to fix bugs within our Design System, follow these instructions:</p>
+      <ol>
+        <li>Choose an issue to work on. If one does not exist, <a href="https://gitlab.com/gitlab-org/design.gitlab.com/issues/new" target="_blank">create a new one</a>. This opens the conversation and allows feedback to happen early, preventing risks such as duplicated or unnecessary work. It can be helpful to comment in the issue to verify that no one is working on it and that the issue is still relevant.</li>
+        <li>Fork <a href="https://gitlab.com/gitlab-org/design.gitlab.com" target="_blank">this project</a>.</li>
+        <li>Make changes to the feature or fix the bug you’ve chosen to work on.</li>
+        <li>Open a merge request. The earlier you open a merge request, the sooner you can get feedback. You can mark it as a Work in Progress to signal that you’re not done yet.</li>
+        <li>Get your merge request reviewed. If your changes involved a new guideline or UX paradigm, then ping a member of the <a href="https://about.gitlab.com/team/" target="_blank">UX team</a> to review. If your changes involve an update to frontend implementation, ping a <a href="https://about.gitlab.com/team/" target="_blank">frontend maintainer</a> to review. Some merge requests will require both a UX and frontend reviewer.</li>
+        <li>Get your changes merged! 🙌</li>
+      </ol>
+      <p>If you are interested in fixing a bug or contributing to our CSS, contribute an issue and merge request to our <a href="https://gitlab.com/gitlab-org/css-framework" target="_blank">CSS Framework</a></p>
     </div>
   </div>
 </template>
