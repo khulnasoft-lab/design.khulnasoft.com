9d7613a2d5dafea6fd78585d13373de8b0bea252
tauriedavis <taurie@gitlab.com>
2018-03-01 17:17:31 -0800

    Transfer documentation from ux guide related to different regions

---
+++ pages/regions/empty-states.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="content limited m-t-7 m-b-7">
+    <div class="typography md">
+      <div class="page-header p-b-5 m-b-6">
+        <h1>Empty States</h1>
+      </div>
+      <p>Empty states should be considered during the implentation of new features. They are vital to helping onboard new users, making the experience feel more approachable and understandable. Empty states should feel inviting and provide just enough information to get people started. There should be a single call to action and a clear explanation of what to use the feature for.</p>
+      <p class="todo">Add examples of empty states</p>
+    </div>
+  </div>
+</template>
---
+++ pages/regions/navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="content limited m-t-7 m-b-7">
+    <div class="typography md">
+      <div class="page-header p-b-5 m-b-6">
+        <h1>Navigation</h1>
+      </div>
+      <h2>Global navigation</h2>
+      <p>The items in the header include a list of pages that allow you to view global content throughout GitLab. For example, you can view your todos, issues, and merge requests across projects and groups. You can also create new groups and projects, as well as search the application.</p>
+      <p class="todo">Add global navigation example</p>
+      <h2>Contextual navigation</h2>
+      <p>The left-side sidebar consists of content that is contextual to each apge. These options change depending on if you are looking at a project, group, or settings page. There can be up to two levels of navigation. Each sub nav group should be a self-contained group of functionality. For example, everything related to the issue tracker should be under the 'Issue' tab, while everything relating to the wiki will be grouped under the 'Wiki' tab. The names used for each section should be short and easy to remember, ideally 1-2 words in length.</p>
+      <p class="todo">Add contextual navigation example</p>
+    </div>
+  </div>
+</template>
