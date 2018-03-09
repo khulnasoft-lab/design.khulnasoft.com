812f976f12e28ba49b1ebbcaec1a7b14ff5846ab
sarrahvesselov <svesselov@gitlab.com>
2018-03-09 18:13:20 -0500

    fix bad link

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,7 +29,7 @@
           <nuxt-link class="nav-sidebar__section-items-anchor" to="/styles/grid">Grid</nuxt-link>
           <nuxt-link class="nav-sidebar__section-items-anchor" to="/styles/illustration">Illustration</nuxt-link>
           <nuxt-link class="nav-sidebar__section-items-anchor" to="/styles/typography">Typography</nuxt-link>
-          <nuxt-link class="nav-sidebar__section-items-anchor" to="/components/cursor">Cursor</nuxt-link>
+          <nuxt-link class="nav-sidebar__section-items-anchor" to="/styles/cursor">Cursor</nuxt-link>
         </details>
 
         <details class="nav-sidebar__section" :open="$route.fullPath.startsWith('/components/')">
