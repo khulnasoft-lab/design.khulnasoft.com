372feb6ad8e215b8e7552407903059ad58e3a554
sarrahvesselov <svesselov@gitlab.com>
2018-03-14 16:54:17 -0400

    removed styles per Tauries comments in the MR. Updated wording based on her suggestions

--- pages/resources/design-resources.vue
+++ pages/resources/design-resources.vue
@@ -5,7 +5,7 @@
         <h1>Design Resources</h1>
       </div>
       <h2>Design Repository</h2>
-      <p>All design files are stored in the <a href="https://gitlab.com/gitlab-org/gitlab-design">gitlab-design</a> repository and maintained by GitLab UX designers. The repository contains GitLab’s open source design pattern library, prototypes and work-in-progress files.</p>
+      <p>All design files are stored in the <a href="https://gitlab.com/gitlab-org/gitlab-design">gitlab-design</a> repository and maintained by GitLab UX designers. The repository contains GitLab’s open source design pattern library, prototypes, and work-in-progress files.</p>
       <h5>Goals</h5>
       <ul>
         <li>Jumpstart design work by using the design pattern library and previous work</li>
@@ -20,18 +20,11 @@
         <li><a href="https://about.gitlab.com/handbook/ux/">UX Handbook</a></li>
         <li><a href="https://dribbble.com/gitlab">GitLab Dribbble</a></li>
       </ul>
-      <div class="page-header p-b-5 m-t-9 m-b-6">
-        <h2>Brand AI</h2>
-      </div>
-      <p>We are in the process of capturing our UI paradigms on brand.ai, visit our public-facing <a href="https://brand.ai/git-lab/primary-brand">GitLab Brand AI</a>.</p>
-      <div class="page-header p-b-5 m-t-9 m-b-6">
-        <h2>GitLab SVGs</h2>
-      </div>
-      <p>All SVG Assets for GitLab are managed in one repository, see <a href="https://gitlab.com/gitlab-org/gitlab-svgs">https://gitlab.com/gitlab-org/gitlab-svgs</a>. Creates SVG Sprites out of Icons and optimises SVG based Illustrations.</p>
-      <p>To view all the SVGs managed in GitLab, use the <a href="http://gitlab-org.gitlab.io/gitlab-svgs/">GitLab SVG Viewer</a>.</p>
-       <div class="page-header p-b-5 m-t-9 m-b-6">
-        <h2>UX Research Panel</h2>
-      </div>
+      <h2>Brand AI</h2>
+      <p>Brand.ai is a hosted team library used to sync symbols and styles across sketch files. Our UI paradigms are included within brand.ai for easy access when working on designs. Visit our public-facing <a href="https://brand.ai/git-lab/primary-brand">GitLab Brand.ai</a> styleguide.</p>
+      <h2>GitLab SVGs</h2>
+      <p>Our <a href="https://gitlab.com/gitlab-org/gitlab-svgs">GitLab SVG</a> repository manages all SVG assets by creating an SVG sprite out of icons and optimizing SVG based illustrations.</p>
+      <h2>UX Research Panel</h2>
       <p>Here at GitLab, we regularly conduct user research to improve our product. You can help shape the future of GitLab. We want your feedback on how we can continue to improve GitLab. By joining our research panel, you’ll be the first to see new features and your thoughts will drive product improvements for a better user experience.</p>
       <p>To join or learn more about the UX Research Panel, see <a href="https://about.gitlab.com/researchpanel/">GitLab UX Research</a>.</p>
     </div>
