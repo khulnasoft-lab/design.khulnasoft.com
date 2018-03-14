fcdce306f9dc1be608fe91197eafde6b85e704c5
sarrahvesselov <svesselov@gitlab.com>
2018-03-14 17:08:23 -0400

    updated cursor docs per Taurie's comments in MR. changed some names

--- pages/styles/cursors.vue
+++ pages/styles/cursors.vue
@@ -15,27 +15,23 @@
         <tbody>
           <tr>
             <td align="center"><img src="~/assets/images/styles/cursors/cursors-default.png"></td>
-            <td><strong>Default cursor:</strong> cursor shown by default</td>
-          </tr>
-          <tr>
-            <td align="center"><img src="~/assets/images/styles/cursors/cursors-ibeam.png"></td>
-            <td><strong>Pointer cursor:</strong> used to indicate that you can click on an element to invoke a command or navigate, such as links and buttons</td>
-          </tr>
-          <tr>
-            <td align="center"><img src="~/assets/images/styles/cursors/cursors-move.png"></td>
-            <td><strong>Move cursor:</strong> used to indicate that you can move an element around on the screen</td>
-          </tr>
-          <tr>
-            <td align="center"><img src="~/assets/images/styles/cursors/cursors-panclosed.png"></td>
-            <td><strong>Pan cursor (opened):</strong> indicates that you can grab and move the entire canvas, affecting what is seen in the view port</td>
-          </tr>
-          <tr>
-            <td align="center"><img src="~/assets/images/styles/cursors/cursors-panopened.png"></td>
-            <td><strong>Pan cursor (closed):</strong> indicates that you are actively panning the canvas</td>
+            <td><strong>Default cursor:</strong> cursor shown by default.</td>
           </tr>
           <tr>
             <td align="center"><img src="~/assets/images/styles/cursors/cursors-pointer.png"></td>
-            <td><strong>I-beam cursor:</strong> indicates that this is either text that you can select and copy, or a text field that you can click into to enter text</td>
+            <td><strong>Pointer cursor:</strong> used to indicate that you can click on an element to invoke a command or navigate, such as links and buttons.</td>
+          </tr>
+          <tr>
+            <td align="center"><img src="~/assets/images/styles/cursors/cursors-panopened.png"></td>
+            <td><strong>Grab:</strong> indicates that you can grab and move an element around the screen.</td>
+          </tr>
+          <tr>
+            <td align="center"><img src="~/assets/images/styles/cursors/cursors-panclosed.png"></td>
+            <td><strong>Grabbing:</strong> indicates that you are actively moving an element around the screen.</td>
+          </tr>
+          <tr>
+            <td align="center"><img src="~/assets/images/styles/cursors/cursors-ibeam.png"></td>
+            <td><strong>I-beam cursor:</strong> indicates that this is either text that you can select and copy, or a text field that you can click into to enter text.</td>
           </tr>
         </tbody>
       </table>
