ec76a788f011fc043576c9e68a2d037a555b1755
sarrahvesselov <svesselov@gitlab.com>
2018-03-15 16:28:33 -0400

    added exception documentation for tooltip max-width

--- pages/components/tooltips.vue
+++ pages/components/tooltips.vue
@@ -14,6 +14,7 @@
       <p>Tooltips shouldn’t repeat information that is shown near the referring element. However, they can show the same data in a different format (e.g. date or timestamps). Information provided in a tooltip should be short and concise. If you need to add more than a single line of information, consider using a <a href="/components/popovers">popover</a> instead. The text will wrap when the content is wider than the max-width.</p>
       <h2>Dimensions</h2>
       <p>The dimensions of a tooltip are dependent on its contents with a max-width of 200px.</p>
+      <p>One exception to this max-width exists on the Groups page. The full namespace in the Groups tree is hidden and shows only the Group name by default. The showing full namespace is shown as a tooltip on hover. This tooltip contains a max-width of 350px.</p>
       <h2>Layout</h2>
       <p>Tooltips contain 8px padding on both the top and bottom and 12px padding on both the left and right.</p>
       <h2>Placement</h2>
