aefc1bccfa9d917620755f6a166afd2a15d3c1ad
sarrahvesselov <svesselov@gitlab.com>
2018-03-17 21:10:08 -0400

    adding skeleton loader docs

--- pages/components/skeleton-loader.vue
+++ pages/components/skeleton-loader.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <h1>TODO</h1>
+  <div class="content limited m-t-7 m-b-7">
+    <div class="component md">
+      <div class="page-header p-b-5 m-b-6">
+        <h1>Skeleton Loader</h1>
+      </div>
+      <p>A skeleton loader is used to let users know what kind of content is currently loading. The content can be loaded independently and asynchronously, while still adhering to the structure and look of the complete view.</p>
+      <h2>Usage</h2>
+      <p>A skeleton loader can replace any existing UI elements for the period in which they are being loaded and should aim for maintaining a similar structure visually.</p>
+      <p>A skeleton should represent an organism recognizably.</p>
+      <p>Atom elements within organisms (for reference see this article on atomic design methodology) may be represented in a maximum of 3 repetitions, if applicable.</p>
+      <p>Skeletons should only be presented in grayscale using the HEX colors: #fafafa or #ffffff (except for shadows).</p>
+      <p>Animate the grey atoms in a pulsating way to show motion, as if "loading." The pulse animation transitions color horizontally from left to right, starting with #f2f2f2 to #fafafa.</p>
+      <h2>Demo</h2>
+      <p class="todo">Todo: Add live component block with code example</p>
+      <h2>Related patterns</h2>
+      <ul>
+        <li><a href="/">A related pattern</a></li>
+      </ul>
+    </div>
   </div>
 </template>
