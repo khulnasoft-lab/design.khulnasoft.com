012340c0b65aeca20251c391a25b27233f817444
sarrahvesselov <svesselov@gitlab.com>
2018-03-21 17:19:26 -0400

    groups to subgroups

--- pages/components/breadcrumbs.vue
+++ pages/components/breadcrumbs.vue
@@ -49,7 +49,7 @@
       <ul>
         <li>Use an ellipsis button to hide groups when there are three or more subgroups in the path.</li>
         <li>Use an ellipsis button to hide all Wiki page breadcrumb items except the current page.</li>
-        <li>The top-level group and the group closest to the project are never collapsed.</li>
+        <li>The top-level group and the subgroup closest to the project are never collapsed.</li>
         <li>All breadcrumb items except the last one are truncated to 128px if there isn't enough horizontal space to fit them on one line.</li>
       </ul>
       <h5>Special breadcrumbs</h5>
