fb999ef5518bf6c5710e7b3c6795b4df8abc13cb
sarrahvesselov <svesselov@gitlab.com>
2018-03-21 16:56:53 -0400

    added code block, paragraph, and demo block

--- pages/components/avatar.vue
+++ pages/components/avatar.vue
@@ -10,8 +10,12 @@
       <p>It is preferable that an avatar display an image. Images are personalized and make good use of the ability of the human brain to recognize faces. Project and group avatars contain a fallback in the case an image is not provided. This fallback will display up to two letters based on the name field of the project or group. Personal avatars have a <a href="https://gravatar.com">Gravatar</a> fallback which contains either a configured image or a randomly generated image.</p>
       <h5>Displaying multiple avatars</h5>
       <p>Some features require the display of multiple avatars, side by side. If there is enough space, they should be displayed in a grid pattern. In tighter places, avatars may overlap one another.</p>
+      <p class="todo">Todo: Add an example of multiple avatars using live components</p>
+      <h2>Demo</h2>
+      <p class="todo">Todo: Add live component block with code example</p>
       <h2>Design specifications</h2>
-      <p>Color, spacing, dimension, and layout specific information pertaining to this component can be viewed using the following link: <a href="https://gitlab-org.gitlab.io/gitlab-design/hosted/design-gitlab-specs/avatar-spec-previews/">Sketch Measure Preview for avatars</a></p>
+      <p>Color, spacing, dimension, and layout specific information pertaining to this component can be viewed using the following link:</p>
+      <p><a href="https://gitlab-org.gitlab.io/gitlab-design/hosted/design-gitlab-specs/avatar-spec-previews/">Sketch Measure Preview for avatars</a></p>
     </div>
   </div>
 </template>
