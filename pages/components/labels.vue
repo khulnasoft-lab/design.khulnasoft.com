08cbe888e69baf34b4640bfd1fc6a16881d6ea25
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-03-28 00:19:04 +0200

    initial version for label documentation

--- pages/components/labels.vue
+++ pages/components/labels.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <h1>TODO</h1>
+  <div class="content limited m-t-7 m-b-7">
+    <div class="component md">
+      <h1>Labels</h1>
+      <p>Labels categorise UI objects by visually labeling them. They provide the user a quick way to recognise to which category or categoies the current object belongs. Clicking on labels navigates the user towards a filtered list view based on that label.</p>
+      <h2>Usage</h2>
+      <p>Labels are used to categorise first class objects like issues, merge requests and epics. They can be used as a way to filter against in list views. Upon hover they present a description of the category they represent. They are easy to spot by being fully colored which is either contrasted by white or black text. </p>
+      <h3>User role labels</h3>
+      <p>User role labels are different from normal labels in that they can only indicate one category, the permission level of the user. Visually they are less obvious by being grayscale and only having a border.</p>
+      <h2>Demo</h2>
+      <p class="todo">Todo: Add live component block with code example</p>
+      <h2>Design specifications</h2>
+      <p>Color, spacing, dimension, and layout specific information pertaining to this component can be viewed using the following link:</p>
+      <p><a href="https://gitlab-org.gitlab.io/gitlab-design/hosted/design-gitlab-specs/labels-spec-previews/">Sketch Measure Preview for labels</a></p>
+      <h2>Related patterns</h2>
+      <ul>
+        <li><a href="/lists">Lists</a></li>
+        <li><a href="../regions/filters">Filters</a></li>
+        <li><a href="../regions/search">Search</a></li>
+      </ul>
+    </div>
   </div>
 </template>
