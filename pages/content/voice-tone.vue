a15fa5f721984035c07cb456091bb57b8f6d3cc5
sarrahvesselov <svesselov@gitlab.com>
2018-03-27 09:27:08 -0400

    removed sentence from voice and tone. Moved another sentence up for clarity

--- pages/content/voice-tone.vue
+++ pages/content/voice-tone.vue
@@ -2,8 +2,7 @@
   <div class="content limited m-t-7 m-b-7">
     <div class="component md">
       <h1>Voice and tone</h1>
-      <p>The copy for GitLab is clear and direct. We strike a clear balance between professional and friendly. We can empathize with users (such as celebrating completing all Todos) and remain respectful of the importance of the work. We are a trusted and friendly coworker, helpful and understanding.</p>
-      <p>The copy and messaging is a core part of the experience of GitLab and the conversation with our users. Follow the below conventions throughout GitLab.</p>
+      <p>The copy and messaging is a core part of the experience of GitLab and the conversation with our users. The copy for GitLab is clear and direct. We strike a clear balance between professional and friendly. We can empathize with users (such as celebrating completing all Todos) and remain respectful of the importance of the work. We are a trusted and friendly coworker, helpful and understanding.</p>
       <p>Portions of this page are inspired by work found in the <a href="https://material.io/guidelines/">Material Design guidelines.</a></p>
       <h2>Brevity</h2>
       <p>Users will skim content, rather than read the text carefully. When familiar with a web app, users rely on muscle memory and may read even less when moving quickly. A good experience should quickly orient a user, regardless of their experience, to the purpose of the current screen. Understanding should happen without the user having to read long strings of text consciously.</p>
