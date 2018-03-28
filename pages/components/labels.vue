a3fb395b66d8101a49d9d4a37a35757c70c1268b
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-03-28 11:59:13 +0200

    labels documentation second revision, rewording changes, changed related patterns

--- pages/components/labels.vue
+++ pages/components/labels.vue
@@ -4,9 +4,9 @@
       <h1>Labels</h1>
       <p>Labels categorise UI objects by visually labeling them. They provide the user a quick way to recognise to which category or categoies the current object belongs. Clicking on labels navigates the user towards a filtered list view based on that label.</p>
       <h2>Usage</h2>
-      <p>Labels are used to categorise first class objects like issues, merge requests and epics. They can be used as a way to filter against in list views. Upon hover they present a description of the category they represent. They are easy to spot by being fully colored which is either contrasted by white or black text. </p>
+      <p>Labels are used to categorize issues, merge requests and epics. They can be used to filter in list views. Upon hover, a tooltip containing a description of the category they represent will appear.</p>
       <h3>User role labels</h3>
-      <p>User role labels are different from normal labels in that they can only indicate one category, the permission level of the user. Visually they are less obvious by being grayscale and only having a border.</p>
+      <p>User role labels are different from standard labels. They indicate the permission level of the user and differ visually.  User labels lack coloration with grayscale text and border.</p>
       <h2>Demo</h2>
       <p class="todo">Todo: Add live component block with code example</p>
       <h2>Design specifications</h2>
@@ -14,9 +14,7 @@
       <p><a href="https://gitlab-org.gitlab.io/gitlab-design/hosted/design-gitlab-specs/labels-spec-previews/">Sketch Measure Preview for labels</a></p>
       <h2>Related patterns</h2>
       <ul>
-        <li><a href="/lists">Lists</a></li>
-        <li><a href="../regions/filters">Filters</a></li>
-        <li><a href="../regions/search">Search</a></li>
+        <li><a href="/tags">Tags</a></li>
       </ul>
     </div>
   </div>
