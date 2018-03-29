99435792adb74031dc78f60992def9a3b2267d53
tauriedavis <taurie@gitlab.com>
2018-03-29 07:54:10 -0700

    Move outlier section under geo progression

--- pages/layout/measures.vue
+++ pages/layout/measures.vue
@@ -5,7 +5,7 @@
       <p>All GitLab components follow an 8px measure system. We’ve defined this as our memorable base number to build upon in order to establish spatial values that are utilized by every component. By following a set spatial convention, we decrease design complexity while increasing consistency across the application.</p>
       <h2>Geometric progression</h2>
       <p>We use 8px as the base for determining our spacial system. In order to create a scale that offers flexibility in spacing without providing too many choices, we’ve defined a set of values using geometric progression rather than a fixed increment.</p>
-      <h2>12px outlier</h2>
+      <h3>12px outlier</h3>
       <p>We include 12px as one of our measures but it should only be reserved for right and left padding of horizontal tabs, buttons, and inputs. This special padding amount allows for better alignment of stacked items while giving elements enough room to breathe. 12px should not be using for any other measurement other than those defined here.</p>
       <h2>Demo</h2>
       <div class="measures m-b-6">
