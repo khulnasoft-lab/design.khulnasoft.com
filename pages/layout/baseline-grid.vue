8587cc98abf94cb714dbe411b714bf414c0bb18f
tauriedavis <taurie@gitlab.com>
2018-03-28 21:38:46 -0700

    Use 4px baseline grid for typography and align type styles to pl

--- pages/layout/baseline-grid.vue
+++ pages/layout/baseline-grid.vue
@@ -2,9 +2,9 @@
   <div class="content limited m-t-7 m-b-7">
     <div class="component md">
       <h1>Baseline grid</h1>
-      <p>Adhering to a baseline grid allows us to be more efficient by removing decision making while maintaining a consistent rhythm between components. Using a baseline grid of 8px allows flexibility when scaling and defining our <a href="/layout/measures">measures</a> without overloading the system with options.</p>
+      <p>Adhering to a baseline grid allows us to be more efficient by removing decision making while maintaining a consistent rhythm between components and typography.
       <h2>Components</h2>
-      <p>By using multiples of 8 to define dimensions, padding, and margins of components, we ensure every UI element aligns to the pixel grid.</p>
+      <p>Using a baseline grid of 8px allows flexibility when scaling and defining our <a href="/layout/measures">measures</a> without overloading the system with options. By using multiples of 8 to define dimensions, padding, and margins of components, we ensure every UI element aligns to the pixel grid.</p>
       <div class="baseline a-center m-b-6">
         <div class="w-13 h-3"></div>
         <div class="w-13 h-5 c-blue-background-300"></div>
@@ -27,9 +27,9 @@
         <div class="w-13 h-3"></div>
       </div>
       <h2>Typography</h2>
-      <p>All typography aligns on our baseline grid. See our <a href="/styles/typography">typography styles</a> for more information about how to use our type scale.</p>
+      <p>All typography aligns to a 4px baseline grid. This allows for readible line heights at all sizes. See our <a href="/styles/typography">typography styles</a> for more information about how to use our type scale.</p>
       <h3>UI typography</h3>
-      <div class="baseline distributed">
+      <div class="baseline--typography distributed">
         <h1>H1</h1>
         <h2>h2</h2>
         <h3>h3</h3>
@@ -40,7 +40,7 @@
         <span class="label">Label</span>
       </div>
       <h3>Markdown</h3>
-      <div class="baseline">
+      <div class="baseline--typography">
         <h1>H1</h1>
         <h2>h2</h2>
         <h3>h3</h3>
@@ -50,7 +50,7 @@
         <p>Paragraph</p>
       </div>
       <h3>Compact markdown</h3>
-      <div class="baseline md compact">
+      <div class="baseline--typography md compact">
         <h1>H1</h1>
         <h2>h2</h2>
         <h3>h3</h3>
