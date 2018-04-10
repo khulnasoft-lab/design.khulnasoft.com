51abd64164f7a378138c636926bbe0dc5bf72e37
Winnie Hellmann <winnie@gitlab.com>
2018-04-10 13:12:12 +0000

    Explicitly use design.gitlab.com as base URL in production

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   router: {
-    base: process.env.NODE_ENV === 'production' ? '/design.gitlab.com/' : '/'
+    base: process.env.NODE_ENV === 'production' ? '//design.gitlab.com/' : '/'
   },
   generate: {
     dir: 'public'
