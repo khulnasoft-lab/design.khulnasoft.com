6bcaf0bb249f92bb8baa2aa7189287f92b38b1c0
Andreas Kämmerle <andreas.kaemmerle@gmail.com>
2018-06-16 01:09:54 +0200

    Add spacing measure diagram

--- pages/layout/spacing.vue
+++ pages/layout/spacing.vue
@@ -5,6 +5,7 @@
       <p>All GitLab components follow an 8px spacing system. We’ve defined this as our memorable base number to build upon in order to establish spatial values that are utilized by every component. By following a set spatial convention, we decrease design complexity while increasing consistency across the application.</p>
       <h2>Geometric progression</h2>
       <p>We use 8px as the base for determining our spacial system. In order to create a scale that offers flexibility in spacing without providing too many choices, we’ve defined a set of values using geometric progression rather than a fixed increment.</p>
+      <img src="~/assets/images/components/spacing.png" height="500"/>
       <h3>12px outlier</h3>
       <p>We include 12px as one of our measures but it should only be reserved for right and left padding of horizontal tabs, buttons, and inputs. This special padding amount allows for better alignment of stacked items while giving elements enough room to breathe. 12px should not be using for any other measurement other than those defined here.</p>
       <h2>Demo</h2>
