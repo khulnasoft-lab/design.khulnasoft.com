bb91bdde6a2acd21f80f2ad37009a96e3cefe2e9
Andreas Kämmerle <andreas.kaemmerle@gmail.com>
2018-06-16 18:17:02 +0200

    Fix color background classes in spacing demo

--- pages/layout/spacing.vue
+++ pages/layout/spacing.vue
@@ -10,19 +10,19 @@
       <p>We include 12px as one of our measures but it should only be reserved for right and left padding of horizontal tabs, buttons, and inputs. This special padding amount allows for better alignment of stacked items while giving elements enough room to breathe. 12px should not be using for any other measurement other than those defined here.</p>
       <h2>Demo</h2>
       <div class="spacing-demo m-b-6">
-        <span class="w-1 h-1 c-blue-background-300 d-inline-block"></span>
-        <span class="w-2 h-2 c-blue-background-300 d-inline-block"></span>
-        <span class="w-3 h-3 c-blue-background-300 d-inline-block"></span>
-        <span class="w-4 h-4 c-blue-background-300 d-inline-block"></span>
-        <span class="w-5 h-5 c-blue-background-300 d-inline-block"></span>
-        <span class="w-6 h-6 c-blue-background-300 d-inline-block"></span>
-        <span class="w-7 h-7 c-blue-background-300 d-inline-block"></span>
-        <span class="w-8 h-8 c-blue-background-300 d-inline-block"></span>
-        <span class="w-9 h-9 c-blue-background-300 d-inline-block"></span>
-        <span class="w-10 h-10 c-blue-background-300 d-inline-block"></span>
-        <span class="w-11 h-11 c-blue-background-300 d-inline-block"></span>
-        <span class="w-12 h-12 c-blue-background-300 d-inline-block"></span>
-        <span class="w-13 h-13 c-blue-background-300 d-inline-block"></span>
+        <span class="w-1 h-1 c-background-blue-300 d-inline-block"></span>
+        <span class="w-2 h-2 c-background-blue-300 d-inline-block"></span>
+        <span class="w-3 h-3 c-background-blue-300 d-inline-block"></span>
+        <span class="w-4 h-4 c-background-blue-300 d-inline-block"></span>
+        <span class="w-5 h-5 c-background-blue-300 d-inline-block"></span>
+        <span class="w-6 h-6 c-background-blue-300 d-inline-block"></span>
+        <span class="w-7 h-7 c-background-blue-300 d-inline-block"></span>
+        <span class="w-8 h-8 c-background-blue-300 d-inline-block"></span>
+        <span class="w-9 h-9 c-background-blue-300 d-inline-block"></span>
+        <span class="w-10 h-10 c-background-blue-300 d-inline-block"></span>
+        <span class="w-11 h-11 c-background-blue-300 d-inline-block"></span>
+        <span class="w-12 h-12 c-background-blue-300 d-inline-block"></span>
+        <span class="w-13 h-13 c-background-blue-300 d-inline-block"></span>
       </div>
       <p class="todo">Todo: Add spacing class examples</p>
     </div>
