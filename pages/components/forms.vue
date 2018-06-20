c14000f3bfd7c0c5ce2e6658e46002420980d341
matejlatin <mlatin@gitlab.com>
2018-06-20 13:40:26 +0100

    Wrote down form guidelines
    Wrote down form guidelines based on gitlab-org/gitlab-design#154.

--- pages/components/forms.vue
+++ pages/components/forms.vue
@@ -1,5 +1,65 @@
 <template>
-  <div>
-    <h1>TODO</h1>
+  <div class="content limited m-t-7 m-b-7">
+    <div class="component md">
+      <h1>Forms</h1>
+      <p>Forms are a fundamental but a complex part of interacting with web applications such as GitLab. Getting them right and assisting the users to successfully complete their tasks is essential.</p>
+      <h2>Usage</h2>
+      <h3>Layout</h3>
+      <p>Form elements must move in single direction so they must only be distributed through one column when the progression of the form is vertical and through one row when the progression is horizontal. Horizontally stacking input fields in a vertically moving form is only acceptable when they’re related (e.g. Name and Surname).</p>
+      <p class="todo">Todo: Add live component block with code example (Vertical form with stacked input fields)</p>
+      <p class="todo">Todo: Add live component block with code example (Horizontal form)</p>
+      <h3>Labels</h3>
+      <p>Input field and text area labels are set in bold and positioned above the form element by default. Labels must use the <strong>for</strong> attribute for accessibility reasons. Labels for <a href="#checkbox">checkboxes and radios</a> are set in regular font weight.</p>
+      <p>In rare cases and exceptions, the label can be placed to the left of the input field. Examples of such exceptions are single input fields (e.g. “Sort by” dropdowns) and horizontally distributed forms.</p>
+      <p class="todo">Todo: Add live component block with code example (sort by dropdown)</p>
+      <p class="todo">Todo: Add live component block with code example (horizontally distributed form)</p>
+      <h3>Input fields</h3>
+      <p>Correct HTML5 input field types must be used to improve the user experience of filling in a form, especially the following ones: email, url, tel, number, date, datetime, color.</p>
+      <p class="todo">Todo: Add live component block with code example (input field types)</p>
+      <p>Disabled input fields need an explanation on why they’re disabled and that comes as Help text below.</p>
+      <p class="todo">Todo: Add live component block with code example (disabled input)</p>
+      <p>The width of an input field should reflect the expected content, for example postcode should be smaller than address.</p>
+      <p class="todo">Todo: Add live component block with code example (inputs of different widths)</p>
+      <h4>Predefined widths</h4>
+      <p>To encourage the usage of different input field widths, we use a set of predefined widths based on the 8 pixels grid. It’s ok to use a width that isn’t on this list but it should be based on the 8 pixels grid.</p>
+      <p class="todo">Todo: Add live component block with code example (predefined widths)</p>
+      <h3>Text area</h3>
+      <p>Should be used when expected content is long. Predefined widths from the <strong>Input fields</strong> section apply to text area as well.</p>
+      <p class="todo">Todo: Add live component block with code example (text area)</p>
+      <h3>Date picker</h3>
+      <p>The date picker comes in two parts: the input field and the date picker dropdown. The input field must use a placeholder text to indicate the expected format of time to be entered (e.g. “YYYY-MM-DD”) and a calendar icon on the right edge to indicate that clicking on it will open a datepicker dropdown.</p>
+      <p>The user should be able to input the date by either typing it in or choosing a day from the datepicker dropdown. The user must never be forced to use only one of the two input methods.</p>
+      <p class="todo">Todo: Add live component block with code example (datepicker: input field + dropdown)</p>
+      <h3>Placeholders</h3>
+      <p>Placeholders are not a replacement for labels. Their usage should be limited so, if possible, try to avoid using them in form design. When they are used, they should be:</p>
+      <ul>
+        <li><strong>Contextual</strong>: Either an example or a further explanation of the label, not a generic “Enter” or a repetition of the label.</li>
+        <li><strong>Helpful</strong>: If a placeholder doesn’t add value and make things easier for the user, it shouldn’t be used.</li>
+        <li><strong>Short</strong>: If a placeholder can’t be short, it should be turned into a Help text below the form element.</li>
+      </ul>
+      <p>If the placeholder isn’t all of the above, it shouldn’t be used.</p>
+      <p>If a placeholder is an example, it must start with “e.g.”.</p>
+      <p class="todo">Todo: Add live component block with code example (placeholder)</p>
+      <p>If a placeholder can’t be shortened but an explanation is needed, a Help text below the input should be used. An easy guideline to find out if the explanatory text is too long for a placeholder: if it resembles a sentence, use Help text below the input, if it’s just a few words (ideally up to two or three), consider using a placeholder.</p>
+      <p class="todo">Todo: Add live component block with code example (help text)</p>
+      <h3 id="checkbox">Checkboxes and radios</h3>
+      <p>Checkbox and radio labels are set in regular font weight and should be as short as possible. If more, explanatory text is required, it should be added and styled as Help text below the label.</p>
+      <p class="todo">Todo: Add live component block with code example (checkbox & radio: active & disabled)</p>
+      <p class="todo">Todo: Add live component block with code example (checkbox & radio with help text below labels)</p>
+      <h3>Validation</h3>
+      <h4>Error messages and states</h4>
+      <p>Error messages and validation in general can happen either inline or on form submission. Inline validation should be seen as assisting the user to input the correct information and not as a way of preventing them from submitting the form with possibly incorrect information (inline validation might be frontend-only but the user has no way of knowing that).</p>
+      <p>The error message shows up below the input field in error state and should be helpful and contextual, not generic.</p>
+      <p class="todo">Todo: Add live component block with code example (error state + message)</p>
+      <h4>Form buttons</h4>
+      <p>We default to active buttons in forms and only disable them once clicked to avoid the possibility of submitting the form twice.</p>
+      <p class="todo">Todo: Add live component block with code example (form button)</p>
+      <h4>Required information</h4>
+      <p>We default to all information being optional unless stated otherwise (with “(Required)” added to the label of the corresponding input field).</p>
+      <p class="todo">Todo: Add live component block with code example (required)</p>
+      <h2>Design specification</h2>
+      <p>Color, spacing, dimension, and layout specific information pertaining to this component can be viewed using the following link:</p>
+      <a href="">Sketch Measure Preview for forms</a>
+    </div>
   </div>
 </template>
