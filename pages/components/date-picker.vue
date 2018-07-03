5436b2f93d85bbfac60aef96d2d92450d9436be9
matejlatin <mlatin@gitlab.com>
2018-07-03 13:40:37 +0100

    Minor changes to the date picker page

--- pages/components/date-picker.vue
+++ pages/components/date-picker.vue
@@ -2,9 +2,9 @@
   <div class="content limited m-t-7 m-b-7">
     <div class="component md">
       <h1>Date picker</h1>
-      <p>Date picker allows users to choose and input a date through a calendar-like dropdown or by manually typing the date into the input field.</p>
+      <p>Date picker allows users to choose and input a date through by manually typing the date into the input field or by using a calendar-like dropdown.</p>
       <h2>Usage</h2>
-      <p>The date picker comes in two parts: the input field and the date picker dropdown. The input field must use a placeholder text to indicate the expected format of time to be entered (e.g. “YYYY-MM-DD”) and a calendar icon on the right edge to indicate that clicking on it will open a datepicker dropdown.</p>
+      <p>The date picker comes in two parts: the input field and the date picker dropdown. The input field must use a placeholder text to indicate the expected format of time to be entered (e.g. "YYYY-MM-DD") and a calendar icon on the right edge to indicate that clicking on it will open a datepicker dropdown.</p>
       <p>The user should be able to input the date by either typing it in or choosing a day from the datepicker dropdown. The user must never be forced to use only one of the two input methods.</p>
       <p class="todo">Todo: Add live component block with code example (datepicker: input field + dropdown)</p>
       <h2>Design specification</h2>
