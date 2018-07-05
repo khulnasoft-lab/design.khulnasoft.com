f9f7f2e5f02bfaa0dfb255f884943fa6aba1ff4f
Lucas Charles <me@lucascharles.me>
2018-07-05 11:55:29 -0700

    Add basic meta tags
     - Enforce UTF-8 charset
     - Set default viewport width to device-width

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   generate: {
     dir: 'public'
+  },
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial scale=1' }
+    ]
   }
 }
 
