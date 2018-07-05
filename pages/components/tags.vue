98af14b955545ef1fe2c033846de5e76c080ed6b
Dimitrie Hoekstra <dimitrie@gitlab.com>
2018-07-05 22:44:30 +0000

    Add tag documentation and specs

--- pages/components/tags.vue
+++ pages/components/tags.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <h1>TODO</h1>
+  <div class="content limited m-t-7 m-b-7">
+    <div class="component md">
+      <h1>Tags</h1>
+      <p>Tags make it possible to show non-quantitative object metadata that are either a system result or used by the system itself. They provide a quick way to identify which object meets certain criteria. There is no click action on a tag as it purely represents meta data itself.</p>
+      <h2>Usage</h2>
+      <p>Tags are used when objects need to show non-quantitative object metadata that are either a system result or used by the system itself. Usage should make sense in terms of context. This means that placement can, for example, be near an ID of an object in order to strengthen the connection that this tag belongs to that object.</p>
+      <p>Don't use this pattern to represent status, quantitative object metadata, or non-quantitative object metadata only meant to be used by humans.</p>
+      <h2>Demo</h2>
+      <p class="todo">Todo: Add live component block with code example</p>
+      <h2>Design specifications</h2>
+      <p>Color, spacing, dimension, and layout specific information pertaining to this component can be viewed using the following link:</p>
+      <p><a href="https://gitlab-org.gitlab.io/gitlab-design/hosted/design-gitlab-specs/tags-spec-previews/">Sketch Measure Preview for tags</a></p>
+      <h2>Related patterns</h2>
+      <ul>
+        <li><a href="/badges">Badges</a></li>
+      </ul>
+    </div>
   </div>
 </template>
