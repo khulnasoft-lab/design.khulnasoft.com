516b290d1ea37c98d46056e129f4968d91c763d4
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-07-11 13:07:47 +0200

    copy improvements

--- pages/components/tags.vue
+++ pages/components/tags.vue
@@ -2,10 +2,10 @@
   <div class="content limited m-t-7 m-b-7">
     <div class="component md">
       <h1>Tags</h1>
-      <p>Tags make it possible to show non-quantitative object metadata that are either a system result or used by the system itself. They provide a quick way to identify which object meets certain criteria. There is no click action on a tag as it purely represents meta data itself.</p>
+      <p>Tags make it possible to show qualitative metadata that are either a system result or used by the system itself. They provide a quick way to identify which object meets certain criteria. There is no click action on a tag as it purely represents metadata itself.</p>
       <h2>Usage</h2>
-      <p>Tags are used when objects need to show non-quantitative object metadata that are either a system result or used by the system itself. Usage should make sense in terms of context. This means that placement can, for example, be near an ID of an object in order to strengthen the connection that this tag belongs to that object.</p>
-      <p>Don't use this pattern to represent status, quantitative object metadata, or non-quantitative object metadata only meant to be used by humans.</p>
+      <p>Tags represent system information and are placed near the object that it belongs to in order to strengthen their connection.</p>
+      <p>Don't use this component to represent status, quantitative object metadata, or qualitative metadata metadata only meant to be used by humans which is where labels are for.</p>
       <h2>Demo</h2>
       <p class="todo">Todo: Add live component block with code example</p>
       <h2>Design specifications</h2>
