08ec51a40c959e72650fd3f4e4949f89103aac58
Pedro Moreira da Silva <hi@pedroms.com>
2018-08-01 00:24:32 +0100

    Move Cursors content into Interactions

--- pages/styles/interactions.vue
+++ pages/styles/interactions.vue
@@ -12,7 +12,7 @@
         <li><strong>Non-draggable</strong> items look like a plain list.</li>
       </ul>
 
-      <p>Use <a href="/styles/cursors">system cursors</a> to indicate when an element is draggable. The <code>grab</code> cursor appears on hover when an element is draggable. Once it’s dragged, the cursor will change to <code>grabbing</code>. For areas where an element cannot be dropped, use the <code>not-allowed</code> cursor.
+      <p>Use <a href="#cursors">system cursors</a> to indicate when an element is draggable. The <code>grab</code> cursor appears on hover when an element is draggable. Once it’s dragged, the cursor will change to <code>grabbing</code>. For areas where an element cannot be dropped, use the <code>not-allowed</code> cursor.
 
       <h3>Usage</h3>
       <p>This interaction model is used to transform the position of elements. Usually, these are items in collections with other similar items. When moving items across collections, the collections should be similar in content.</p>
@@ -30,6 +30,50 @@
         <li><a href="/components/data-tables">Data tables</a></li>
         <li><a href="/components/lists">Lists</a></li>
       </ul>
+
+      <h2 id="cursors">Cursors</h2>
+      <p>The mouse cursor is key in helping users understand how to interact with elements on the screen.</p>
+      <table>
+        <thead>
+          <tr>
+            <th>CSS value</th>
+            <th>Example</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><code>default</code></td>
+            <td align="center"><img src="~/assets/images/styles/cursors/cursors-default.svg"></td>
+            <td>The platform-dependent default cursor. Typically an arrow.</td>
+          </tr>
+          <tr>
+            <td><code>pointer</code></td>
+            <td align="center"><img src="~/assets/images/styles/cursors/cursors-pointer.svg"></td>
+            <td>Indicates that you can click on an element to invoke a command or navigate, such as links and buttons.</td>
+          </tr>
+          <tr>
+            <td><code>grab</code></td>
+            <td align="center"><img src="~/assets/images/styles/cursors/cursors-grab.svg"></td>
+            <td>Indicates that you can grab and move an element around the screen.</td>
+          </tr>
+          <tr>
+            <td><code>grabbing</code></td>
+            <td align="center"><img src="~/assets/images/styles/cursors/cursors-grabbing.svg"></td>
+            <td>Indicates that you are actively moving an element around the screen.</td>
+          </tr>
+          <tr>
+            <td><code>text</code></td>
+            <td align="center"><img src="~/assets/images/styles/cursors/cursors-text.svg"></td>
+            <td>Indicates that this is either text that you can select and copy, or a text field that you can click into to enter text.</td>
+          </tr>
+          <tr>
+            <td><code>not-allowed</code></td>
+            <td align="center"><img src="~/assets/images/styles/cursors/cursors-notallowed.svg"></td>
+            <td>Indicates that an item may not be dropped at the current location or that the requested action will not be carried out.</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
