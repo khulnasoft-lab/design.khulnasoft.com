7400d82c11f7241a42a914d959cd6d738e44fbca
tauriedavis <taurie@gitlab.com>
2018-08-15 13:05:51 -0700

    Add more documentation regarding geometric progression and augmented flexbility

--- pages/layout/spacing.vue
+++ pages/layout/spacing.vue
@@ -4,7 +4,7 @@
       <h1>Spacing</h1>
       <p>All GitLab components follow an 8px spacing system. We’ve defined this as our memorable base number to build upon in order to establish spatial values that are utilized by every component. By following a set spatial convention, we decrease design complexity while increasing consistency across the application.</p>
       <h2>Geometric progression</h2>
-      <p>We use 8px as the base for determining our spacial system. In order to create a scale that offers flexibility in spacing without providing too many choices, we’ve defined a set of values using geometric progression rather than a fixed increment.</p>
+      <p>We use 8px as the base for determining our spacial system. In order to create a scale that offers flexibility in spacing without providing an abundance of choices, we’ve defined a set of values using geometric progression: <code>8 * 2^n</code>. We have augmented the progression in order to provide the flexibility that a complex application needs in order to accomodate a large amount of content. To do this, we’ve added values after 16px using <code>n * 1.5</code>, where <code>n</code> is the previous value from the geometric progression.</p>
       <h3>12px outlier</h3>
       <p>We include 12px as one of our measures but it should only be reserved for right and left padding of horizontal tabs, buttons, and inputs. This special padding amount allows for better alignment of stacked items while giving elements enough room to breathe. 12px should not be using for any other measurement other than those defined here.</p>
       <h2>Demo</h2>
