5d7126459ebbe06cb807d638289b8524aff61222
sarrahvesselov <svesselov@gitlab.com>
2018-08-16 19:35:27 -0400

    add just the toggle.vue file from Hazel's branch

--- pages/components/toggles.vue
+++ pages/components/toggles.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>
-    <h1>TODO</h1>
-  </div>
+	<div class="content limited m-t-7 m-b-7">
+		<div class="component md">
+			<h1>Toggle</h1>
+			<p>Toggles are used to provide users the options in an intuitive way when there are only two to five options, and there is enough horizontal space.</p>
+			<!-- Usage -->
+			<h2>Usage</h2>
+			<h3>Label</h3>
+			<p>The label position should follow the guidelines for input fields in [forms](http://localhost:2018/components/forms).
+			<h3>Sizing</h3>
+			<p>The options should have equal prominence and be of the same size, for example, three options - 33% each.</p>	
+			<p class="todo">Todo: Add the example of toggle with the equal width.</p>
+			<!-- Dos and Dont's -->
+			<h3>Dos and Dont's</h3>
+			<p class="todo">Todo: Add the example of dos and dont's.</p>
+			<table class="do-dont m-b-6">
+				<thead>
+				  <tr>
+				    <th>Do</th>
+				    <th>Don't</th>
+				  </tr>
+				</thead>
+				<tbody>
+				  <tr>
+				    <td>Use the shortest labels for options as possible. Try complementing them with a general, all-encompassing label above.</td>
+				    <td>Use long labels for the options. </td>
+				  </tr>
+				  <tr>
+				    <td></td>
+				    <td>Don't use toggles as a replacement for a group of checkboxes displayed as a group of buttons. Toggles are a better version of radio selection so only one of the options can be active. </td>
+				  </tr>
+				  <tr>
+				    <td></td>
+				    <td>Don't use toggles for long lists of options. We recommend using this pattern for up to five options. Use a dropdown if you need more. </td>
+				  </tr>
+				</tbody>
+			</table>
+		</div>
+	 </div>
 </template>
