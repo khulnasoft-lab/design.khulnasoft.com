733c808412aa93af842306e3482db53287bfe3ec
Hazel <hazel@gitlab.com>
2018-08-21 13:35:26 +0800

    Rewrite the documentation

--- pages/components/toggles.vue
+++ pages/components/toggles.vue
@@ -2,14 +2,10 @@
 	<div class="content limited m-t-7 m-b-7">
 		<div class="component md">
 			<h1>Toggle</h1>
-			<p>Toggles are used to provide users the options in an intuitive way when there are only two to five options, and there is enough horizontal space.</p>
+			<p>A toggle is a digital visual switch includes two mutually exclusive states - either On or Off.</p>
 			<!-- Usage -->
 			<h2>Usage</h2>
-			<h3>Label</h3>
-			<p>The label position should follow the guidelines for input fields in [forms](http://localhost:2018/components/forms).
-			<h3>Sizing</h3>
-			<p>The options should have equal prominence and be of the same size, for example, three options - 33% each.</p>	
-			<p class="todo">Todo: Add the example of toggle with the equal width.</p>
+			<p>Using toggles when there is only an available option and users can make an action between two selections. The result of the toggles should be delivered immediately.</p>
 			<!-- Dos and Dont's -->
 			<h3>Dos and Dont's</h3>
 			<p class="todo">Todo: Add the example of dos and dont's.</p>
@@ -22,16 +18,16 @@
 				</thead>
 				<tbody>
 				  <tr>
-				    <td>Use the shortest labels for options as possible. Try complementing them with a general, all-encompassing label above.</td>
-				    <td>Use long labels for the options. </td>
+				    <td>When a user toggles switch, the response should take effect immediately.</td>
+				    <td></td>
 				  </tr>
 				  <tr>
-				    <td></td>
-				    <td>Don't use toggles as a replacement for a group of checkboxes displayed as a group of buttons. Toggles are a better version of radio selection so only one of the options can be active. </td>
+				    <td>The labels should be concise, direct, and short. Keep the label scannable.</td>
+				    <td>Asking questions and adding a description for the value of a switch.</td>
 				  </tr>
 				  <tr>
-				    <td></td>
-				    <td>Don't use toggles for long lists of options. We recommend using this pattern for up to five options. Use a dropdown if you need more. </td>
+				    <td>Toggles should use high-contrast colors to indicate the states - On and Off. </td>
+				    <td>Applying the low-constrast colors for the states.</td>
 				  </tr>
 				</tbody>
 			</table>
