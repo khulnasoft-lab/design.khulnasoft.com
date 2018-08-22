126d5eb1e3bbea50c889e7f03a4f5d682bed53cc
Hazel <hazel@gitlab.com>
2018-08-22 14:16:32 +0800

    Add more description

--- pages/components/toggles.vue
+++ pages/components/toggles.vue
@@ -6,6 +6,15 @@
 			<!-- Usage -->
 			<h2>Usage</h2>
 			<p>Use toggles when there is only one available option and users can make an choice between two selections. The result of the toggle should be delivered immediately.</p>
+			<h3>Checkboxes and radio buttons</h3>
+			<ul>
+			 <li><p>Use checkboxes when the user is allowed to select one or more items from a list. Use radio buttons when the user is allowed to make only one choice from a list and needs to see all available options.</p></li>
+			 <li><p>Use radio buttons when the user is allowed to make only one choice from a list and needs to see all available options. </p></li>
+			</ul>
+			<h3>Visual Design</h3>
+			<p>For on state, the track should present in `$blue-500`, and the thumb should present in `$white` with checkmark icon. For off state, the track should present in `$grey-600`, and the thumb should present in `$white` with close icon.</p>
+			<h3>Position</h3>
+			<p>The toggle should be inline with the label and at the right side.</p>
 			<!-- Dos and Dont's -->
 			<h3>Dos and Dont's</h3>
 			<p class="todo">Todo: Add the example of dos and dont's.</p>
@@ -19,15 +28,15 @@
 				<tbody>
 				  <tr>
 				    <td>When a user toggles switch, the response should take effect immediately.</td>
-				    <td></td>
+				    <td>Ask users to click Save or Submit to apply the new state.</td>
 				  </tr>
 				  <tr>
-				    <td>The labels should be concise, direct, and short. Keep the label scannable.</td>
+				    <td>The labels should be concise, direct, and short. Keep the label scannable. Also, it should describe the result when the option is on.</td>
 				    <td>Asking questions and adding a description for the value of a switch.</td>
 				  </tr>
 				  <tr>
 				    <td>Toggles should use high-contrast colors to indicate the states - On and Off. </td>
-				    <td>Applying the low-constrast colors for the states.</td>
+				    <td>Applying the similar colors for the states.</td>
 				  </tr>
 				</tbody>
 			</table>
