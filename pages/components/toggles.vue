ebf7921e4be3828b869d64b090d2b2334db3ee9c
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-09-19 14:29:21 +0200

    add related patterns

--- pages/components/toggles.vue
+++ pages/components/toggles.vue
@@ -79,6 +79,8 @@
 			<h2>Related patterns</h2>
 			<ul>
 				<li><a href="/components/segmented-control">Segmented control</a></li>
+        <li><a href="/components/checkboxes">checkboxes</a></li>
+        <li><a href="/components/radio-buttons">Radio buttons</a></li>
 			</ul>
     </div>
    </div>
