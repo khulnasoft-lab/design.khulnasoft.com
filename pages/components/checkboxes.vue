844eb04bb14580662f4688ff696e179d22483fc4
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-09-19 14:28:15 +0200

    finish documentation on checkboxes

--- pages/components/checkboxes.vue
+++ pages/components/checkboxes.vue
@@ -3,18 +3,12 @@
     <div class="component md">
       <h1>Checkboxes</h1>
       <p>A checkbox is an input which communicates that an option is true, false or indeterminate - one is active by default.</p>
+      <p>The last state represents a situation where a list of sub-options is grouped under a parent option and sub-options are in both selected and unselected states.</p>
       <!-- Usage -->
       <h2>Usage</h2>
-
-
-            single
-
-      Multiple
-
       <p>Use a single checkbox when there is a choice between two selections, it has a default state, the results are <strong>not</strong> effective or noticable immediately, and there is a need for a user to click a submit button.</p>
       <p>Use multiple checkboxes to allow users to select multiple from a list of options, including all or none.</p>
-			<p>Toggles may replace a secgmented control, two radio buttons, or a single checkbox to allow users to choose between two opposing states. In order to help choosing the right solution follow the table below.</p>
-
+			<p>Checkboxes may replace toggles, segmented control, or radio buttons to allow users to choose between two opposing states. In order to help choosing the right solution follow the table below.</p>
 			<table>
 				<thead>
 					<tr>
@@ -31,56 +25,53 @@
 						<th>How many states are available?</th>
 						<td>Multiple</td>
 						<td>Multiple</td>
-						<td>Multiple</td>
-						<td>2 (on/off)</td>
+						<td class="highlighted">Multiple</td>
+						<td class="highlighted">2 (on/off)</td>
 						<td>2 (on/off)</td>
 					</tr>
 					<tr>
 						<th>How many selections can the user make?</th>
 						<td>1</td>
 						<td>1</td>
-						<td>0 – all</td>
-						<td>1</td>
+						<td class="highlighted">0 – all</td>
+						<td class="highlighted">1</td>
 						<td>1</td>
 					</tr>
 					<tr>
 						<th>Is there a default state?</th>
 						<td>Yes</td>
 						<td>Yes</td>
-						<td>No</td>
-						<td>Yes</td>
+						<td class="highlighted">No</td>
+						<td class="highlighted">Yes</td>
 						<td>Yes</td>
 					</tr>
 					<tr>
 						<th>How would you describe the choices?</th>
 						<td>Mutually exclusive</td>
 						<td>Mutually exclusive</td>
-						<td>Independent of each other</td>
-						<td>Mutually exclusive</td>
+						<td class="highlighted">Independent of each other</td>
+						<td class="highlighted">Mutually exclusive</td>
 						<td>Mutually exclusive</td>
 					</tr>
 					<tr>
 						<th>When does the selection take effect?</th>
 						<td>After a user clicks a submit button</td>
 						<td>Immediately</td>
-						<td>After a user clicks a submit button</td>
-						<td>After a user clicks a submit button</td>
+						<td class="highlighted">After a user clicks a submit button</td>
+						<td class="highlighted">After a user clicks a submit button</td>
 						<td>Immediately</td>
 					</tr>
 				</tbody>
 			</table>
-
 			<h3>Labels</h3>
-				<p>Toggle labels are set in bold, positioned above the element by default, and should describe what the control will do when the switch is on in a concise, direct, and short way.</p>
+				<p><p>Checkbox labels are set in regular font weight, positioned right from the element, and should be as short as possible. If more, explanatory text is required, it should be added and styled as <a href="/components/forms#help-text">Help text</a> below the label.</p>
 			<h3>Visual Design</h3>
-			<p>Toggles should use high-contrast colors to indicate the states - On and Off.</p>
-
+			<p>Checkboxes should use aside from high-contrast colors to indicate the states - true or false, a checkmark and hyphen icon to differentiate between true or indeterminate .</p>
 			<h2>Demo</h2>
       <p class="todo">Todo: Add live component block with code example</p>
-
 			<h2>Design specifications</h2>
       <p>Color, spacing, dimension, and layout specific information pertaining to this component can be viewed using the following link:</p>
-      <p><a href="https://gitlab-org.gitlab.io/gitlab-design/hosted/design-gitlab-specs/toggles-spec-previews/">Sketch Measure Preview for toggles</a></p>
+      <p><a href="https://gitlab-org.gitlab.io/gitlab-design/hosted/design-gitlab-specs/checkboxes-spec-previews/">Sketch Measure Preview for checkboxes</a></p>
 
 			<h2>Related patterns</h2>
 			<ul>
