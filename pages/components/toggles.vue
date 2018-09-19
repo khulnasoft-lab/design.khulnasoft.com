448338543f1b5eb4917035712b8d2af99a19e99d
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-09-19 14:52:48 +0200

    remove unnecessary indentions and new lines

--- pages/components/toggles.vue
+++ pages/components/toggles.vue
@@ -3,12 +3,10 @@
     <div class="component md">
       <h1>Toggles</h1>
       <p>A toggle is a digital visual switch which includes two mutually exclusive states - either On or Off - one is active by default.</p>
-      <!-- Usage -->
       <h2>Usage</h2>
       <p>Use toggles when there is a choice between two selections, it has a default state, the results are effective and noticable immediately, and there is <strong>no need</strong> for a user to click a submit button.</p>
 			<p>Avoid using toggles in for example <a href="/components/forms">forms</a> where a submit button is needed in order to confirm the user's input. This would confuse the user and dillute the expected experience of producing instant results for any toggle throughout the interface.</p>
 			<p>Toggles may replace a segmented control, two radio buttons, or a single checkbox to allow users to choose between two opposing states. In order to help choosing the right solution follow the table below.</p>
-
 			<table>
 				<thead>
 					<tr>
@@ -63,19 +61,15 @@
 					</tr>
 				</tbody>
 			</table>
-
 			<h3>Labels</h3>
-				<p>Toggle labels are set in bold, positioned above the element by default, and should describe what the control will do when the switch is on in a concise, direct, and short way.</p>
+			<p>Toggle labels are set in bold, positioned above the element by default, and should describe what the control will do when the switch is on in a concise, direct, and short way.</p>
 			<h3>Visual Design</h3>
 			<p>Toggles should use high-contrast colors to indicate the states - On and Off.</p>
-
 			<h2>Demo</h2>
       <p class="todo">Todo: Add live component block with code example</p>
-
 			<h2>Design specifications</h2>
       <p>Color, spacing, dimension, and layout specific information pertaining to this component can be viewed using the following link:</p>
       <p><a href="https://gitlab-org.gitlab.io/gitlab-design/hosted/design-gitlab-specs/toggles-spec-previews/">Sketch Measure Preview for toggles</a></p>
-
 			<h2>Related patterns</h2>
 			<ul>
 				<li><a href="/components/segmented-control">Segmented control</a></li>
