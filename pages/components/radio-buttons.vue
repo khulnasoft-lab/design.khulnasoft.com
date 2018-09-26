677470581e1727584811fc03a93da41cdd5177fb
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-09-26 23:57:39 +0200

    convert tabs to spaces

--- pages/components/radio-buttons.vue
+++ pages/components/radio-buttons.vue
@@ -7,79 +7,79 @@
       <!-- Usage -->
       <h2>Usage</h2>
       <p>Radio buttons can never be used alone and must always be used in group form. Use a group of radio buttons when there is a choice between two or multiple selections, it has a default state, the results are <strong>not</strong> effective or noticeable immediately, and there is the need for a user to click a submit button.</p>
-			<p>Radio buttons may replace a segmented control, a list of checkboxes, or a toggle to allow users to choose between two or multiple options. For help with choosing the right solution, follow the table below.</p>
-			<table>
-				<thead>
-					<tr>
-						<th>&nbsp;</th>
-						<th>Radio buttons</th>
-						<th>Segmented control</th>
-						<th>Checkboxes</th>
-						<th>Single checkbox</th>
-						<th>Toggle switches</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th>How many states are available?</th>
-						<td class="highlighted">Multiple</td>
-						<td>Multiple</td>
-						<td>Multiple</td>
-						<td>2 (on/off)</td>
-						<td>2 (on/off)</td>
-					</tr>
-					<tr>
-						<th>How many selections can the user make?</th>
-						<td class="highlighted">1</td>
-						<td>1</td>
-						<td>0 – all</td>
-						<td>1</td>
-						<td>1</td>
-					</tr>
-					<tr>
-						<th>Is there a default state?</th>
-						<td class="highlighted">Yes</td>
-						<td>Yes</td>
-						<td>No</td>
-						<td>Yes</td>
-						<td>Yes</td>
-					</tr>
-					<tr>
-						<th>How would you describe the choices?</th>
-						<td class="highlighted">Mutually exclusive</td>
-						<td>Mutually exclusive</td>
-						<td>Independent of each other</td>
-						<td>Mutually exclusive</td>
-						<td>Mutually exclusive</td>
-					</tr>
-					<tr>
-						<th>When does the selection take effect?</th>
-						<td class="highlighted">After a user clicks a submit button</td>
-						<td>Immediately</td>
-						<td>After a user clicks a submit button</td>
-						<td>After a user clicks a submit button</td>
-						<td>Immediately</td>
-					</tr>
-				</tbody>
-			</table>
-			<h3>Labels</h3>
-			<p>Radio button labels are set in regular font weight, positioned to the right of the element, and should be as short as possible. If more, explanatory text is required, it should be added and styled as <a href="/components/forms#help-text">Help text</a> below the label.</p>
-			<h3>Visual Design</h3>
-			<p>Radio buttons should use high-contrast colors and a dot icon to indicate the states - selected and unselected.</p>
+      <p>Radio buttons may replace a segmented control, a list of checkboxes, or a toggle to allow users to choose between two or multiple options. For help with choosing the right solution, follow the table below.</p>
+      <table>
+        <thead>
+          <tr>
+            <th>&nbsp;</th>
+            <th>Radio buttons</th>
+            <th>Segmented control</th>
+            <th>Checkboxes</th>
+            <th>Single checkbox</th>
+            <th>Toggle switches</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>How many states are available?</th>
+            <td class="highlighted">Multiple</td>
+            <td>Multiple</td>
+            <td>Multiple</td>
+            <td>2 (on/off)</td>
+            <td>2 (on/off)</td>
+          </tr>
+          <tr>
+            <th>How many selections can the user make?</th>
+            <td class="highlighted">1</td>
+            <td>1</td>
+            <td>0 – all</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <th>Is there a default state?</th>
+            <td class="highlighted">Yes</td>
+            <td>Yes</td>
+            <td>No</td>
+            <td>Yes</td>
+            <td>Yes</td>
+          </tr>
+          <tr>
+            <th>How would you describe the choices?</th>
+            <td class="highlighted">Mutually exclusive</td>
+            <td>Mutually exclusive</td>
+            <td>Independent of each other</td>
+            <td>Mutually exclusive</td>
+            <td>Mutually exclusive</td>
+          </tr>
+          <tr>
+            <th>When does the selection take effect?</th>
+            <td class="highlighted">After a user clicks a submit button</td>
+            <td>Immediately</td>
+            <td>After a user clicks a submit button</td>
+            <td>After a user clicks a submit button</td>
+            <td>Immediately</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3>Labels</h3>
+      <p>Radio button labels are set in regular font weight, positioned to the right of the element, and should be as short as possible. If more, explanatory text is required, it should be added and styled as <a href="/components/forms#help-text">Help text</a> below the label.</p>
+      <h3>Visual Design</h3>
+      <p>Radio buttons should use high-contrast colors and a dot icon to indicate the states - selected and unselected.</p>
 
-			<h2>Demo</h2>
+      <h2>Demo</h2>
       <p class="todo">Todo: Add live component block with code example</p>
 
-			<h2>Design specifications</h2>
+      <h2>Design specifications</h2>
       <p>Color, spacing, dimension, and layout specific information pertaining to this component can be viewed using the following link:</p>
       <p><a href="https://gitlab-org.gitlab.io/gitlab-design/hosted/design-gitlab-specs/radiobuttons-spec-previews/">Sketch Measure Preview for radio buttons</a></p>
 
-			<h2>Related patterns</h2>
-			<ul>
-				<li><a href="/components/segmented-control">Segmented control</a></li>
+      <h2>Related patterns</h2>
+      <ul>
+        <li><a href="/components/segmented-control">Segmented control</a></li>
         <li><a href="/components/checkboxes">Checkboxes</a></li>
         <li><a href="/components/toggles">Toggles</a></li>
-			</ul>
+      </ul>
     </div>
    </div>
 </template>
