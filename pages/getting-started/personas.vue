f14e0ba17971a879dce8ba9807f2fcb77b5c5535
Emily von Hoffmann ✨ <evhoffmann@gitlab.com>
2018-09-28 17:47:59 +0000

    Add new personas to personas.vue

--- pages/getting-started/personas.vue
+++ pages/getting-started/personas.vue
@@ -315,7 +315,7 @@
         </ul>
       </div>
     </div>
-    <div class="persona card">
+    <div class="persona card m-b-8">
       <div class="card--header">
         <h2>Delaney</h2>
       </div>
@@ -345,6 +345,111 @@
           <li>It can be difficult for my team to do thorough code reviews in a reasonable timeframe, while still making progress on their own assignments.</li>
           <li>When demand surpasses our current capacity, it can be stressful to resolve issues while not creating new ones that result from hasty work.</li>
           <li>I am not always aware of the best way to explain technical limitations to stakeholders who are not involved in the development process.</li>
+        </ul>
+     </div>
+    </div>
+    <div class="persona card m-b-8">
+      <div class="card--header">
+        <h2>Devon</h2>
+      </div>
+         <div class="card--body md">
+        <table class="m-b-6">
+          <tbody>
+            <tr>
+              <th class="header">Job Title:</th>
+              <td>DevOps Engineer</td>
+            </tr>
+            <tr>
+              <th class="header">Alternative Job Titles:</th>
+              <td>Application Developer, Operations Engineer, Systems Engineer, IT Consultant</td>
+            </tr>
+          </tbody>
+        </table>
+        <h3>Job Summary</h3>
+        <p>I provide support for our infrastructure, environments, and integrations. I split my time between coding to implement features and bug fixes, and helping developers deploy, build, and release as efficiently as possible. </p>
+        <h3>Motivations</h3>
+        <ul>
+          <li>When developers plan a project that requires my support, I want to be notified, so I can avoid unnecessary reactive work.</li>
+          <li>When I resolve problems, I want to be able to track impact and other important success metrics, so I can raise my profile in the organization.</li>
+          <li>When I create a solution for developers, I want to see it being used, so I know that my contributions are reliable and valued.</li>
+          <li>When I’m facing a new or novel challenge, I want to whiteboard it with a teammate, so I have the satisfaction of helping solve a problem that has no manual.</li>
+          <li>When I’m advocating for a new process or tool, I want to point to a metric or test, so I can support my case with something objective.</li>
+        </ul>
+        <h3>Frustrations</h3>
+        <ul>
+          <li>It is hard for me to quantify my efforts and convince others of time that will be saved in the future by investing time in proactive tasks in the present.</li>
+          <li>I'm frustrated by the avoidable crises, caused by a lack of communication, that derail my work and burn up my resources.</li>
+          <li>I dislike frequent context-switching and being responsible for some tasks that I feel I am not good at, because of the many hats my job requires me to wear.</li>
+          <li>I'm frustrated by the politics of convincing people to adopt my recommendations.</li>
+        </ul>
+     </div>
+    </div>
+    <div class="persona card m-b-8">
+      <div class="card--header">
+        <h2>Sasha</h2>
+      </div>
+         <div class="card--body md">
+        <table class="m-b-6">
+          <tbody>
+            <tr>
+              <th class="header">Job Title:</th>
+              <td>Software Developer</td>
+            </tr>
+            <tr>
+              <th class="header">Alternative Job Titles:</th>
+              <td>Digital Solutions Developer, Consultant, Database Developer, Mobile Developer</td>
+            </tr>
+          </tbody>
+        </table>
+        <h3>Job Summary</h3>
+        <p>I spend the majority of my time focused on completing planned development tasks, with roughly 30-40% of time taken by meetings, planning for the next sprint, and fixing bugs or customer requests as they arise. I work off of JIRA tickets and have a regular stand-up with my team.</p>
+        <h3>Motivations</h3>
+        <ul>
+          <li>When I’m planning work, I want to have better communication between stakeholders, so I can deliver something they really need and use.</li>
+          <li>When I’m on-call, I want to be the expert on some part of the system, so I know that I’m a valuable part of the team.</li>
+          <li>When collaborating with a large number of developers, I want to see a record of everyone’s changes, so we can pinpoint and unwind mistakes.</li>
+          <li>When I’m pairing with my teammates, I want to learn new tools and skills, so I can keep growing in my career. </li>
+        </ul>
+        <h3>Frustrations</h3>
+        <ul>
+          <li>I’m frustrated when requirements change after work has already begun on a project.</li>
+          <li>I’m frustrated when work is inaccurately scoped, because it causes stress and eats into time planned for other work.</li>
+          <li>I’m frustrated when I come across brittle code and something that should be an easy fix requires a lot of rework.</li>
+          <li>I’m concerned that by taking longer than expected on a task I may be judged or seen as blocking others’ work.</li>
+        </ul>
+     </div>
+    </div>
+    <div class="persona card m-b-8">
+      <div class="card--header">
+        <h2>Sidney</h2>
+      </div>
+         <div class="card--body md">
+        <table class="m-b-6">
+          <tbody>
+            <tr>
+              <th class="header">Job Title:</th>
+              <td>Systems Administrator</td>
+            </tr>
+            <tr>
+              <th class="header">Alternative Job Titles:</th>
+              <td>Systems Engineer, Database Administrator, Infrastructure Engineer, Site Availability Engineer, Site Reliability Engineer</td>
+            </tr>
+          </tbody>
+        </table>
+        <h3>Job Summary</h3>
+        <p>I maintain and scale our infrastructure and configurations, and my priority is to automate as much as possible. When needed, I also build servers and help developers deploy to them.</p>
+        <h3>Motivations</h3>
+        <ul>
+          <li>When I get asked for help multiple times on the same task, I want to automate that task, to save time and avoid mistakes in the future.</li>
+          <li>When I’m on-call, I want to receive tiered notifications, so that the true emergencies don’t get lost in the noise.</li>
+          <li>When I’m releasing an improvement, I want no one to notice, so I know that it has gone smoothly.</li>
+        </ul>
+        <h3>Frustrations</h3>
+        <ul>
+          <li>I'm frustrated by the large volume of reactive work that I face.</li>
+          <li>I'm frustrated by the number of channels (email, Skype, SMS, Slack, pager) on which I receive on-call notifications.</li>
+          <li>I'm frustrated when I get inundated by requests from people who have not followed the correct process.</li>
+          <li>I'm frustrated when developers do not implement my recommendations, and I’m responsible for fixing their preventable problems anyway.</li>
         </ul>
      </div>
     </div>
