af7a905aa7527126e3ffd3b9aa9dbe22fe511ff0
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-10-01 18:54:47 +0200

    replaced table with todo item

--- pages/components/toggles.vue
+++ pages/components/toggles.vue
@@ -7,60 +7,7 @@
       <p>Use toggles when there is a choice between two selections, it has a default state, the results are effective and noticeable immediately, and there is <strong>no need</strong> for a user to click a submit button.</p>
       <p> Avoid using toggles when a submit button is needed. This confuses users and dilutes the experience by preventing instant results.</p>
       <p>Toggles may replace a segmented control, two radio buttons, or a single checkbox to allow users to choose between two opposing states. For help with choosing the right solution, follow the table below.</p>
-      <table>
-        <thead>
-          <tr>
-            <th>&nbsp;</th>
-            <th>Radio buttons</th>
-            <th>Segmented control</th>
-            <th>Checkboxes</th>
-            <th>Single checkbox</th>
-            <th>Toggle switches</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>How many states are available?</th>
-            <td>Multiple</td>
-            <td>Multiple</td>
-            <td>Multiple</td>
-            <td>2 (on/off)</td>
-            <td class="highlighted">2 (on/off)</td>
-          </tr>
-          <tr>
-            <th>How many selections can the user make?</th>
-            <td>1</td>
-            <td>1</td>
-            <td>0 – all</td>
-            <td>1</td>
-            <td class="highlighted">1</td>
-          </tr>
-          <tr>
-            <th>Is there a default state?</th>
-            <td>Yes</td>
-            <td>Yes</td>
-            <td>No</td>
-            <td>Yes</td>
-            <td class="highlighted">Yes</td>
-          </tr>
-          <tr>
-            <th>How would you describe the choices?</th>
-            <td>Mutually exclusive</td>
-            <td>Mutually exclusive</td>
-            <td>Independent of each other</td>
-            <td>Mutually exclusive</td>
-            <td class="highlighted">Mutually exclusive</td>
-          </tr>
-          <tr>
-            <th>When does the selection take effect?</th>
-            <td>After a user clicks a submit button</td>
-            <td>Immediately</td>
-            <td>After a user clicks a submit button</td>
-            <td>After a user clicks a submit button</td>
-            <td class="highlighted">Immediately</td>
-          </tr>
-        </tbody>
-      </table>
+      <p class="todo">Todo: Add replacement-comparison-table</p>
       <h3>Labels</h3>
       <p>Toggle labels are set in bold, positioned above the element by default, and should describe what the control will do when the switch is on in a concise, direct, and short way.</p>
       <p>In rare cases and exceptions, the label can be placed to the left of the input field. Examples include non inline positioned elements.</p>
