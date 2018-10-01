fd98baaf946384cef2dfe879a196d89c170d16f7
Clement Ho <ClemMakesApps@gmail.com>
2018-10-01 16:40:05 -0500

    Test doc integration with gitlab-ui

--- pages/components/progress-bars.vue
+++ pages/components/progress-bars.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
     <h1>TODO</h1>
+    <h1>Component documentation</h1>
+    <style>
+      iframe.documentation {
+        width: 100%;
+        height: 100vh;
+        border: none;
+      }
+  </style>
+    <iframe
+      class="documentation"
+      scrolling="no"
+      src="https://gitlab-org.gitlab.io/gitlab-ui/iframe.html?selectedKind=progress-bar&selectedStory=default&full=0&addons=1&stories=1&panelRight=1&addonPanel=storybooks%2Fstorybook-addon-knobs">
+    </iframe>
   </div>
 </template>
