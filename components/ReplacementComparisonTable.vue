7b27dfbd43123dbc4489cef8ed8c5be926a012ad
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-10-01 18:50:00 +0200

    Implemented a dry replacement-comparison-table for use in  toggle, segmented control, radio buttons and checkboxes component pages

---
+++ components/ReplacementComparisonTable.vue
@@ -0,0 +1,77 @@
+<script>
+/**
+ * Renders a table which compares checkboxes with segmented control, toggles, and radio buttons
+ *
+ * @example
+ * <replacement-comparison-table 
+ *   column="1">
+ * />
+ */
+export default {
+  props: {
+    column: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <table>
+        <thead>
+          <tr>
+            <th>&nbsp;</th>
+            <th>Radio buttons</th>
+            <th>Segmented control</th>
+            <th>Checkboxes</th>
+            <th>Single checkbox</th>
+            <th>Toggle switches</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>How many states are available?</th>
+            <td :class="{'highlighted': column === '1'}">Multiple</td>
+            <td :class="{'highlighted': column === '2'}">Multiple</td>
+            <td :class="{'highlighted': column === '3'}">Multiple</td>
+            <td :class="{'highlighted': column === '4'}">2 (on/off)</td>
+            <td :class="{'highlighted': column === '5'}">2 (on/off)</td>
+          </tr>
+          <tr>
+            <th>How many selections can the user make?</th>
+            <td :class="{'highlighted': column === '1'}">1</td>
+            <td :class="{'highlighted': column === '2'}">1</td>
+            <td :class="{'highlighted': column === '3'}">0 – all</td>
+            <td :class="{'highlighted': column === '4'}">1</td>
+            <td :class="{'highlighted': column === '5'}">1</td>
+          </tr>
+          <tr>
+            <th>Is there a default state?</th>
+            <td :class="{'highlighted': column === '1'}">Yes</td>
+            <td :class="{'highlighted': column === '2'}">Yes</td>
+            <td :class="{'highlighted': column === '3'}">No</td>
+            <td :class="{'highlighted': column === '4'}">Yes</td>
+            <td :class="{'highlighted': column === '5'}">Yes</td>
+          </tr>
+          <tr>
+            <th>How would you describe the choices?</th>
+            <td :class="{'highlighted': column === '1'}">Mutually exclusive</td>
+            <td :class="{'highlighted': column === '2'}">Mutually exclusive</td>
+            <td :class="{'highlighted': column === '3'}">Independent of each other</td>
+            <td :class="{'highlighted': column === '4'}">Mutually exclusive</td>
+            <td :class="{'highlighted': column === '5'}">Mutually exclusive</td>
+          </tr>
+          <tr>
+            <th>When does the selection take effect?</th>
+            <td :class="{'highlighted': column === '1'}">After a user clicks a submit button</td>
+            <td :class="{'highlighted': column === '2'}">Immediately</td>
+            <td :class="{'highlighted': column === '3'}">After a user clicks a submit button</td>
+            <td :class="{'highlighted': column === '4'}">After a user clicks a submit button</td>
+            <td :class="{'highlighted': column === '5'}">Immediately</td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
+</template>
--- pages/components/toggles.vue
+++ pages/components/toggles.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
     <h1>TODO</h1>
+    <replacement-comparison-table column="1"></replacement-comparison-table>
   </div>
 </template>
+
+
+<script>
+import ReplacementComparisonTable from '~/components/ReplacementComparisonTable.vue'
+
+export default {
+  components: {
+    ReplacementComparisonTable
+  }
+}
+</script>
