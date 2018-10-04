bf604e8c1186d2df9e2614b2b0e4ac5bade3ac92
Sarrah Vesselov <svesselov@gitlab.com>
2018-10-04 23:40:19 +0000

    Add toggle documentation

--- pages/components/toggles.vue
+++ pages/components/toggles.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
-    <h1>TODO</h1>
-  </div>
+  <div class="content limited m-t-7 m-b-7">
+    <div class="component md">
+      <h1>Toggles</h1>
+      <p>A toggle is a visual switch which acts as a boolean.</p>
+      <h2>Usage</h2>
+      <p>Use toggles when there is a choice between two selections, it has a default state, the results are effective and noticeable immediately, and there is <strong>no need</strong> for a user to click a submit button.</p>
+      <p> Avoid using toggles when a submit button is needed. This confuses users and dilutes the experience by preventing instant results.</p>
+      <p>Toggles may replace a segmented control, two radio buttons, or a single checkbox to allow users to choose between two opposing states. For help with choosing the right solution, follow the table below.</p>
+      <p class="todo">Todo: Add replacement-comparison-table</p>
+      <h3>Labels</h3>
+      <p>Toggle labels are set in bold, positioned above the element by default, and should describe what the control will do when the switch is on in a concise, direct, and short way.</p>
+      <p>In rare cases and exceptions, the label can be placed to the left of the input field. Examples include non inline positioned elements.</p>
+      <h3>Visual Design</h3>
+      <p>Toggles should use high-contrast colors to indicate the states - On and Off.</p>
+      <h2>Demo</h2>
+      <p class="todo">Todo: Add live component block with code example</p>
+      <h2>Design specifications</h2>
+      <p>Color, spacing, dimension, and layout specific information pertaining to this component can be viewed using the following link:</p>
+      <p><a href="https://gitlab-org.gitlab.io/gitlab-design/hosted/design-gitlab-specs/toggles-spec-previews/">Sketch Measure Preview for toggles</a></p>
+      <h2>Related patterns</h2>
+      <ul>
+        <li><a href="/components/checkboxes">Checkboxes</a></li>
+        <li><a href="/components/radio-buttons">Radio buttons</a></li>
+        <li><a href="/components/segmented-control">Segmented control</a></li>
+      </ul>
+    </div>
+   </div>
 </template>
