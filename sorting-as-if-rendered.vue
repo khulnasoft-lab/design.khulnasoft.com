1bee3d3f57c7761ae7b22f0af195871c038c334e
Dimitrie Hoekstra <dimitrie@gitlab.com>
2018-10-10 20:26:00 +0000

    added mockup info

--- sorting-as-if-rendered.vue
+++ sorting-as-if-rendered.vue
@@ -2,6 +2,9 @@
   <div>
     <h1>Some other page</h1>
     <p>Please see the following content from tags:</p>
-    <embed page="/components/tags" content-id="vip-content" />
+    <div id="vip-content">
+      <h1>Some unique content</h1>
+      <p>bla bla</p>
+    </div>
   </div>
 </template>
