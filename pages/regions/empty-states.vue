c996cad05a27f9dadc668628dc59a3f5f83ca566
Andy Volpe <avolpe@gitlab.com>
2018-10-15 14:51:52 -0400

    empty state documentation

--- pages/regions/empty-states.vue
+++ pages/regions/empty-states.vue
@@ -2,8 +2,49 @@
   <div class="content limited m-t-7 m-b-7">
     <div class="typography md">
       <h1>Empty states</h1>
-      <p>Empty states should be considered during the implentation of new features. They are vital to helping onboard new users, making the experience feel more approachable and understandable. Empty states should feel inviting and provide just enough information to get people started. There should be a single call to action and a clear explanation of what to use the feature for.</p>
-      <p class="todo">Add examples of empty states</p>
+      <p>Empty states should be considered during the implentation of new features. They are vital to helping onboard new users, making the experience feel more approachable and understandable.</p>
+      <h2>Usage</h2>
+      <p>An empty state is our way to communicate to the user that there is no content to load and what action they should take next. They should tell users: what it’s for, why they are seeing it and what they need to do next.</p>
+      <p>Empty states are composed of 3 elements</p>
+      <strong>1. Illustration</strong>
+      <p>The illustration should compliment the need for the empty state</p>
+      <strong>2. Message</strong>
+      <p>The message should be concise and explain why the user is seeing this state</p>
+      <strong>3. Call to action</strong>
+      <p>typically taking the form of a button, the text should be direct and stuff</p>
+      </br>
+      <p class="todo">An example of a live component to add related to a usage guideline</p>
+      <!--
+        DEMO, keep this section for all patterns, the code block demo will be added at a later date
+      -->
+      <h2>Demo</h2>
+      <p class="todo">Todo: Add live component block with code example</p>
+      <!--
+        DESIGN SPECIFICATIONS, add a link here to the component-specific sketch-measure preview.
+        All design specifications should live in the design repo under 'hosted/design-gitlab-specs/COMPONENTNAME-spec-previews'
+        *** If there are max-width, min-width, or other specs that should be known about this component,
+        please add it in Sketch Measur via a note.
+        This link must ALWAYS be included.
+      -->
+      <h2>Design specifications</h2>
+      <h3>Style</h3>
+      <p><a href="/foundations/illustration">See illustration guidelines</a></p>
+      <h3>Size</h3>
+      <p>Max size on empty state illustrations is <strong>425px wide by 375px tall</strong>. Do not scale smaller illustrations up to this size as they will look too large. Instead use the original size of the illustration if it has already been drawn.</p>
+      <p><a href="#">Sketch Measure Preview for [Insert component name here]</a></p>
+      <h3>Messsage</h3>
+      <p>Use the <strong>h4</strong> style for the main headline and the <strong>paragraph</strong> style for the supporting body text. </p>
+      <h3>Buttons</h3>
+      <p>align them</p>
+      <!--
+        RELATED PATTERNS should be similar in usage/type of pattern
+        e.g. tooltips, popover, and modals are all similar constructs used for different purposes
+        Remove this section if there are no related patterns at this time. Please list them in alphabetical order.
+      -->
+      <h2>Related patterns</h2>
+      <ul>
+        <li><a href="/foundations/illustration">Illustration</a></li>
+      </ul>
     </div>
   </div>
 </template>
