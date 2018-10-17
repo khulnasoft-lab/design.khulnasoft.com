fb6b45008d91f59a4c4fa2917eab26f7ac240cfa
Andy Volpe <avolpe@gitlab.com>
2018-10-17 11:03:59 -0400

    Adding empty state documentation

--- pages/regions/empty-states.vue
+++ pages/regions/empty-states.vue
@@ -5,42 +5,45 @@
       <p>Empty states should be considered during the implentation of new features. They are vital to helping onboard new users, making the experience feel more approachable and understandable.</p>
       <h2>Usage</h2>
       <p>An empty state is our way to communicate to the user that there is no content to load and what action they should take next. They should tell users: what it’s for, why they are seeing it and what they need to do next.</p>
-      <p>Empty states are composed of 3 elements</p>
-      <strong>1. Illustration</strong>
-      <p>The illustration should compliment the need for the empty state</p>
-      <strong>2. Message</strong>
-      <p>The message should be concise and explain why the user is seeing this state</p>
-      <strong>3. Call to action</strong>
-      <p>typically taking the form of a button, the text should be direct and stuff</p>
-      </br>
-      <p class="todo">An example of a live component to add related to a usage guideline</p>
-      <!--
-        DEMO, keep this section for all patterns, the code block demo will be added at a later date
-      -->
+      <p>Empty states are composed of 3 elements, the illustration, message, and call to action. The illustration is optional but strongly recommended as it adds personality and aides the user in understanding the situation.</p>
+      <ol>
+        <strong>1. Illustration</strong>
+        <p>The illustration should compliment the need for the empty state and help the user understand the situation.</p>
+      </ol>
+      <ol>
+        <strong>2. Description</strong>
+        <p>The title and message should be clear concise and explain why the user is seeing this screen.</p>
+      </ol>
+      <ol>
+        <strong>3. Call to action</strong>
+        <p>Taking the form of a primary button with the option to add a secondary button, the text within should be direct and actionable.</p>
+      </ol>
       <h2>Demo</h2>
       <p class="todo">Todo: Add live component block with code example</p>
-      <!--
-        DESIGN SPECIFICATIONS, add a link here to the component-specific sketch-measure preview.
-        All design specifications should live in the design repo under 'hosted/design-gitlab-specs/COMPONENTNAME-spec-previews'
-        *** If there are max-width, min-width, or other specs that should be known about this component,
-        please add it in Sketch Measur via a note.
-        This link must ALWAYS be included.
-      -->
       <h2>Design specifications</h2>
-      <h3>Style</h3>
+      <h3>Illustration Style</h3>
       <p><a href="/foundations/illustration">See illustration guidelines</a></p>
       <h3>Size</h3>
       <p>Max size on empty state illustrations is <strong>425px wide by 375px tall</strong>. Do not scale smaller illustrations up to this size as they will look too large. Instead use the original size of the illustration if it has already been drawn.</p>
-      <p><a href="#">Sketch Measure Preview for [Insert component name here]</a></p>
-      <h3>Messsage</h3>
-      <p>Use the <strong>h4</strong> style for the main headline and the <strong>paragraph</strong> style for the supporting body text. </p>
-      <h3>Buttons</h3>
-      <p>align them</p>
-      <!--
-        RELATED PATTERNS should be similar in usage/type of pattern
-        e.g. tooltips, popover, and modals are all similar constructs used for different purposes
-        Remove this section if there are no related patterns at this time. Please list them in alphabetical order.
-      -->
+      <h3>Fonts</h3>
+      <ul>
+        <li>Header title: H4 primary style</li>
+        <li>Description: Primary paragraph style</li>
+      </ul>
+      <h3>Description</h3>
+      <ul>
+        <li>If the description needs to wrap on desktop, align it to the left.</li>
+        <li>If the title needs to wrap on desktop, align both the title and description to the left.</li>
+        <li>If the title and description do not wrap on desktop, center them horizontally.</li>
+      </ul>
+      <h3>Call to action</h3>
+      <ul>
+        <li>The Call to action button in the empty state should be a primary button.</li>
+        <li>There is only one primary button since there can only be one primary button per form. If there is a need for more than one button, the other should be a secondary button.</li>
+      </ul>
+      <h3>Mobile</h3>
+      <p>On mobile, the layout is same to the one on desktop. It only scales down the illustrations and changes the width of paragraph.</p>
+      <p><a href="#">To Do: Sketch Measure Preview for empty states</a></p>
       <h2>Related patterns</h2>
       <ul>
         <li><a href="/foundations/illustration">Illustration</a></li>
