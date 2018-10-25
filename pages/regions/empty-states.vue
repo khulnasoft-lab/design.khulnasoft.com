65a817c232feca2e9498771421f8ca6d7e7667e5
Andy Volpe <avolpe@gitlab.com>
2018-10-25 01:27:21 +0000

    updates to empty state documentation

--- pages/regions/empty-states.vue
+++ pages/regions/empty-states.vue
@@ -4,14 +4,14 @@
       <h1>Empty states</h1>
       <p>An empty state is our way of communicating to the user that there is no content to load and what action they should take next. They are vital in making the experience feel more approachable and understandable.</p>
       <h2>Usage</h2>
-      <p>Empty states are composed of 3 elements: the illustration, message, and call to action. The illustration is optional but strongly recommended as it adds personality and aides the user in understanding the situation.</p>
+      <p>Empty states are composed of 3 elements: the illustration, description, and call to action. The illustration is optional but strongly recommended as it adds personality and aides the user in understanding the situation.</p>
         <strong>1. Illustration</strong>
         <p>The illustration should compliment the need for the empty state and help the user understand the situation.</p>
       <strong>2. Description</strong>
         <p>The title and message should be clear concise and explain why the user is seeing this screen.</p>
         <strong>3. Call to action</strong>
         <p>Taking the form of a primary button with the option to add a secondary button, the text within should be direct and actionable.</p>
-        <a href="/components/buttons">See buttons for more usage information</a>     
+        <p><a href="/components/buttons">See buttons for more usage information</a></p>     
       <h3>When to use an empty state</h3>
       <p>Empty states can be employed to:</p>
       <ul>
@@ -21,12 +21,12 @@
         <li>Display empty search results.</li>
       </ul>
       <p>Empty states differ from banners in that they are not intended to promote or advertise new features or functionality.</p>
-      <a href="/components/banners">See banners for more usage information</a></li>
+      <p><a href="/components/banners">See banners for more usage information</a></p>
       <h2>Demo</h2>
       <p class="todo">Todo: Add live component block with code example</p>
       <h2>Design specifications</h2>
       <h3>Illustration Style</h3>
-      <a href="/foundations/illustration">See illustration guidelines</a>
+      <p><a href="/foundations/illustration">See illustration guidelines</a></p>
       <h3>Size</h3>
       <p>Max size on empty state illustrations is <strong>512px wide by 512px tall</strong>. Do not scale smaller illustrations up to this size as they will look too large. Instead use the original size of the illustration if it has already been drawn.</p>
       <h3>Fonts</h3>
