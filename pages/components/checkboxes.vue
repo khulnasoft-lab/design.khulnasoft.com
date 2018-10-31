35e8fb5687cd3e991897b78698e900e0c3eaeea0
Dimitrie Hoekstra <dimitrie@gitlab.com>
2018-10-31 13:32:40 +0000

    Improve wording

--- pages/components/checkboxes.vue
+++ pages/components/checkboxes.vue
@@ -2,17 +2,17 @@
   <div class="content limited m-t-7 m-b-7">
     <div class="component md">
       <h1>Checkboxes</h1>
-      <p>A checkbox is an input which communicates that an option is true, false, or indeterminate - one is active by default.</p>
+      <p>A checkbox is an input which acts as a boolean. Additionally it can have an indeterminate state.</p>
       <p>The indeterminate state represents a situation where a list of sub-options is grouped under a parent option and sub-options are in both selected and unselected states.</p>
       <h2>Usage</h2>
-      <p>Use a single checkbox when there is a choice between two selections, it has a default state, the results are <strong>not</strong> effective or noticeable immediately, and there is a need for a user to click a submit button.</p>
+      <p>Use a single checkbox when there is a choice between two selections, it has a default state, the results are <strong>not</strong> effective or noticeable immediately, and the user needs a confirmation of it being saved.</p>
       <p>Use multiple checkboxes to allow users to select multiple from a list of options, including all or none.</p>
-      <p>Checkboxes may replace toggles, segmented control, or radio buttons to allow users to select one, two, or multiple options. For help with choosing the right solution, follow the table below.</p>
+      <p>Checkboxes may replace toggles, segmented controls, or radio buttons to allow users to select one, two, or multiple options. For help with choosing the right solution, follow the table below.</p>
       <p class="todo">Todo: Add replacement-comparison-table</p>
       <h3>Labels</h3>
       <p>Checkbox labels are set in regular font weight, positioned to the right of the element, and should be as short as possible. If more, explanatory text is required, it should be added and styled as <a href="/components/forms#help-text">Help text</a> below the label.</p>
       <h3>Visual Design</h3>
-      <p>Checkboxes should use high-contrast colors to indicate the states - true or false. A checkmark and hyphen icon are  used to differentiate between true or indeterminate.</p>
+      <p>Checkboxes should use high-contrast colors to indicate the states - true or false. A checkmark and hyphen icon are used to differentiate between true or indeterminate.</p>
       <h2>Demo</h2>
       <p class="todo">Todo: Add live component block with code example</p>
       <h2>Design specifications</h2>
@@ -22,7 +22,7 @@
       <ul>
         <li><a href="/components/dropdowns">Dropdowns</a></li>
         <li><a href="/components/radio-buttons">Radio buttons</a></li>
-        <li><a href="/components/segmented-control">Segmented control</a></li>
+        <li><a href="/components/segmented-control">Segmented controla</a></li>
         <li><a href="/components/toggles">Toggles</a></li>
       </ul>
     </div>
