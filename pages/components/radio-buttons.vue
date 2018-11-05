12c5cb59e1bc4290d01dc81241fc23cecb7aa2e7
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-11-05 14:24:15 +0100

    improved wording

--- pages/components/radio-buttons.vue
+++ pages/components/radio-buttons.vue
@@ -2,29 +2,24 @@
   <div class="content limited m-t-7 m-b-7">
     <div class="component md">
       <h1>Radio buttons</h1>
-      <p>A radio button is an input which includes two mutually exclusive states - either selected or unselected - one is active by default.</p>
-      <p>A group of radio buttons is a select list which can only have one radio button selected at any time - one is active by default.</p>
-      <!-- Usage -->
+      <p>Radio buttons are an input which acts as a select list. Every radio button acts as a boolean, but only have one radio button can be selected at any time - one is active by default.</p>
       <h2>Usage</h2>
-      <p>Radio buttons can never be used alone and must always be used in group form. Use a group of radio buttons when there is a choice between two or multiple selections, it has a default state, the results are <strong>not</strong> effective or noticeable immediately, and there is the need for a user to click a submit button.</p>
-      <p>Radio buttons may replace a segmented control, a list of checkboxes, or a toggle to allow users to choose between two or multiple options. For help with choosing the right solution, follow the table below.</p>
+      <p>Radio buttons can never be used alone and must always be used in group form. Use radio buttons when there is a choice between two or multiple selections, it has a default state, the results are <strong>not</strong> effective or noticeable immediately, and the user needs a confirmation of it being saved.</p>
+      <p>Radio buttons may replace segmented controls, a list of checkboxes, or a toggle to allow users to choose between two or multiple options. For help with choosing the right solution, follow the table below.</p>
       <p class="todo">Todo: Add replacement-comparison-table</p>
       <h3>Labels</h3>
       <p>Radio button labels are set in regular font weight, positioned to the right of the element, and should be as short as possible. If more, explanatory text is required, it should be added and styled as <a href="/components/forms#help-text">Help text</a> below the label.</p>
       <h3>Visual Design</h3>
       <p>Radio buttons should use high-contrast colors and a dot icon to indicate the states - selected and unselected.</p>
-
       <h2>Demo</h2>
       <p class="todo">Todo: Add live component block with code example</p>
-
       <h2>Design specifications</h2>
       <p>Color, spacing, dimension, and layout specific information pertaining to this component can be viewed using the following link:</p>
       <p><a href="https://gitlab-org.gitlab.io/gitlab-design/hosted/design-gitlab-specs/radiobuttons-spec-previews/">Sketch Measure Preview for radio buttons</a></p>
-
       <h2>Related patterns</h2>
       <ul>
-        <li><a href="/components/segmented-control">Segmented control</a></li>
         <li><a href="/components/checkboxes">Checkboxes</a></li>
+        <li><a href="/components/segmented-control">Segmented control</a></li>
         <li><a href="/components/toggles">Toggles</a></li>
       </ul>
     </div>
