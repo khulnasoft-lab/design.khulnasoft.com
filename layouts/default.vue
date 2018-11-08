12eb38208c1d2d975fd4a24702f78b5c17aa168d
Andy Volpe <avolpe@gitlab.com>
2018-11-08 00:44:36 +0000

    adding accordions to default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -73,6 +73,10 @@
           <summary class="nav-sidebar__section-summary">Components</summary>
           <nuxt-link 
             class="nav-sidebar__section-items-anchor" 
+            to="/components/accordions"
+          >Accordions</nuxt-link>
+          <nuxt-link 
+            class="nav-sidebar__section-items-anchor" 
             to="/components/alerts"
           >Alerts</nuxt-link>
           <nuxt-link 
