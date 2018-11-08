8495bf7737b0e7e54f5e4f142532bc456a31adad
Dimitrie Hoekstra <dimitrie@gitlab.com>
2018-11-08 00:15:43 +0000

    Checkboxes

---
+++ pages/components/checkboxes.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="content limited m-t-7 m-b-7">
+    <div class="component md">
+      <h1>Checkboxes</h1>
+      <p>A checkbox is an input which acts as a boolean. Additionally it can have an indeterminate state.</p>
+      <p>The indeterminate state represents a situation where a list of sub-options is grouped under a parent option and sub-options are in both selected and unselected states.</p>
+      <h2>Usage</h2>
+      <p>Use a single checkbox when there is a choice between two selections, it has a default state, the results are <strong>not</strong> effective or noticeable immediately, and the user needs a confirmation of it being saved.</p>
+      <p>Use multiple checkboxes to allow users to select multiple from a list of options, including all or none.</p>
+      <p>Checkboxes may replace toggles, segmented controls, or radio buttons to allow users to select one, two, or multiple options. For help with choosing the right solution, follow the table below.</p>
+      <p class="todo">Todo: Add replacement-comparison-table</p>
+      <h3>Labels</h3>
+      <p>Checkbox labels are set in regular font weight, positioned to the right of the element, and should be as short as possible. If more, explanatory text is required, it should be added and styled as <a href="/components/forms#help-text">help text</a> below the label.</p>
+      <h3>Visual Design</h3>
+      <p>Checkboxes should use high-contrast colors to indicate the states - true or false. A checkmark and hyphen icon are used to differentiate between true or indeterminate.</p>
+      <h2>Demo</h2>
+      <p class="todo">Todo: Add live component block with code example</p>
+      <h2>Design specifications</h2>
+      <p>Color, spacing, dimension, and layout specific information pertaining to this component can be viewed using the following link:</p>
+      <p><a href="https://gitlab-org.gitlab.io/gitlab-design/hosted/design-gitlab-specs/checkboxes-spec-previews/">Sketch Measure Preview for checkboxes</a></p>
+      <h2>Related patterns</h2>
+      <ul>
+        <li><a href="/components/dropdowns">Dropdowns</a></li>
+        <li><a href="/components/radio-buttons">Radio buttons</a></li>
+        <li><a href="/components/segmented-control">Segmented controla</a></li>
+        <li><a href="/components/toggles">Toggles</a></li>
+      </ul>
+    </div>
+   </div>
+</template>
