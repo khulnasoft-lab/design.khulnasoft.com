fbd12dc02a7de15232544186f4c6eec177695f54
Tim Zallmann <tzallmann@gitlab.com>
2018-11-15 07:54:29 +0100

    Updated package.json with newest gitlab-ui

--- plugins/gitlab-ui.js
+++ plugins/gitlab-ui.js
@@ -3,7 +3,6 @@ import * as gitlabComponents from '@gitlab-org/gitlab-ui/documentation' // eslin
 import { GlComponentDocumentation } from '@gitlab-org/gitlab-ui/documentation' // eslint-disable-line
 
 Object.keys(gitlabComponents).forEach(plugin => {
-  console.log('Register : ' + plugin) // eslint-disable-line
   if (plugin.indexOf('Directive') === -1) {
     Vue.component(plugin, gitlabComponents[plugin])
   }
