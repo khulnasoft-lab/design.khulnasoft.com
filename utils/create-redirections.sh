7cf31aa09f64133ddc81e478913d33d93088e7c5
Winnie Hellmann <winnie@gitlab.com>
2018-11-16 11:32:32 +0100

    Create redirections for dynamic URLs

---
+++ utils/create-redirections.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Generates HTML files which redirect to dynamic routes so we can link to them without # in the URL
+
+set -o errexit
+set -o nounset
+
+basedir=$(cd "$(dirname $0)/.."; pwd)
+
+cd "${basedir}/contents"
+for file in $(find components -name '*.md')
+do
+  echo "Creating redirection for ${file} ..."
+  url="$(dirname ${file})/$(basename -s .md ${file})"
+  mkdir -p "${basedir}/public/${url}"
+  echo "<meta http-equiv=\"refresh\" content=\"0;url=/#/${url}\">Redirecting..." > "${basedir}/public/${url}/index.html"
+done
