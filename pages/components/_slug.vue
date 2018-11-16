e5a0784f924b1243d9a1ee1389a9d2759fb03a5f
Winnie Hellmann <winnie@gitlab.com>
2018-11-16 14:14:27 +0100

    chore: Use asyncData to load ComponentInfo

--- pages/components/_slug.vue
+++ pages/components/_slug.vue
@@ -1,39 +1,43 @@
 <template>
   <div class="content limited m-t-7 m-b-7">
-    <div v-if="component">
-      <component 
-        :is="component"
-        :frontmatter-info="fmResult"
-      />
+    <div v-if="frontMatter">
+      <component-info :frontmatter-info="frontMatter" />
     </div>
     <div v-else>
-      Loading component ...
+      Loading component…
     </div>
   </div>
 </template>
 
 <script>
-const getPost = () => ({
-  component: import(`~/components/componentinfo.vue`),
-})
+import ComponentInfo from '../../components/componentinfo.vue';
+
 export default {
+  components: {
+    ComponentInfo
+  },
+  validate ({ params }) {
+    return !params.slug.endsWith('.json')
+  },
+  asyncData ({ app, params, error }) {
+    return app.$axios
+      .$get(`/contents/components/${params.slug}.json`)
+      .then((frontMatter) => {
+        if (!frontMatter.attributes || !frontMatter.body) {
+          throw new Error(`Unexpected response: ${frontMatter}`)
+        }
+
+        return { frontMatter }
+      })
+      .catch((e) => {
+        error({ statusCode: 404, message: `Failed to load component ${params.slug}!` })
+        console.error(e) // eslint-disable-line
+      })
+  },
   data() {
     return {
-      component: null,
-      fmResult: null,
+      frontMatter: null,
     }
-  },
-  beforeCreate() {
-    this.$axios
-      .$get(`/contents/components/${this.$route.params.slug}.json`)
-      .then(fmResult => {
-        this.fmResult = fmResult
-        this.component = () => getPost(this.$route.params.slug)
-      })
-      .catch(e => {
-        // eslint-disable-next-line
-        console.log('Err : ', e)
-      })
-  },
+  }
 }
 </script>
