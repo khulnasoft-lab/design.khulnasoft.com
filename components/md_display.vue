d82046380251db0b359173fda99ee2937fa1bed9
Tim Zallmann <tzallmann@gitlab.com>
2018-11-19 12:55:41 +0100

    Changed the no description text

--- components/md_display.vue
+++ components/md_display.vue
@@ -5,35 +5,29 @@ export default {
   props: {
     md: {
       default: '',
-      type: String
-    }
+      type: String,
+    },
   },
   render(createElement) {
     if (!this.md) {
-      return createElement('h4', 'No description')
+      return createElement('h4', "This component's documentation has not yet been added.")
     }
 
     const md = markdowner({
       xhtmlOut: true,
-      typographer: true
+      typographer: true,
     })
     let mdOutput = md.render(this.md)
-    mdOutput = mdOutput.replace(
-      /\[\[Example:(.*?)\]\]/g,
-      '<gl-example-display exampleName="$1" />'
-    )
+    mdOutput = mdOutput.replace(/\[\[Example:(.*?)\]\]/g, '<gl-example-display exampleName="$1" />')
 
     // Format Todo Messages as before
-    mdOutput = mdOutput.replace(
-      /Todo:(.*?)\n/gm,
-      '<p class="todo">Todo: $1</p>'
-    )
+    mdOutput = mdOutput.replace(/Todo:(.*?)\n/gm, '<p class="todo">Todo: $1</p>')
 
     const dynamicElement = {
-      template: `<div class="component md">${mdOutput}</div>`
+      template: `<div class="component md">${mdOutput}</div>`,
     }
 
     return createElement(dynamicElement)
-  }
+  },
 }
 </script>
