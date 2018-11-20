7487741649e4276b50c491f4b9d6e6ad6cc05022
Andy Volpe <avolpe@gitlab.com>
2018-11-19 20:52:29 -0500

    draft of pagiantion component documentation

--- pages/foundations/illustration.vue
+++ pages/foundations/illustration.vue
@@ -1,34 +1,5 @@
 <template>
-  <div class="content limited m-t-7 m-b-7">
-    <div class="component md">
-      <h1>Pagination</h1>
-      <p>Breaking-up lists and distributing the results either by the use of pagination or by loading more results at the bottom of the page helps the user parse a large number items whenever there are too many results to show at once. We use both pagination and a load more button to segment results based on the users task. </p>
-      <h3>Pagination</h3>
-      <p>Pagination breaks up results into several pages with controls for navigating to a specific page or to the next or previous page. Pagination aides users who are looking for a specific item in a list to complete their task.</p>
-      <h3>Load more</h3>
-      <p>A load more button positioned at the bottom of a list gives users an efficient way to browse long lists of content without having to reload the page, allowing users to scroll though long streams of information efficiently.</p>
-      <h2>Usage</h2>
-      <p>We use pagination to breakup results into several pages with a control for navigating to the next or previous page. Alternatively, there are instances where load more provides a better experience to users with browsing tasks. </p>
-      <h3>Pagination usage</h3>
-      <p>Use pagination with lists where the user's goal is to find a specific item to complete their task regardless of when that item was entered into the list. Good examples for lists better suited for pagination are, issues, merge requests, todos, and milestones. </p>
-      <p class="todo">An example of a live pagination component to add related to a usage guideline</p>
-      <h3>Load more usage</h3>
-      <h2>Demo</h2>
-      <p class="todo">Todo: Add live component block with code example</p>
-      <!--
-        DESIGN SPECIFICATIONS, add a link here to the component-specific sketch-measure preview.
-        All design specifications should live in the design repo under 'hosted/design-gitlab-specs/COMPONENTNAME-spec-previews'
-        *** If there are max-width, min-width, or other specs that should be known about this component,
-        please add it in Sketch Measur via a note.
-        This link must ALWAYS be included.
-      -->
-      <h2>Design specifications</h2>
-      <p>Color, spacing, dimension, and layout specific information pertaining to this component can be viewed using the following link:</p>
-      <p><a href="#">Sketch Measure Preview for [Insert component name here]</a></p>
-      <h2>Related patterns</h2>
-      <ul>
-        <li><a href="/components/buttons">Buttons</a></li>
-      </ul>
-    </div>
+  <div>
+    <h1>TODO</h1>
   </div>
 </template>
