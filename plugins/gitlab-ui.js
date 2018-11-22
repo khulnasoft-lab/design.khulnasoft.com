12a8d18c0532bc44bc1a9d9ca3d36c346e218599
Winnie Hellmann <winnie@gitlab.com>
2018-11-22 22:46:25 +0100

    Upgrade to @gitlab/ui 1.12.0

--- plugins/gitlab-ui.js
+++ plugins/gitlab-ui.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import * as gitlabComponents from '@gitlab-org/gitlab-ui/documentation' // eslint-disable-line import/no-duplicates
-import { GlComponentDocumentation } from '@gitlab-org/gitlab-ui/documentation' // eslint-disable-line import/no-duplicates
+import * as gitlabComponents from '@gitlab/ui/documentation' // eslint-disable-line import/no-duplicates
+import { GlComponentDocumentation } from '@gitlab/ui/documentation' // eslint-disable-line import/no-duplicates
 
 Object.entries(gitlabComponents)
   .filter(([componentName]) => !componentName.includes('Directive'))
