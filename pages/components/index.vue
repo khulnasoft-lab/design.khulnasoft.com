20337617c32a99b5a3bb66c7d1076bd5859a8e9c
Tim Zallmann <tzallmann@gitlab.com>
2018-11-26 13:54:33 +0100

    Added back deleted css and adapted table on index

--- pages/components/index.vue
+++ pages/components/index.vue
@@ -1,40 +1,31 @@
 <template>
   <div class="content limited m-t-7 m-b-7">
-    <div class="">
+    <div class>
       <h1>GitLab Components</h1>
-      <div
-        v-if="contentTree"
-        class="mt-3"
-      >
-        <b-list-group>
-          <b-list-group-item 
-            v-for="component in contentTree.components"
-            :key="component.id"
-          >
-            <nuxt-link 
-              :key="`link-${component.id}`"
-              :to="`/components/${component.id}`"
-              class="nav-sidebar__section-items-anchor"
-            >
-              {{ component.name }}
-            </nuxt-link>
-            <b-badge
-              v-if="component.hasVueComponent"
-              class="float-right ml-1"
-              variant="primary"
-            >
-              Vue
-            </b-badge>
-            <b-badge
-              v-if="!component.hasInfo"
-              class="float-right"
-              variant="warning"
-            >
-              To-Do
-            </b-badge>
-          </b-list-group-item>
-        </b-list-group>
-      </div>
+      <table v-if="contentTree" class="m-t-6 m-b-6" style="width: 65%;">
+        <tbody>
+          <b-list-group>
+            <b-list-group-item v-for="component in contentTree.components" :key="component.id">
+              <tr>
+                <td>
+                  <nuxt-link
+                    :key="`link-${component.id}`"
+                    :to="`/components/${component.id}`"
+                  >{{ component.name }}</nuxt-link>
+                </td>
+                <td>
+                  <b-badge
+                    v-if="component.hasVueComponent"
+                    class="float-right ml-1"
+                    variant="primary"
+                  >Vue</b-badge>
+                  <b-badge v-if="!component.hasInfo" class="float-right" variant="warning">To-Do</b-badge>
+                </td>
+              </tr>
+            </b-list-group-item>
+          </b-list-group>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -43,19 +34,19 @@
 export default {
   data() {
     return {
-      contentTree: null,
-    };
+      contentTree: null
+    }
   },
   created() {
     this.$axios
       .$get(`/contents/contentTree.json`)
       .then(treeResult => {
-        this.contentTree = treeResult;
+        this.contentTree = treeResult
       })
       .catch(e => {
         // eslint-disable-next-line
-        console.log('Err : ', e);
-      });
-  },
-};
+        console.log('Err : ', e)
+      })
+  }
+}
 </script>
