927242daa50012e5c7cae07923818e8ba88498a6
Tim Zallmann <tzallmann@gitlab.com>
2018-11-26 14:06:11 +0100

    Fixed the components overview

--- pages/components/index.vue
+++ pages/components/index.vue
@@ -2,29 +2,37 @@
   <div class="content limited m-t-7 m-b-7">
     <div class>
       <h1>GitLab Components</h1>
-      <table v-if="contentTree" class="m-t-6 m-b-6" style="width: 65%;">
-        <tbody>
-          <b-list-group>
-            <b-list-group-item v-for="component in contentTree.components" :key="component.id">
-              <tr>
-                <td>
-                  <nuxt-link
-                    :key="`link-${component.id}`"
-                    :to="`/components/${component.id}`"
-                  >{{ component.name }}</nuxt-link>
-                </td>
-                <td>
-                  <b-badge
-                    v-if="component.hasVueComponent"
-                    class="float-right ml-1"
-                    variant="primary"
-                  >Vue</b-badge>
-                  <b-badge v-if="!component.hasInfo" class="float-right" variant="warning">To-Do</b-badge>
-                </td>
-              </tr>
-            </b-list-group-item>
-          </b-list-group>
-        </tbody>
+      <table 
+        v-if="contentTree"
+        class="m-t-6 m-b-6"
+        style="width:65%"
+      >
+        <b-list-group tag="tbody">
+          <b-list-group-item
+            v-for="component in contentTree.components"
+            :key="component.id"
+            tag="tr"
+          >
+            <td>
+              <nuxt-link
+                :key="`link-${component.id}`"
+                :to="`/components/${component.id}`"
+              >{{ component.name }}</nuxt-link>
+            </td>
+            <td class="app-styles">
+              <b-badge
+                v-if="!component.hasInfo"
+                class="float-right ml-1"
+                variant="warning"
+              >To-Do</b-badge>
+              <b-badge
+                v-if="component.hasVueComponent"
+                class="float-right"
+                variant="primary"
+              >Vue</b-badge>
+            </td>
+          </b-list-group-item>
+        </b-list-group>
       </table>
     </div>
   </div>
