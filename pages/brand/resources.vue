f415b8ab20c7e46824154df8ec8c14abf42badf0
sarrahvesselov <svesselov@gitlab.com>
2018-11-29 21:08:46 -0500

    update to-dos and add proper indentation

--- pages/brand/resources.vue
+++ pages/brand/resources.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content limited m-t-7 m-b-7">
     <div class="component md">
-      To do
+       This documentation has not yet been added
     </div>
   </div>
 </template>
