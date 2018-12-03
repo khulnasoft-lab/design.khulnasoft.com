3b3f93c0975f28397c7c2a72181bef12d95979b6
tauriedavis <taurie@gitlab.com>
2018-12-03 15:06:19 -0800

    Remove system messages

--- pages/getting-started/status.vue
+++ pages/getting-started/status.vue
@@ -187,10 +187,6 @@
             <td>🚫</td>
           </tr>
           <tr>
-            <td><a href="../../components/system-messages">System messages</a></td>
-            <td>🚫</td>
-          </tr>
-          <tr>
             <td><a href="../../components/tabs">Tabs</a></td>
             <td>⚠️</td>
           </tr>
