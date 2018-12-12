27eb370bcd015c2d93227c4d03ba263184264d4e
sarrahvesselov <svesselov@gitlab.com>
2018-12-12 16:32:20 -0500

    add logo image to assets. add content to logo page

--- pages/brand-foundations/gitlab-logo.vue
+++ pages/brand-foundations/gitlab-logo.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="content limited m-t-7 m-b-7">
     <div class="component md">
-       This documentation has not yet been added
+      <h1>The GitLab logo</h1>
+      <p>The GitLab logo consists of two components, the icon (the tanuki) and the wordmark:</p>
+      <img 
+            src="assets/images/brand/gitlab-lockup.png" alt="GitLab logo lockup" 
+            class="a-center"
+          />
     </div>
   </div>
 </template>
---
+++ pages/brand-get-started/brand-personas.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="content limited m-t-7 m-b-7">
+    <div class="component md">
+      <h1>Brand personas</h1>
+    </div>
+  </div>
+</template>
