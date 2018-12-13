91f7773e9c020ddc24d68437383a47fee62fe35c
sarrahvesselov <svesselov@gitlab.com>
2018-12-13 12:18:20 -0500

    add logo image and a temporary static width

--- pages/brand-foundations/gitlab-logo.vue
+++ pages/brand-foundations/gitlab-logo.vue
@@ -3,10 +3,10 @@
     <div class="component md">
       <h1>The GitLab logo</h1>
       <p>The GitLab logo consists of two components, the icon (the tanuki) and the wordmark:</p>
-      <img 
-            src="assets/images/brand/gitlab-lockup.png" alt="GitLab logo lockup" 
-            class="a-center"
-          />
+        <img 
+          src="~/assets/images/gitlab-lockup.png" alt="GitLab logo lockup" 
+          width="600px"
+        />
     </div>
   </div>
 </template>
