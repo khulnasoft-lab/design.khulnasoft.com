fa9cc6443d102c9146ef6b44c8fe53b98880f845
sarrahvesselov <svesselov@gitlab.com>
2018-12-13 12:35:35 -0500

    add logo safe space text and images

--- pages/brand-foundations/gitlab-logo.vue
+++ pages/brand-foundations/gitlab-logo.vue
@@ -3,10 +3,30 @@
     <div class="component md">
       <h1>The GitLab logo</h1>
       <p>The GitLab logo consists of two components, the icon (the tanuki) and the wordmark:</p>
-        <img 
-          src="~/assets/images/gitlab-lockup.png" alt="GitLab logo lockup" 
-          width="600px"
-        />
+      <img 
+        src="~/assets/images/brand/gitlab-lockup.png" alt="GitLab logo lockup" 
+        width="600px"
+      />
+      <p>GitLab is most commonly represented by the logo, and in some cases, the icon alone. GitLab is rarely represented by the wordmark alone as we'd like to build brand recognition of the icon alone (e.g. the Nike swoosh), and doing so by pairing it with the GitLab wordmark.</p>
+      <h2>Logo safe space</h2>
+      <p>Safe space acts as a buffer between the logo or icon and other visual components, including text. this space is the minimum distance needed and is equal to the x-height of the GitLab wordmark:</p>
+      <img 
+        src="~/assets/images/brand/x-height.png" alt="GitLab logo x-height" 
+        width="600px"
+      />
+      <img 
+        src="~/assets/images/brand/logo-safe-space.png" alt="GitLab logo safe space" 
+        width="600px"
+      />
+      <img 
+        src="~/assets/images/brand/icon-safe-space.png" alt="GitLab logo icon safe space" 
+        width="278px"
+      />
+      <p>The x-height also determines the proper spacing between icon and workdmark, as well as, the correct scale of the icon relative to the wordmark:</p>
+      <img 
+        src="~/assets/images/brand/stacked-logo-safe-space.png" alt="stacked GitLab logo safe space" 
+        width="278px"
+      />
     </div>
   </div>
 </template>
