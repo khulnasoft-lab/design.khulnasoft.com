33615313aae2d82f4d8519b2f98958834f19df6d
Tim Zallmann <tzallmann@gitlab.com>
2018-12-14 20:07:02 +0100

    Updated Menu Interaction, Moved status page and added data

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,48 +7,47 @@
           class="nav-sidebar__header-anchor p-a-5"
         >
           <svg 
-                  width="24" 
-                  height="24" 
-                  class="tanuki-logo m-r-2" 
-                  viewBox="0 0 36 36"
-                >
-                  <path
-                    class="tanuki-shape tanuki-left-ear"
-                    fill="#e24329"
-                    d="M2 14l9.38 9v-9l-4-12.28c-.205-.632-1.176-.632-1.38 0z"
-                  />
-                  <path
-                    class="tanuki-shape tanuki-right-ear"
-                    fill="#e24329"
-                    d="M34 14l-9.38 9v-9l4-12.28c.205-.632 1.176-.632 1.38 0z"
-                  />
-                  <path
-                    class="tanuki-shape tanuki-nose"
-                    fill="#e24329"
-                    d="M18,34.38 3,14 33,14 Z"
-                  />
-                  <path
-                    class="tanuki-shape tanuki-left-eye"
-                    fill="#fc6d26"
-                    d="M18,34.38 11.38,14 2,14 6,25Z"
-                  />
-                  <path
-                    class="tanuki-shape tanuki-right-eye"
-                    fill="#fc6d26"
-                    d="M18,34.38 24.62,14 34,14 30,25Z"
-                  />
-                  <path
-                    class="tanuki-shape tanuki-left-cheek"
-                    fill="#fca326"
-                    d="M2 14L.1 20.16c-.18.565 0 1.2.5 1.56l17.42 12.66z"
-                  />
-                  <path
-                    class="tanuki-shape tanuki-right-cheek"
-                    fill="#fca326"
-                    d="M34 14l1.9 6.16c.18.565 0 1.2-.5 1.56L18 34.38z"
-                  />
-                </svg>
-
+            width="24" 
+            height="24" 
+            class="tanuki-logo m-r-2" 
+            viewBox="0 0 36 36"
+          >
+            <path
+              class="tanuki-shape tanuki-left-ear"
+              fill="#e24329"
+              d="M2 14l9.38 9v-9l-4-12.28c-.205-.632-1.176-.632-1.38 0z"
+            />
+            <path
+              class="tanuki-shape tanuki-right-ear"
+              fill="#e24329"
+              d="M34 14l-9.38 9v-9l4-12.28c.205-.632 1.176-.632 1.38 0z"
+            />
+            <path
+              class="tanuki-shape tanuki-nose"
+              fill="#e24329"
+              d="M18,34.38 3,14 33,14 Z"
+            />
+            <path
+              class="tanuki-shape tanuki-left-eye"
+              fill="#fc6d26"
+              d="M18,34.38 11.38,14 2,14 6,25Z"
+            />
+            <path
+              class="tanuki-shape tanuki-right-eye"
+              fill="#fc6d26"
+              d="M18,34.38 24.62,14 34,14 30,25Z"
+            />
+            <path
+              class="tanuki-shape tanuki-left-cheek"
+              fill="#fca326"
+              d="M2 14L.1 20.16c-.18.565 0 1.2.5 1.56l17.42 12.66z"
+            />
+            <path
+              class="tanuki-shape tanuki-right-cheek"
+              fill="#fca326"
+              d="M34 14l1.9 6.16c.18.565 0 1.2-.5 1.56L18 34.38z"
+            />
+          </svg>
           GitLab Design System
         </nuxt-link>
       </div>
@@ -58,13 +57,16 @@
             :open="$route.fullPath.startsWith('/brand-personality/')" 
             class="nav-sidebar__section"
           >
+            <span class="nav-sidebar__section-title">Brand</span>
             <li>
-              <span class="nav-sidebar__section-title">Brand</span>
-                <details 
+              <details 
                 :open="$route.fullPath.startsWith('/brand-personality/')" 
                 class="nav-sidebar__section"
-                >
-                <summary class="nav-sidebar__section-summary nav-sidebar--indent-1">Get started</summary>
+              >
+                <summary
+                  class="nav-sidebar__section-summary nav-sidebar--indent-1"
+                  @click="selectFirstSubItem"
+                >Get started</summary>
                 <nuxt-link 
                   class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
                   to="/brand-personality/brand-personality"
@@ -73,35 +75,38 @@
                   class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
                   to="/brand-personality/tone-of-voice"
                 >Tone of voice</nuxt-link>
-                </details>
+              </details>
             </li>
             <li>
-                <details 
+              <details 
                 :open="$route.fullPath.startsWith('/brand-foundations/')" 
                 class="nav-sidebar__section"
-                >
-                  <summary class="nav-sidebar__section-summary nav-sidebar--indent-1">Foundations</summary>
-                  <nuxt-link 
-                    class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
-                    to="/brand-foundations/gitlab-logo"
-                  >The GitLab logo</nuxt-link>
-                  <nuxt-link 
-                    class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
-                    to="/brand-foundations/colors"
-                  >Colors</nuxt-link>
-                  <nuxt-link
-                    class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
-                    to="/brand-foundations/typography"
-                  >Typography</nuxt-link>
-                  <nuxt-link 
-                    class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
-                    to="/brand-foundations/buttons"
-                  >Buttons</nuxt-link>
-                  <nuxt-link 
-                    class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
-                    to="/brand-foundations/iconography"
-                  >Iconography</nuxt-link>
-                </details>
+              >
+                <summary
+                  class="nav-sidebar__section-summary nav-sidebar--indent-1"
+                  @click="selectFirstSubItem"
+                >Foundations</summary>
+                <nuxt-link 
+                  class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
+                  to="/brand-foundations/gitlab-logo"
+                >The GitLab logo</nuxt-link>
+                <nuxt-link 
+                  class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
+                  to="/brand-foundations/colors"
+                >Colors</nuxt-link>
+                <nuxt-link
+                  class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
+                  to="/brand-foundations/typography"
+                >Typography</nuxt-link>
+                <nuxt-link 
+                  class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
+                  to="/brand-foundations/buttons"
+                >Buttons</nuxt-link>
+                <nuxt-link 
+                  class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
+                  to="/brand-foundations/iconography"
+                >Iconography</nuxt-link>
+              </details>
             </li>
             <nuxt-link 
               tag="li"
@@ -124,31 +129,33 @@
                 :open="$route.fullPath.startsWith('/getting-started/')" 
                 class="nav-sidebar__section"
               >
-                <summary class="nav-sidebar__section-summary nav-sidebar--indent-1">Get started</summary>
+                <summary
+                  class="nav-sidebar__section-summary nav-sidebar--indent-1"
+                  @click="selectFirstSubItem"
+                >Get started</summary>
                 <nuxt-link 
-                class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
-                to="/getting-started/structure"
-              >Structure</nuxt-link>
-              <nuxt-link 
-                class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
-                to="/getting-started/personas"
-              >Personas</nuxt-link>
-              <nuxt-link 
-                class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
-                to="/getting-started/contributing"
-              >Contributing</nuxt-link>
-              <nuxt-link 
-                class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
-                to="/getting-started/status"
-              >Status</nuxt-link>
+                  class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
+                  to="/getting-started/structure"
+                >Structure</nuxt-link>
+                <nuxt-link 
+                  class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
+                  to="/getting-started/personas"
+                >Personas</nuxt-link>
+                <nuxt-link 
+                  class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
+                  to="/getting-started/contributing"
+                >Contributing</nuxt-link>
               </details>
             </li>
             <li>
               <details 
-              :open="$route.fullPath.startsWith('/foundations/')" 
-              class="nav-sidebar__section"
+                :open="$route.fullPath.startsWith('/foundations/')" 
+                class="nav-sidebar__section"
               >
-                <summary class="nav-sidebar__section-summary nav-sidebar--indent-1">Foundations</summary>
+                <summary
+                  class="nav-sidebar__section-summary nav-sidebar--indent-1"
+                  @click="selectFirstSubItem"
+                >Foundations</summary>
                 <nuxt-link
                   class="nav-sidebar__section-items-anchor nav-sidebar--indent-2"
                   to="/foundations/colors"
@@ -184,7 +191,10 @@
                 :open="$route.fullPath.startsWith('/layout/')" 
                 class="nav-sidebar__section"
               >
-                <summary class="nav-sidebar__section-summary nav-sidebar--indent-1">Layout</summary>
+                <summary
+                  class="nav-sidebar__section-summary nav-sidebar--indent-1"
+                  @click="selectFirstSubItem"
+                >Layout</summary>
                 <nuxt-link 
                   class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
                   to="/layout/spacing"
@@ -200,7 +210,14 @@
                 :open="$route.fullPath.startsWith('/components')" 
                 class="nav-sidebar__section"
               >
-                <summary class="nav-sidebar__section-summary nav-sidebar--indent-1">Components</summary>
+                <summary
+                  class="nav-sidebar__section-summary nav-sidebar--indent-1"
+                  @click="selectFirstSubItem"
+                >Components</summary>
+                <nuxt-link 
+                  class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
+                  to="/components/status"
+                >Status</nuxt-link>
                 <nuxt-link 
                   v-for="component in contentTree.components"
                   :key="component.id"
@@ -213,10 +230,13 @@
             </li>
             <li>
               <details 
-              :open="$route.fullPath.startsWith('/regions/')" 
-              class="nav-sidebar__section"
+                :open="$route.fullPath.startsWith('/regions/')" 
+                class="nav-sidebar__section"
               >
-                <summary class="nav-sidebar__section-summary nav-sidebar--indent-1">Regions</summary>
+                <summary
+                  class="nav-sidebar__section-summary nav-sidebar--indent-1"
+                  @click="selectFirstSubItem"
+                >Regions</summary>
                 <nuxt-link 
                   class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
                   to="/regions/empty-states"
@@ -232,7 +252,10 @@
                 :open="$route.fullPath.startsWith('/content/')" 
                 class="nav-sidebar__section"
               >
-                <summary class="nav-sidebar__section-summary nav-sidebar--indent-1">Content</summary>
+                <summary
+                  class="nav-sidebar__section-summary nav-sidebar--indent-1"
+                  @click="selectFirstSubItem"
+                >Content</summary>
                 <nuxt-link 
                   class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
                   to="/content/voice-tone"
@@ -252,7 +275,10 @@
                 :open="$route.fullPath.startsWith('/usability/')" 
                 class="nav-sidebar__section"
               >
-                <summary class="nav-sidebar__section-summary nav-sidebar--indent-1">Usability</summary>
+                <summary
+                  class="nav-sidebar__section-summary nav-sidebar--indent-1"
+                  @click="selectFirstSubItem"
+                >Usability</summary>
                 <nuxt-link 
                   class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
                   to="/usability/i18n"
@@ -265,15 +291,18 @@
             </li>
             <li>
               <details 
-              :open="$route.fullPath.startsWith('/resources/')" 
-              class="nav-sidebar__section"
-            >
-              <summary class="nav-sidebar__section-summary nav-sidebar--indent-1">Resources</summary>
-              <nuxt-link 
-                class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
-                to="/resources/design-resources"
-              >Design resources</nuxt-link>
-            </details>
+                :open="$route.fullPath.startsWith('/resources/')" 
+                class="nav-sidebar__section"
+              >
+                <summary
+                  class="nav-sidebar__section-summary nav-sidebar--indent-1"
+                  @click="selectFirstSubItem"
+                >Resources</summary>
+                <nuxt-link 
+                  class="nav-sidebar__section-items-anchor nav-sidebar--indent-2" 
+                  to="/resources/design-resources"
+                >Design resources</nuxt-link>
+              </details>
             </li>
           </ul>
           <ul
@@ -361,6 +390,25 @@ export default {
         // eslint-disable-next-line
         console.log('Err : ', e)
       })
+  },
+  methods: {
+    selectFirstSubItem(ev) {
+      const { target } = ev
+      if (target && target.parentNode) {
+        const detailsLinks = target.parentNode.querySelectorAll('a')
+        if (detailsLinks.length > 0) {
+          const firstLink = detailsLinks[0].getAttribute('href')
+          if (firstLink) {
+            const that = this
+            setTimeout(() => {
+              that.$router.push({
+                path: firstLink.replace(/#/, '')
+              })
+            }, 5)
+          }
+        }
+      }
+    }
   }
 }
 </script>
