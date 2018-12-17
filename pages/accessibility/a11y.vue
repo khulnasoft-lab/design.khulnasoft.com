36308718bbef807e628de608487c826c70b9ebf0
tauriedavis <taurie@gitlab.com>
2018-12-17 11:11:32 -0800

    Update a11y statement to fix lost paragraph from rebase

--- pages/accessibility/a11y.vue
+++ pages/accessibility/a11y.vue
@@ -3,10 +3,7 @@
     <div class="accessibility md">
       <h1>GitLab Accessibility Statement</h1>
       <p>GitLab is committed to maintaining an accessible product to the widest possible audience. We are actively working to increase the accessibility and usability of our product.</p>
-      <p>GitLab is conformant with the <a href="https://gitlab.com/gitlab-org/gitlab-ce/uploads/5a68a95c05dcc0d41bc550ce136421a2/VPAT-GitLab_508_compliance_.pdf">Voluntary Product Accessibility Template (VPAT) 1.3 conducted January 4, 2017</a>. This assessment aligns with the GitLab Enterprise Edition Premium version 8.14. For GitLab.com, this only includes the SaaS product and not other pages found on the GitLab.com domain. We are currently in the process of providing a <a 
-        href="https://gitlab.com/gitlab-org/gitlab-ce/issues/39662" 
-        target="_blank"
-      >new VPAT assessment</a>.</p>
+      <p>GitLab is conformant with the <a href="/usability/vpat">Voluntary Product Accessibility Template (VPAT) 2.2</a> conducted December, 2018. This assessment aligns with the GitLab Enterprise Edition Premium version 11.6. For GitLab.com, this only includes the SaaS product and not other pages found on the GitLab.com domain.</p>
       <p>We aim to conform to level AA of the World Wide Web Consortium (W3C) <a 
         href="https://www.w3.org/TR/WCAG21/" 
         target="_blank"
