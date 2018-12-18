824a6ce73a8b974fba2a02c020510b30aa566871
sarrahvesselov <svesselov@gitlab.com>
2018-12-17 21:13:24 -0500

    add persona information

--- pages/brand-get-started/brand-personas-roles.vue
+++ pages/brand-get-started/brand-personas-roles.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="content limited m-t-7 m-b-7">
     <div class="component md">
-      This documentation has not yet been added
+      <h1>GitLab personas and enterprise IT roles</h1>
+      <h2>Roles vs personas</h2>
+      <p>Personas describe the ideal target for GitLab. They help us define our messaging and marketing delivery. They are theoretical people to target. By defining their concerns and where they go for information, we can best spend our marketing dollars and sales efforts by focusing on this ideal target.</p>
+      <p>Roles are distinct job titles. These are the real people you will encounter while selling. You will find a contact at an account with a specific role. Understanding the challenges faced by each role in IT, along with what they care most about, is helpful to deliver the right value proposition to the right person.</p>
+      <h2>Personas</h2>
+      <p>"Personas" are a generalized way of talking about the people we communicate with and design for. Keeping the personas in mind with their problems and pain points allows us to use language that the personas understands. GitLab has both buyer and user persona types.</p>
+      <h2>Writing for all personas</h2>
+      <ul>
+        <li>When writing content, always remember to use the GitLab voice regardless of the persona.</li>
+        <li>Never alienate other personas. You can appeal to a buyer without buzzword bingo.</li>
+        <li>Look at who uses a channel most (twitter &amp; docs: user, webinar linkedin: buyer) when shared (website) guide them /features for users /solutions for buyers.</li>
+      </ul>
+      <h2>User Personas</h2>
+      <p>User personas are people who actually use GitLab. They may or may not be the person in the organization who has the authority and budget to purchase Gitlab, but they are heavy influencers in the buying process. User personas are kept under <a href="../getting-started/personas">Product > Getting started > Personas</a>.</p>
+        <ul>
+          <li><a href="../getting-started/personas#persona-parker">Product Manager</a></li>
+          <li><a href="../getting-started/personas#persona-delaney">Development Team Lead</a></li>
+          <li><a href="../getting-started/personas#persona-sasha">Software Developer</a></li>
+          <li><a href="../getting-started/personas#persona-devon">DevOps Engineer</a></li>
+          <li><a href="../getting-started/personas#persona-sidney">Systems Administrator</a></li>
+          <li><a href="../getting-started/personas#persona-sam">Security analyst (uses the Security Dashboard)</a></li>
+        </ul>
     </div>
   </div>
 </template>
