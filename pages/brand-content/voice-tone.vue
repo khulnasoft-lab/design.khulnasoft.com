12f51cb2b4663cefb6115bf6080b8c84e73dd5e3
Sarrah Vesselov <svesselov@gitlab.com>
2018-12-19 02:02:01 +0000

    Adding branding docs: Personality, Logo, Voice & tone

---
+++ pages/brand-content/voice-tone.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="content limited m-t-7 m-b-7">
+    <div class="component md">
+      <h1>Voice and tone</h1>
+       <p>The following guide outlines the set of standards used for all written company communications to ensure consistency in voice, style, and personality across all of GitLab's public communications.</p>
+       <p>See <a href="https://about.gitlab.com/handbook/marketing/corporate-marketing/#general-editorial-style-guidelines" target="_blank">General editorial</a> style guidelines for more.</p>
+       <h2>About</h2>
+       <h3>GitLab the community</h3>
+       <p>GitLab is an <a href="https://gitlab.com/gitlab-org/gitlab-ce/" target="_blank">open source project</a> with a large community of contributors. Over 2,000 people worldwide have contributed to GitLab's source code.</p>
+       <h3>GitLab the company</h3>
+       <p>GitLab Inc. is a company based on the GitLab open source project. GitLab Inc. is an active participant in our community (see our <a href="https://about.gitlab.com/stewardship" target="_blank">stewardship of GitLab CE</a> for more information), as well as offering GitLab, a product (see below).</p>
+       <h3>GitLab the product</h3>
+       <p>GitLab is a single application for the complete DevOps lifecycle. See the <a href="https://about.gitlab.com/handbook/marketing/product-marketing/#elevator-pitch" target="_blank">product elevator pitch</a> for additional messaging.</p>
+       <h2>Tone of Voice</h2>
+       <p>The tone of voice we use when speaking as GitLab should always be informed by our <a href="https://gitlab.com/gitlab-com/marketing/general/blob/master/content/content-strategy.md#strategy" target="_blank">Content Strategy</a>. Most importantly, we see our audience as co-conspirators, working together to define and create the next generation of software development practices. The below table should help to clarify further:</p>
+       <table class="do-dont m-b-6">
+        <thead>
+          <tr>
+            <th>We are</th>
+            <th>We aren't</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <p>Equals in our community</p>
+            </td>
+            <td>
+              <p>Superior</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Knowledgable</p>
+            </td>
+            <td>
+              <p>Know-it-alls</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Empathetic</p>
+            </td>
+            <td>
+              <p>Patronizing</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Straightforward</p>
+            </td>
+            <td>
+              <p>Verbose</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Irreverent</p>
+            </td>
+            <td>
+              <p>Disrespectful</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Playful</p>
+            </td>
+            <td>
+              <p>Jokey</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Helpful</p>
+            </td>
+            <td>
+              <p>Dictatorial</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Transparent</p>
+            </td>
+            <td>
+              <p>Opaque</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <p>We explain things in the simplest way possible, using plain, accessible language.</p>
+      <p>We keep a sense of humor about things, but don't make light of serious issues or problems our users or customers face.</p>
+      <p>We use colloquialisms and slang, but sparingly (don't look like you're trying too hard!).</p>
+      <p>We use <a href="https://litreactor.com/columns/5-easy-ways-to-make-your-writing-gender-neutral" target="_blank">inclusive, gender-neutral language</a>.</p>
+    </div>
+  </div>
+</template>
