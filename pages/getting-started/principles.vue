8b8cce746aa0a1a3ca5d5a34c77ce71f6bf0fe75
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-12-19 18:19:05 +0100

    add vue template html

--- pages/getting-started/principles.vue
+++ pages/getting-started/principles.vue
@@ -1,60 +1,66 @@
-<h2>About our Principles</h2>
-<p>Principles act as a reusable standard for teams to measure their work. They replace subjective ideals with a shared understanding of what the result must do for users. Just as guardrails keep you safe and on the road, principles keep teams on the path to achieving their vision. As it creates alignment, it enables us to scale and build velocity.</p>
-<p>There are two kinds of principles; The principles that guide the process and principles that define the output. The process principles are described in our <a href="https://about.gitlab.com/handbook/product/#product-principles">handbook</a>, while the outcome principles are detailed here.</p>
-<p>Each principle has been assigned a hierarchy position to resolve confusion about what to do in a specific circumstance while remaining consistent.</p>
-<p>We take inspiration from other companies and look inwards to our work as to where our principles are best actionable or to be improved to be most effective. Just like the rest of our work, we continually adjust our principles and strive always to make them better. Our principle guidelines are defined at the bottom of this , so everyone is welcome to suggest improvements!</p>
-<h2>Productive (1)</h2>
-<p>GitLab is an application to support what people do, day in, day out. We need to respect the importance of their work and avoid gimicky details.</p>
-<ol>
-  <li><strong>Bold sub principle(s) title</strong> Explanation, plus optional reference</li>
-</ol>
-<ul>
-  <li>Example(s), plus mandatory reference</li>
-</ul>
-<h2>Minimal (2)</h2>
-<p>While work can get complicated, GitLab is about bringing a sharp focus, helping our customers know what matters now.</p>
-<ol>
-  <li><strong>Bold sub principle(s) title</strong> Explanation, plus optional reference</li>
-</ol>
-<ul>
-  <li>Example(s), plus mandatory reference</li>
-</ul>
-<h2>Human (3)</h2>
-<p>We need to build empathy with our users, understand their state of mind, and connect with them on a human level.</p>
-<ol>
-  <li><strong>Bold sub principle(s) title</strong> Explanation, plus optional reference</li>
-</ol>
-<ul>
-  <li>Example(s), plus mandatory reference</li>
-</ul>
-<h2>Principle guidelines</h2>
-<p>The principles defined should align to the following guidelines to assure they are to be most effective.</p>
-<h3>Origin and purpose</h3>
-<p>Purpose and values inform principles, while principles inform the product. Make sure that no conflicting principles are introduced.</p>
-<p>Principles should either inform the process or the desired output. Those that define processes are described in our <a href="https://about.gitlab.com/handbook/product/#product-principles">handbook</a>, while the outcome principles are detailed here.</p>
-<h3>Target audience</h3>
-<p>Different teams require different principles to guide them, while some principles might be useful to more than one. It is important that the target audience is clear to avoid confusion while considering them when making decisions.</p>
-<h3>Actionable and genuine</h3>
-<p>Principles should be actionable and thus strike a balance between being too generic and too specific. Additionally, for them to be genuine and have context, they are required to have real demonstration examples.</p>
-<h3>Give direction by being opinionated</h3>
-<p>When principles have a point of view, they give direction and keep teams on the path to achieving their vision. It defines a decision where other companies could have diverged. By contrast, it is less useful to state things in principles that are logical and wished upon by default. An example of this would be “Useful,” of which the opposite would be “not useful.” Slim are the chances a company wants to create such a product and obvious the fact that a company wants to create a useful product.</p>
-<h3>Explicit hierarchy</h3>
-<p>Each principle should be assigned a hierarchy position to resolve confusion about what to do in a specific
-  circumstance while remaining consistent.</p>
-<h3>Memorable</h3>
-<p>Similar to the <a href="https://about.gitlab.com/handbook/values/#about-our-values">GitLab Company Values</a>, principles are most effective when they are easy to remember. Because of this, the number of principles should be kept low, and there is the possibility to add sub-principles. Three to five principles are ideal. Anything that makes them even more memorable is helpful.</p>
-<h3>Format</h3>
-<pre><code>
-## Principle title (Number of hierarchy position)
+<template>
+  <div class="content limited m-t-7 m-b-7">
+    <div class="md">
+      <h2>About our Principles</h2>
+      <p>Principles act as a reusable standard for teams to measure their work. They replace subjective ideals with a shared understanding of what the result must do for users. Just as guardrails keep you safe and on the road, principles keep teams on the path to achieving their vision. As it creates alignment, it enables us to scale and build velocity.</p>
+      <p>There are two kinds of principles; The principles that guide the process and principles that define the output. The process principles are described in our <a href="https://about.gitlab.com/handbook/product/#product-principles">handbook</a>, while the outcome principles are detailed here.</p>
+      <p>Each principle has been assigned a hierarchy position to resolve confusion about what to do in a specific circumstance while remaining consistent.</p>
+      <p>We take inspiration from other companies and look inwards to our work as to where our principles are best actionable or to be improved to be most effective. Just like the rest of our work, we continually adjust our principles and strive always to make them better. Our principle guidelines are defined at the bottom of this , so everyone is welcome to suggest improvements!</p>
+      <h2>Productive (1)</h2>
+      <p>GitLab is an application to support what people do, day in, day out. We need to respect the importance of their work and avoid gimicky details.</p>
+      <ol>
+        <li><strong>Bold sub principle(s) title</strong> Explanation, plus optional reference</li>
+      </ol>
+      <ul>
+        <li>Example(s), plus mandatory reference</li>
+      </ul>
+      <h2>Minimal (2)</h2>
+      <p>While work can get complicated, GitLab is about bringing a sharp focus, helping our customers know what matters now.</p>
+      <ol>
+        <li><strong>Bold sub principle(s) title</strong> Explanation, plus optional reference</li>
+      </ol>
+      <ul>
+        <li>Example(s), plus mandatory reference</li>
+      </ul>
+      <h2>Human (3)</h2>
+      <p>We need to build empathy with our users, understand their state of mind, and connect with them on a human level.</p>
+      <ol>
+        <li><strong>Bold sub principle(s) title</strong> Explanation, plus optional reference</li>
+      </ol>
+      <ul>
+        <li>Example(s), plus mandatory reference</li>
+      </ul>
+      <h2>Principle guidelines</h2>
+      <p>The principles defined should align to the following guidelines to assure they are to be most effective.</p>
+      <h3>Origin and purpose</h3>
+      <p>Purpose and values inform principles, while principles inform the product. Make sure that no conflicting principles are introduced.</p>
+      <p>Principles should either inform the process or the desired output. Those that define processes are described in our <a href="https://about.gitlab.com/handbook/product/#product-principles">handbook</a>, while the outcome principles are detailed here.</p>
+      <h3>Target audience</h3>
+      <p>Different teams require different principles to guide them, while some principles might be useful to more than one. It is important that the target audience is clear to avoid confusion while considering them when making decisions.</p>
+      <h3>Actionable and genuine</h3>
+      <p>Principles should be actionable and thus strike a balance between being too generic and too specific. Additionally, for them to be genuine and have context, they are required to have real demonstration examples.</p>
+      <h3>Give direction by being opinionated</h3>
+      <p>When principles have a point of view, they give direction and keep teams on the path to achieving their vision. It defines a decision where other companies could have diverged. By contrast, it is less useful to state things in principles that are logical and wished upon by default. An example of this would be “Useful,” of which the opposite would be “not useful.” Slim are the chances a company wants to create such a product and obvious the fact that a company wants to create a useful product.</p>
+      <h3>Explicit hierarchy</h3>
+      <p>Each principle should be assigned a hierarchy position to resolve confusion about what to do in a specific
+        circumstance while remaining consistent.</p>
+      <h3>Memorable</h3>
+      <p>Similar to the <a href="https://about.gitlab.com/handbook/values/#about-our-values">GitLab Company Values</a>, principles are most effective when they are easy to remember. Because of this, the number of principles should be kept low, and there is the possibility to add sub-principles. Three to five principles are ideal. Anything that makes them even more memorable is helpful.</p>
+      <h3>Format</h3>
+      <pre><code>
+      ## Principle title (Number of hierarchy position)
 
-Intro stating why, how, and what of the principle.
+      Intro stating why, how, and what of the principle.
 
-1. **Bold sub principle(s) title** Explanation, plus optional reference
-  - Example(s), plus mandatory reference
-</code></pre>
-<h2>References</h2>
-<ul>
-  <li><a href="https://www.designbetter.co/design-systems-handbook/expanding-design-system">Design systems handbook - Design better</a></li>
-  <li><a href="https://speakerdeck.com/craftui/from-purpose-to-patterns">From purpose to patterns - Alla Kholmatova</a></li>
-  <li><a href="https://principles.design/#what-are-design-principles">Design principles</a></li>
-</ul>
+      1. **Bold sub principle(s) title** Explanation, plus optional reference
+        - Example(s), plus mandatory reference
+      </code></pre>
+      <h2>References</h2>
+      <ul>
+        <li><a href="https://www.designbetter.co/design-systems-handbook/expanding-design-system">Design systems handbook - Design better</a></li>
+        <li><a href="https://speakerdeck.com/craftui/from-purpose-to-patterns">From purpose to patterns - Alla Kholmatova</a></li>
+        <li><a href="https://principles.design/#what-are-design-principles">Design principles</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
