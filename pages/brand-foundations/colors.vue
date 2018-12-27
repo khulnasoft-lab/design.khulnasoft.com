1eaa64f6a2f6f6c76087cb622cd671eca1bb3f31
Sarrah Vesselov <svesselov@gitlab.com>
2018-12-27 17:41:47 +0000

    add color documentation from gitlab marketing handbook

--- pages/brand-foundations/colors.vue
+++ pages/brand-foundations/colors.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="content limited m-t-7 m-b-7">
     <div class="component md">
-      This documentation has not yet been added
+      <h1>Colors</h1>
+      <p>While the brand is ever-evolving, the GitLab brand currently consists of six primary colors that are used in a wide array of marketing materials.</p>
+      <img 
+        src="~/assets/images/brand/gitlab-hex-rgb-colors.png" alt="GitLab brand colors" 
+        width="600px"
+      />
     </div>
   </div>
 </template>
