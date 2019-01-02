abfbef598ab69ecc422bb29baac6d854b7a4bcf0
Jarek Ostrowski <jarek.j.ostrowski@gmail.com>
2019-01-02 14:12:01 -0500

    Add title

--- pages/brand-foundations/buttons.vue
+++ pages/brand-foundations/buttons.vue
@@ -8,8 +8,8 @@
       <p class="todo">Todo:  Add an example of a primary button</p>
       <h2>Secondary buttons</h2>
       <p>There will be times when two buttons are needed. This will be in places such as <a href="https://about.gitlab.com/jobs/" target="_blank">our jobs page</a>, where we have a button to view opportunities and one to view our culture video. In this example, both buttons are solid, but one is considered the primary button (orange), and the other is the secondary button (white). The CSS class for the solid white button is <code>.btn.cta-btn.btn-white</code>.</p>
-      <img 
-        src="~/assets/images/brand/jobs-buttons-example.png" alt="primary and secondary button example" 
+      <img
+        src="~/assets/images/brand/jobs-buttons-example.png" alt="primary and secondary button example"
         width="600px"
       />
       <p>This is the proper use of two buttons, both being solid, but different colors based on hierarchy. If the background is white or a lighter color that doesn't contrast well with a white-backgound button, a ghost button should be used as a secondary button, and should match in color to the primary button beside it as shown below:</p>
@@ -17,6 +17,7 @@
       <p>DO NOT: Do not use these ghost buttons styles as standalone buttons. They have been proven to be less effective than solid buttons <a href="https://conversionxl.com/blog/ghost-buttons/" target="_blank">in a number of studies</a>. They should only be used as a secondary button, next to a solid primary button that already exists. Here are the classes for the secondary buttons:</p>
       <p class="todo">Todo:  Add an example of secondary button one</p>
       <p class="todo">Todo:  Add an example of secondary button two</p>
+      <h2>Call-to-action (CTA) links</h2>
     </div>
   </div>
 </template>
