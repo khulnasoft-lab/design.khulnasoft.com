699a830dc72c2ba33fe8cc5dfbfd8e94fea5164c
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2019-02-14 11:58:59 +0100

    moved contribute page to dedicated section + created menu section component

---
+++ components/menu_section.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul 
+    :open="$route.fullPath.startsWith(`/${category}`)"
+    class="nav-sidebar__section"
+  >
+    <span class="nav-sidebar__section-title">{{ categoryName || (category.charAt(0).toUpperCase() + category.slice(1)) }}</span>
+    <slot></slot>
+    <nuxt-link
+      v-for="page in contentTree[category]"
+      :key="page.id"
+      :to="`/${category}/${page.id}`"
+      class="nav-sidebar__section-items-anchor nav-sidebar--indent-2"
+    >
+      {{ page.name }}
+    </nuxt-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    contentTree: {
+      type: Object,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    categoryName: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+};
+</script>
--- pages/contribute/get-started.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="content limited m-t-7 m-b-7">
-    <div class="component md">
-      To do
-    </div>
-  </div>
-</template>
