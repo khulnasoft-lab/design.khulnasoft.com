0d5ed1b92840f9383f03427ee4373bea159ce778
Lukas Eipert <leipert@gitlab.com>
2019-02-17 23:07:31 +0100

    Update gitlab ui to version 2

--- plugins/gitlab-ui.js
+++ plugins/gitlab-ui.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import * as gitlabComponents from '@gitlab/ui';
+import * as gitlabCharts from '@gitlab/ui/dist/charts';
+
+import GlExampleExplorer from '@gitlab/ui/documentation/components/example_explorer.vue';
+import GlExampleDisplay from '@gitlab/ui/documentation/components/example_display.vue';
 
 Vue.use(BootstrapVue);
 
-Object.entries(gitlabComponents)
+Object.entries({ ...gitlabComponents, ...gitlabCharts, GlExampleExplorer, GlExampleDisplay })
   .filter(([componentName]) => !componentName.includes('Directive'))
   .forEach(([componentName, component]) => Vue.component(componentName, component));
