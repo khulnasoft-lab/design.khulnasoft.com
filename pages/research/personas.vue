f9d81044e72f3a199a0c0ed8f3a04c9f9a2068f0
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2019-02-26 13:44:56 +0100

    Moved persona information to handbook and converted research pages to markdown
    Fix menu item padding
    
    hardcoded icon color as discussed in mr 254
    
    isolate external page icon to sidebar for now
    
    Adding new lines
    
    changed css to be in line with our spacing variables

--- pages/research/personas.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="personas content limited m-t-7 m-b-7">
-    <div class="md">
-      <h1>Personas</h1>
-      <p>This page has been moved to the <a href="https://about.gitlab.com/handbook/marketing/product-marketing/roles-personas/" target="_blank" rel="noopener">handbook</a></p>
-    </div>
-  </div>
-</template>
