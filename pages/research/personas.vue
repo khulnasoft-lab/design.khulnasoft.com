2f25a6238a84dca8b1102418753cfd22bb376df4
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2019-02-26 13:44:56 +0100

    Moved persona information to handbook and converted research pages to markdown

--- pages/research/personas.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="personas content limited m-t-7 m-b-7">
-    <div class="md">
-      <h1>Personas</h1>
-      <p>This page has been moved to the <a href="https://about.gitlab.com/handbook/marketing/product-marketing/roles-personas/" target="_blank" rel="noopener" class="nav-sidebar__section-items-anchor nav-sidebar--indent-2">handbook</a></p>
-    </div>
-  </div>
-</template>
