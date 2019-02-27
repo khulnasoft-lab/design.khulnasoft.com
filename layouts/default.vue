00b34880986630fad2a2108c919e1fcbb83343a8
Dimitrie Hoekstra <dimitrie@gitlab.com>
2019-02-27 11:12:04 +0000

    Adds no rel
    [ci skip]

--- layouts/default.vue
+++ layouts/default.vue
@@ -157,10 +157,10 @@
           </ul>
           <menu-section :content-tree="contentTree" category="research">
             <li>
-              <a href="https://about.gitlab.com/community/gitlab-first-look/" target="_blank" class="nav-sidebar__section-items-anchor nav-sidebar--indent-2">First Look</a>
+              <a href="https://about.gitlab.com/community/gitlab-first-look/" target="_blank" rel="noopener" class="nav-sidebar__section-items-anchor nav-sidebar--indent-2">First Look</a>
             </li>
             <li>
-              <a href="https://about.gitlab.com/handbook/marketing/product-marketing/roles-personas/" target="_blank" class="nav-sidebar__section-items-anchor nav-sidebar--indent-2">Personas</a>
+              <a href="https://about.gitlab.com/handbook/marketing/product-marketing/roles-personas/" target="_blank" rel="noopener" class="nav-sidebar__section-items-anchor nav-sidebar--indent-2">Personas</a>
             </li>
           </menu-section>
           <ul 
