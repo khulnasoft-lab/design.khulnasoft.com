7c9bbc5753fa0d03f103ef3c955de5b338080851
Dimitrie Hoekstra <dimitrie@gitlab.com>
2019-03-14 16:43:45 +0000

    Remove unneeded html for persona page

--- pages/research/personas.vue
+++ pages/research/personas.vue
@@ -2,7 +2,7 @@
   <div class="personas content limited m-t-7 m-b-7">
     <div class="md">
       <h1>Personas</h1>
-      <p>This page has been moved to the <a href="https://about.gitlab.com/handbook/marketing/product-marketing/roles-personas/" target="_blank" rel="noopener" class="nav-sidebar__section-items-anchor nav-sidebar--indent-2">handbook</a></p>
+      <p>This page has been moved to the <a href="https://about.gitlab.com/handbook/marketing/product-marketing/roles-personas/" target="_blank" rel="noopener">handbook</a></p>
     </div>
   </div>
 </template>
