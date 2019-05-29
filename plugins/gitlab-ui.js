3a2ca0e7d502611a0be37412506ba01e9c8c8cdd
Jacques Erasmus <jerasmus@gitlab.com>
2019-05-29 13:14:43 +0000

    Add toast examples
    Added toast examples to the toast markdown

--- plugins/gitlab-ui.js
+++ plugins/gitlab-ui.js
@@ -7,6 +7,7 @@ import GlExampleExplorer from '@gitlab/ui/documentation/components/example_explo
 import GlExampleDisplay from '@gitlab/ui/documentation/components/example_display.vue';
 
 Vue.use(BootstrapVue);
+Vue.use(gitlabComponents.GlToast); // The toast plugin needs to be registered before being used
 
 Object.entries({ ...gitlabComponents, ...gitlabCharts, GlExampleExplorer, GlExampleDisplay })
   .filter(([componentName]) => !componentName.includes('Directive'))
