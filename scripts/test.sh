4ab53284db47e2a2bb8dec3e3a6550533cdbf47c
Dimitrie Hoekstra <dimitrie@gitlab.com>
2019-07-04 14:12:44 +0000

    Create review stage before test stage and introduce test for  empty line on end of file for markdown files

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,8 +3,24 @@
 #
 # We moved this to a small script wrapper, so that this compatible with Auto DevOps
 #
+
 echo "Linting files with eslint"
 yarn run eslint --ext .js,.vue --ignore-path .gitignore .
 
 echo "Linting files with prettier"
 yarn run prettier --list-different --ignore-path .gitignore "**/*.{js,vue}"
+
+echo "Ensuring that markdown files end with an empty line"
+
+ERROR=false
+
+while IFS= read -r -d '' file; do
+  if test "$(tail -c 1 "$file" | wc -l)" -eq 0; then
+    echo -e "\tError: No new line at end of $file"
+    ERROR=true
+  fi
+done < <(git ls-files -z -- '*.md')
+
+if [[ "$ERROR" == "true" ]]; then
+  exit 1
+fi
