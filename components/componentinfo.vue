bccb3845dc362c16df3096a6e6c7619520b62dff
Simon Knox <psimyn@gmail.com>
2019-07-05 20:47:22 +1000

    Allow full links in Related section of frontmatter

--- components/componentinfo.vue
+++ components/componentinfo.vue
@@ -19,7 +19,7 @@
             <md-display :md="componentBody" />
             <div class="m-t-6">
               <div class="md">
-                <h2 id="related-patterns">Related patterns</h2>
+                <h2 id="related-patterns">Related</h2>
                 <ul v-if="hasRelatedPatterns">
                   <li v-for="pattern in relatedPatterns" :key="pattern.slug">
                     <a :href="pattern.url">{{ pattern.label }}</a>
@@ -138,11 +138,22 @@ export default {
       return this.relatedPatterns && this.relatedPatterns.length > 0;
     },
     relatedPatterns() {
-      return (this.related || []).map(slug => ({
-        slug,
-        url: `/components/${slug}`,
-        label: (slug.charAt(0).toLocaleUpperCase() + slug.substring(1)).split('-').join(' '),
-      }));
+      return (this.related || []).map(item => {
+        let slug;
+        let url;
+        if (item.startsWith('/')) {
+          url = item;
+          slug = item.split('/').pop();
+        } else {
+          slug = item;
+          url = `/components/${slug}`;
+        }
+        return {
+          slug,
+          url,
+          label: (slug.charAt(0).toLocaleUpperCase() + slug.substring(1)).split('-').join(' '),
+        };
+      });
     },
   },
   created() {
