b36dcabee2261875dbe051aa013362cdabc4b995
Pedro Moreira da Silva <hi@pedroms.com>
2019-08-30 17:46:21 +0100

    Update link to pattern library
    - Use the term “Sketch library” to encompass both the instance sheet and the pattern library files
    - Link to the Sketch library documentation instead of the pattern library file

--- pages/index.vue
+++ pages/index.vue
@@ -91,7 +91,7 @@
             View repository
           </a>
           <a
-            href="https://gitlab.com/gitlab-org/gitlab-design/blob/master/gitlab-pattern-library.sketch"
+            href="https://gitlab.com/gitlab-org/gitlab-design/blob/master/doc/pattern-library.md#files"
             target="_blank"
             class="card card--highlighted card--link text-center m-l-5 p-a-4"
           >
@@ -100,7 +100,7 @@
               srcset="../assets/images/resources/r-sketch@2x.png 2x"
               class="a-center d-block m-b-3"
             />
-            Download pattern library
+            Download Sketch library
           </a>
           <a
             href="https://about.gitlab.com/community/gitlab-first-look/"
