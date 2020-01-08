398014add271cb8c0b529ab80a7649b935fa0b98
Dimitrie Hoekstra <dimitrie@gitlab.com>
2020-01-08 18:10:14 +0000

    Apply suggestion to pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -90,7 +90,7 @@
               target="_blank"
               class="text-center"
             >
-              View the project issue tracker
+              Issue tracker
             </a>
           </div>
           <div class="col col-33 d-flex flex-column align-items-center justify-content-center">
