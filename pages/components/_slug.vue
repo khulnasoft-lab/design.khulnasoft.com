47228abb4ac2495f9b65de211fad19a0bd34340b
Justin Ho <hduong@gitlab.com>
2020-02-04 12:10:31 +0700

    Use normal component instead of dynamic

--- pages/components/_slug.vue
+++ pages/components/_slug.vue
@@ -1,42 +1,32 @@
 <template>
   <div class="content limited m-t-7 m-b-8">
-    <div v-if="component">
-      <component
-        :is="component"
-        :frontmatter-info="fmResult"
-      />
-    </div>
-    <div v-else>
-      Loading component ...
-    </div>
+    <ComponentInfo
+      :frontmatter-info="fmResult"
+    />
   </div>
 </template>
 
 <script>
-import component from '../../components/componentinfo.vue';
+import ComponentInfo from '../../components/componentinfo.vue';
 
 export default {
-  data() {
-    return {
-      component,
-      fmResult: {
-        attributes: {}
-      },
-    };
+  components: {
+    ComponentInfo
   },
   editThisPage: {
     resolve: ({ route }) => `contents/components/${route.params.slug}.md`,
   },
   async asyncData({ $axios, route }) {
-    await $axios.$get(`/contents/components/${route.params.slug}.json`)
-      // eslint-disable-next-line promise/always-return
-      .then(fmResult => {
-        this.fmResult = fmResult;
-      })
-      .catch(e => {
-        // eslint-disable-next-line
-        console.log('Err : ', e);
-      });
+    try {
+      const fmResult = await $axios.$get(`/contents/components/${route.params.slug}.json`)
+
+      return {
+        fmResult
+      }
+    } catch (e) {
+      console.log('Err : ', e);
+      return {}
+    }
   }
 };
 </script>
