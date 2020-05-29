4ce4d14ca6030a504cb2a7a2b9920ea72dffe060
Simon Knox <simon@gitlab.com>
2020-05-29 13:04:09 +1000

    Add burger, fix title padding
    Indent title to make room for the menu button

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,7 +16,7 @@ export default {
   },
   computed: {
     contentWrapper() {
-      return this.$route.fullPath === '/' ? '' : 'content';
+      return this.$route.fullPath === '/' ? '' : 'content main';
     },
   },
   mounted() {
@@ -37,17 +37,9 @@ export default {
 <template>
   <div class="page">
     <button class="nav-sidebar__toggle" @click="open = !open">
-      <svg height="36" viewBox="0 0 376 342" xmlns="http://www.w3.org/2000/svg" class="tanuki-logo">
+      <svg width="32" height="32" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <title>Toggle menu</title>
-        <g fill="none" fill-rule="evenodd">
-          <path d="M185.398 341.13l68.013-209.322H117.39L185.4 341.13z" fill="#E24329" class="tanuki-shape tanuki-left-ear"/>
-          <path d="M185.398 341.13l-68.013-209.322h-95.32L185.4 341.128z" fill="#FC6D26" class="tanuki-shape tanuki-left-eye"/>
-          <path d="M22.066 131.808l-20.67 63.61c-1.884 5.803.18 12.16 5.117 15.744L185.398 341.13 22.066 131.807z" fill="#FCA326" class="tanuki-shape tanuki-left-cheek"/>
-          <path d="M22.066 131.808h95.32L76.42 5.735c-2.107-6.487-11.284-6.487-13.39 0L22.065 131.808z" fill="#E24329" class="tanuki-shape tanuki-nose"/>
-          <path d="M185.398 341.13l68.013-209.322h95.32L185.4 341.128z" fill="#FC6D26" class="tanuki-shape tanuki-right-eye"/>
-          <path d="M348.73 131.808l20.67 63.61c1.884 5.803-.18 12.16-5.117 15.744L185.398 341.13 348.73 131.807z" fill="#FCA326" class="tanuki-shape tanuki-right-cheek"/>
-          <path d="M348.73 131.808h-95.32L294.376 5.735c2.108-6.487 11.285-6.487 13.392 0l40.963 126.073z" fill="#E24329" class="tanuki-shape tanuki-right-ear"/>
-        </g>
+        <path fill="#333" d="M2 3h12a1 1 0 0 1 0 2H2a1 1 0 1 1 0-2zm0 4h12a1 1 0 0 1 0 2H2a1 1 0 1 1 0-2zm0 4h12a1 1 0 0 1 0 2H2a1 1 0 0 1 0-2z" />
       </svg>
     </button>
     <nav :class="{ 'nav-sidebar--open': open }" class="nav-sidebar">
