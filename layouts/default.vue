b5d8bab2eca982b0450bdbca3682d53361778f18
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2020-07-23 16:32:30 -0400

    Bind aria-expanded to open property

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,7 +37,7 @@ export default {
 <template>
   <div class="page">
     <div v-if="open" class="nav-sidebar__overlay" @click="open = false"></div>
-    <button type="button" class="nav-sidebar__toggle" :aria-expanded="show" @click="open = !open">
+    <button type="button" class="nav-sidebar__toggle" :aria-expanded="open" @click="open = !open">
       <svg width="32" height="32" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <title>Toggle menu</title>
         <path d="M2 3h12a1 1 0 0 1 0 2H2a1 1 0 1 1 0-2zm0 4h12a1 1 0 0 1 0 2H2a1 1 0 1 1 0-2zm0 4h12a1 1 0 0 1 0 2H2a1 1 0 0 1 0-2z" />
