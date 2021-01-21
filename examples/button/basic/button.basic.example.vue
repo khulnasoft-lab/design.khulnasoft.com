36213352dfbc50e5db2304982760981025eef001
Jarek Ostrowski <jarek.j.ostrowski@gmail.com>
2021-01-21 16:40:05 -0500

    feat(GlButton): Update button variants and add deprecated variants
    BREAKING CHANGE: Updates button variants docs and gitlab-ui examples.
    This change deprecates info, success, and warning button variants
    and adds the confirm variant.

--- examples/button/basic/button.basic.example.vue
+++ examples/button/basic/button.basic.example.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <gl-button>Default</gl-button>
-    <gl-button category="primary" variant="success">Primary Success</gl-button>
-    <gl-button category="secondary" variant="success">Secondary Success</gl-button>
+    <gl-button category="primary" variant="confirm">Primary confirm</gl-button>
+    <gl-button category="secondary" variant="confirm">Secondary confirm</gl-button>
+    <gl-button category="tertiary" variant="confirm">Tertiary confirm</gl-button>
   </div>
 </template>
