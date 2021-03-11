63803718d67eaaf24b407e60ceda93608e6e2568
Jarek Ostrowski <jarek.j.ostrowski@gmail.com>
2021-03-11 09:54:32 -0500

    docs(GlTable): Add docs and example for sortable table

--- examples/table/index.js
+++ examples/table/index.js
@@ -1,5 +1,6 @@
 import component from './table.basic.example.vue';
 import componentCustomFields from './table.custom_fields.example.vue';
+import componentSortable from './table.sortable.example.vue';
 
 export default [
   {
@@ -16,6 +17,12 @@ export default [
         name: 'Custom Fields',
         description: 'Custom component fields',
         component: componentCustomFields,
+      },
+      {
+        id: 'table-sortable',
+        name: 'Sortable table',
+        description: 'Sortable table columns',
+        component: componentSortable,
       },
     ],
   },
