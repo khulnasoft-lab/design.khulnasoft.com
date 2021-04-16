8f0ed0de40e99170da79ae13cead770b75caf533
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2021-04-16 13:23:27 -0400

    chore: add basic search
    Add a basic search input based on Nuxt's lunr-module

---
+++ components/search/search_result.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  props: {
+    meta: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <gl-dropdown-item :to="meta.route" v-on="$listeners">
+    {{ meta.title }}
+  </gl-dropdown-item>
+</template>
