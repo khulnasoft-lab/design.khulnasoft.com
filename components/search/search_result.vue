1b96849da57b6cc443f8495e17530a26d1e5572c
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2021-04-20 10:58:36 -0400

    chore: fix down arrow event and improve UX

--- components/search/search_result.vue
+++ components/search/search_result.vue
@@ -10,7 +10,7 @@ export default {
 </script>
 
 <template>
-  <gl-dropdown-item :to="meta.route" class="lunr-result" v-on="$listeners">
+  <gl-dropdown-item :to="meta.route" v-on="$listeners">
     {{ meta.title }}
   </gl-dropdown-item>
 </template>
