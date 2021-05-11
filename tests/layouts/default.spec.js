cfdaabc152595b6120b23fee93d543e0018e72b9
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2021-05-11 08:14:28 -0400

    test: test navbar structure
    Adds snapshot test to ensure that the navbar will look similar after the
    refactor.

---
+++ tests/layouts/default.spec.js
@@ -0,0 +1,43 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import DefaultLayout from '../../layouts/default.vue';
+import { state } from '../../store';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('default layout', () => {
+  let wrapper;
+  let store;
+
+  const createComponent = () => {
+    store = new Vuex.Store({
+      state: state(),
+    });
+    wrapper = mount(DefaultLayout, {
+      localVue,
+      store,
+      stubs: {
+        'nuxt-link': true,
+        nuxt: true,
+        'edit-this-page-link': true,
+      },
+      mocks: {
+        $route: {
+          fullPath: '/',
+          params: {
+            section: '',
+          },
+        },
+      },
+    });
+  };
+
+  beforeEach(() => {
+    createComponent();
+  });
+
+  it('renders sidebar properly', () => {
+    expect(wrapper.find('nav').element).toMatchSnapshot();
+  });
+});
