288edb274d0d166f46afe67d5c5784af445be860
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2021-05-13 07:19:19 -0400

    chore: do not select fist subitem automatically
    Prevents the first subitem from being automatically selected when
    expanding a menu item that has children.

--- components/sub_menu.vue
+++ components/sub_menu.vue
@@ -20,35 +20,13 @@ export default {
       default: '',
     },
   },
-  methods: {
-    selectFirstSubItem(ev) {
-      const { target } = ev;
-      if (target && target.parentNode) {
-        const detailsLinks = target.parentNode.querySelectorAll('a');
-        if (detailsLinks.length > 0) {
-          const firstLink = detailsLinks[0].getAttribute('href');
-          if (firstLink) {
-            const that = this;
-            setTimeout(() => {
-              that.$router.push({
-                path: firstLink.replace(/#/, ''),
-              });
-            }, 1);
-          }
-        }
-      }
-    },
-  },
 };
 </script>
 
 <template>
   <li>
     <details :open="$route.fullPath.startsWith(`/${category}`)" class="nav-sidebar__section">
-      <summary
-        class="nav-sidebar__section-summary nav-sidebar--indent-1"
-        @click="selectFirstSubItem"
-      >
+      <summary class="nav-sidebar__section-summary nav-sidebar--indent-1">
         {{ categoryName || category.charAt(0).toUpperCase() + category.slice(1) }}
       </summary>
       <slot></slot>
