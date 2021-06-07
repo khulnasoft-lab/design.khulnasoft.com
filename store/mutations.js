2a660efc2caf869777d0c76ae304ef7e31d897d4
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2021-06-07 15:55:46 -0400

    refactor: drop actions in favor of composable mutations

--- store/mutations.js
+++ store/mutations.js
@@ -1,13 +1,27 @@
 import { getDefaultFrontmatter } from './state';
 
-export default {
-  resetFrontmatter(state) {
-    state.frontmatter = getDefaultFrontmatter();
-  },
-  setFrontmatter(state, frontmatter) {
-    state.frontmatter = frontmatter;
-  },
-  setActiveNavItem(state, navItem) {
-    state.activeNavItem = navItem;
-  },
+const resetFrontmatter = (state) => {
+  state.frontmatter = getDefaultFrontmatter();
 };
+
+const setFrontmatter = (state, frontmatter) => {
+  state.frontmatter = frontmatter;
+};
+
+const setActiveNavItem = (state, navItem) => {
+  state.activeNavItem = navItem;
+};
+
+const toggleActiveNavItem = (state, navItem) => {
+  if (!navItem) {
+    setActiveNavItem(state, '');
+  }
+
+  if (state.activeNavItem.startsWith(navItem)) {
+    setActiveNavItem(state, '');
+  } else {
+    setActiveNavItem(state, navItem);
+  }
+};
+
+export default { resetFrontmatter, setFrontmatter, setActiveNavItem, toggleActiveNavItem };
