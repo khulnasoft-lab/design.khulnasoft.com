c9293c2c893c1c9947cd3d19612d18c06760c207
Jarek Ostrowski <jostrowski@gitlab.com>
2021-08-30 11:07:51 -0400

    feat(Components): Add overview page for components
    Adds overview page of components with new frontmatter
    descriptions.
    
    MR: https://gitlab.com/gitlab-org/gitlab-services/design.gitlab.com/-/merge_requests/2517

---
+++ pages/components/overview.vue
@@ -0,0 +1,35 @@
+<script>
+import contentTree from '../../content_tree.json'; // eslint-disable-line import/no-unresolved
+
+export default {
+  data() {
+    return {
+      contentTree,
+      componentAttributes: null,
+    };
+  },
+  head: {
+    title: 'Components overview',
+  },
+};
+</script>
+
+<template>
+  <div class="content limited m-t-7 m-b-7 app-styles">
+    <div class="structure md">
+      <h1>Pajamas components</h1>
+      <p class="mt-4">
+        Pajamas components are written in Vue and designed in Figma. Select a component to learn
+        more about how to use them for your design or development project.
+      </p>
+      <ul class="components-list mt-4">
+        <li v-for="component in contentTree.components" :key="component.id">
+          <a class="component-card" :href="`/components/${component.id}`">
+            <h3 class="component-name">{{ component.name }}</h3>
+            <p class="component-description mb-0 mt-2">{{ component.description }}</p>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
