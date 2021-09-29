606b9a67cee5aac6d7b95c3ffb4a765972025803
Mike Greiling <mike@pixelcog.com>
2021-09-29 17:44:01 -0500

    feat(Analytics): Add consent banner for cookies
    Places a gl-alert banner at the bottom of the page prompting the user
    to consent to being tracked with cookies before enabling Googla
    Analytics.

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import VueGtag from 'vue-gtag';
 
+const enableAnalytics = localStorage.getItem('GDPR:accepted') === 'true';
+
 Vue.use(VueGtag, {
   config: { id: process.env.GOOGLE_ANALYTICS_ID },
+  bootstrap: enableAnalytics,
+  appName: 'APP_NAME',
+  enabled: enableAnalytics,
+  pageTrackerScreenviewEnabled: true,
 });
