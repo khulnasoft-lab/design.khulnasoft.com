52dede28951b31ec5ffe512fffee3663f5ec08a9
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2022-02-08 09:58:43 +0100

    test: mock navbar JSON
    This updates the navbar's spec to use mock JSON instead of the actual
    nav.json. This will prevent us from having to udpate the snapshot
    whenever the nav is updated.

--- tests/components/navbar.spec.js
+++ tests/components/navbar.spec.js
@@ -4,6 +4,50 @@ import Navbar from '../../components/navbar.vue';
 import mutations from '../../store/mutations';
 import state from '../../store/state';
 
+const navMock = [
+  {
+    title: 'Top-level item 1',
+    path: 'top1',
+    items: [
+      {
+        title: 'Second-level item 1',
+        path: 'second1',
+      },
+      {
+        title: 'Second-level item 2',
+        path: 'second2',
+        items: [
+          {
+            title: 'Third-level item 1',
+            path: 'third1',
+          },
+          {
+            title: 'Third-level item 2',
+            path: 'third2',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Top-level item 2',
+    path: 'top2',
+    items: [
+      {
+        title: 'Second-level item without path',
+        items: [
+          {
+            title: 'Third-level item 3',
+            path: 'third2',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+jest.mock('../../nav.json', () => navMock);
+
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
