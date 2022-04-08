9d75995b307441910c112729ac9a307999bc3bd8
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2022-04-08 10:15:11 -0400

    chore: create and leverage note component
    Replaces the generic admonition component with a specific note
    component.

--- plugins/register_global_components.js
+++ plugins/register_global_components.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
 import ExampleDisplay from '../components/example_display.vue';
 import StoryViewer from '../components/story_viewer.vue';
-import Admonition from '../components/admonition.vue';
+import Note from '../components/note.vue';
 import Todo from '../components/todo.vue';
 
 Object.entries({
   ExampleDisplay,
   StoryViewer,
-  Admonition,
+  Note,
   Todo,
 }).forEach(([componentName, component]) => Vue.component(componentName, component));
