e8a9588ff18f0ec1d77a48a806260cd6deb540a2
Lukas Eipert <leipert@gitlab.com>
2022-04-13 09:33:31 +0200

    chore: Remove GlExampleDisplay and wrappers
    We currently load GlExampleDisplay and GlExampleExplorer globally, we
    can simply remove them as they are not needed any longer. This also
    removes some related webpack loaders.

--- plugins/register_global_components.js
+++ plugins/register_global_components.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
-import ExampleDisplay from '../components/example_display.vue';
 import StoryViewer from '../components/story_viewer.vue';
 import Note from '../components/note.vue';
 import Todo from '../components/todo.vue';
 
 Object.entries({
-  ExampleDisplay,
   StoryViewer,
   Note,
   Todo,
