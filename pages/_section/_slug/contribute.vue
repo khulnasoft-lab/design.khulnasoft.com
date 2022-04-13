d2b6dce4d381e29eadf5cb15179fed119ee8abbd
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2022-04-13 18:14:02 -0400

    perf: render as much as we can server-side
    This reorganizes the code and pages structure to enable proper static
    generation when building the production site. This should result in
    better performances and an easier to grasp app architecture.

---
+++ pages/_section/_slug/contribute.vue
@@ -0,0 +1,23 @@
+<script>
+import ContributeToComponent from '../../../components/contribute_to_component.vue';
+
+export default {
+  components: {
+    ContributeToComponent,
+  },
+  props: {
+    page: {
+      type: Object,
+      required: true,
+    },
+    componentLabel: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <contribute-to-component :component-label="componentLabel" class="p-t-3" />
+</template>
---
+++ pages/_section/_slug/index.vue
@@ -0,0 +1,24 @@
+<script>
+import RelatedPages from '../../../components/related_pages.vue';
+
+export default {
+  components: {
+    RelatedPages,
+  },
+  props: {
+    page: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="p-t-3">
+    <div class="component md typography">
+      <nuxt-content :document="page" />
+    </div>
+    <related-pages :related="page.related" class="m-t-6" />
+  </div>
+</template>
