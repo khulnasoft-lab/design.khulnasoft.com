3b45a46ecae29b299151407b2b667932b6c182ca
Lukas 'Eipi' Eipert <leipert@gitlab.com>
2022-05-30 09:17:02 +0000

    fix: Add GitLab Pages Auth site to CSP

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ const cspPolicies = [
   "style-src 'self' 'unsafe-inline'",
   "object-src 'none'",
   "img-src 'self' https: data:",
-  `frame-src https://www.figma.com ${new URL(GITLAB_UI_URL).origin}`,
+  `frame-src https://www.figma.com https://projects.gitlab.io ${new URL(GITLAB_UI_URL).origin}`,
   "connect-src 'self' https://sentry.gitlab.net https://www.google-analytics.com https://cdn.cookielaw.org https://geolocation.onetrust.com  https://gitlab-requests.my.onetrust.com",
 ];
 
