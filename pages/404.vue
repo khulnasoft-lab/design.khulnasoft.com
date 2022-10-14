ef4c832e9684a422a2f763e4789c1408bf89f4a5
Jeremy Elder <jelder@gitlab.com>
2022-10-14 17:57:30 +0000

    feat(ErrorPage): Format better error page

---
+++ pages/404.vue
@@ -0,0 +1,16 @@
+<script>
+import PageNotFound from '../components/page_not_found.vue';
+
+/**
+ * This component is a special 404 page just for nginx,
+ * in development we fall back to the good old
+ * layouts/error.vue
+ */
+export default {
+  components: { PageNotFound },
+  layout: 'no_footer',
+};
+</script>
+<template>
+  <page-not-found />
+</template>
