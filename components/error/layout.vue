392ca815c8173e971307bbfc75603ac1e7aeb25a
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2022-11-01 12:25:53 -0400

    refactor: consolidate error pages
    Refactors the error pages' code to be more reusable and less duplicated.

---
+++ components/error/layout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="typography gl--flex-center gl-flex-direction-column gl-h-100vh">
+    <slot name="illustration"></slot>
+    <h1 class="gl-font-size-h1 gl-mb-4">
+      <slot name="title"></slot>
+    </h1>
+    <p><slot name="text"></slot></p>
+    <pre
+      v-if="$scopedSlots.details"
+      class="gl-text-center gl-font-sm gl-text-gray-500"
+    ><slot name="details"></slot></pre>
+  </div>
+</template>
