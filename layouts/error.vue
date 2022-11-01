58fae9f08e512743639630b967dbf05d4258c265
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2022-11-01 12:25:53 -0400

    refactor: consolidate error pages
    Refactors the error pages' code to be more reusable and less duplicated.

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,8 +1,12 @@
 <script>
-import PageNotFound from '../components/page_not_found.vue';
+import NotFound from '../components/error/not_found.vue';
+import Default from '../components/error/default.vue';
 
 export default {
-  components: { PageNotFound },
+  components: {
+    NotFound,
+    Default,
+  },
   layout: 'no_footer',
   props: {
     error: {
@@ -18,35 +22,8 @@ export default {
 };
 </script>
 <template>
-  <page-not-found v-if="error.statusCode === 404" />
-  <div v-else class="typography gl--flex-center gl-flex-direction-column gl-h-100vh">
-    <svg
-      role="img"
-      aria-hidden="true"
-      width="72"
-      height="72"
-      viewBox="0 0 72 72"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle
-        cx="36"
-        cy="36"
-        r="35"
-        fill="#FCA326"
-        stroke="#171321"
-        stroke-width="2"
-        stroke-linecap="square"
-      />
-      <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M37 20v-1h-2v24h2V20Zm-1 33a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"
-        fill="#171321"
-      />
-    </svg>
-    <h1 class="gl-font-size-h1 gl-mb-4">An error occurred</h1>
-    <p><nuxt-link to="/">Go to homepage</nuxt-link> or try refreshing.</p>
-    <pre v-if="details" class="gl-text-center gl-font-sm gl-text-gray-500">{{ details }}</pre>
-  </div>
+  <not-found v-if="error.statusCode === 404" />
+  <default v-else>
+    <template v-if="details" #details>{{ details }}</template>
+  </default>
 </template>
