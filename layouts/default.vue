65c34e1253fbe031e950997f49667704532408e2
sdejonge <sdejonge@gitlab.com>
2022-11-04 11:43:31 +1100

    chore: Resolve layout shift on page navigation
    Move `<footer>` outside of `<main>`
    Remove `.content` CSS class, consolidate `.main` style to own selector
    Create `.container` CSS class
    Remove conditional classes in default layout `<main>` element
    Remove excessive spacing from `gl-pt-xx` classes in page templates

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,9 +8,6 @@ export default {
   },
   computed: {
     ...mapState(['sidebarOpen']),
-    contentWrapper() {
-      return this.$route.fullPath === '/' ? '' : 'content main';
-    },
   },
   methods: {
     ...mapMutations(['toggleSidebar', 'closeSidebar']),
@@ -39,30 +36,30 @@ export default {
     </button>
     <navbar />
 
-    <main :class="contentWrapper">
+    <main class="main">
       <nuxt />
-      <footer class="content footer limited gl-mb-7">
-        <div class="row gl-justify-content-center">
-          <edit-this-page-link
-            edit-url="https://gitlab.com/gitlab-org/gitlab-services/design.gitlab.com/-/blob/main/"
-          >
-            Edit this page
-          </edit-this-page-link>
-          <span class="footer-link-divider"></span>
-          <edit-this-page-link
-            edit-url="https://gitlab.com/-/ide/project/gitlab-org/gitlab-services/design.gitlab.com/edit/main/-"
-          >
-            Open Web IDE
-          </edit-this-page-link>
-          <span class="footer-link-divider"></span>
-          <nuxt-link to="/get-started/contribute">Please contribute</nuxt-link>
-          <span class="footer-link-divider"></span>
-          <a href="https://about.gitlab.com/privacy/">Privacy policy</a>
-          <span class="footer-link-divider"></span>
-          <button id="ot-sdk-btn" class="ot-sdk-show-settings">Cookie Settings</button>
-        </div>
-      </footer>
     </main>
+    <footer class="footer container gl-pb-7">
+      <div class="row gl-justify-content-center">
+        <edit-this-page-link
+          edit-url="https://gitlab.com/gitlab-org/gitlab-services/design.gitlab.com/-/blob/main/"
+        >
+          Edit this page
+        </edit-this-page-link>
+        <span class="footer-link-divider"></span>
+        <edit-this-page-link
+          edit-url="https://gitlab.com/-/ide/project/gitlab-org/gitlab-services/design.gitlab.com/edit/main/-"
+        >
+          Open Web IDE
+        </edit-this-page-link>
+        <span class="footer-link-divider"></span>
+        <nuxt-link to="/get-started/contribute">Please contribute</nuxt-link>
+        <span class="footer-link-divider"></span>
+        <a href="https://about.gitlab.com/privacy/">Privacy policy</a>
+        <span class="footer-link-divider"></span>
+        <button id="ot-sdk-btn" class="ot-sdk-show-settings">Cookie Settings</button>
+      </div>
+    </footer>
   </div>
 </template>
 
--- pages/_section/_slug/code.vue
+++ pages/_section/_slug/code.vue
@@ -10,7 +10,7 @@ export default {
 </script>
 
 <template>
-  <div class="app-styles gl-pt-0">
+  <div class="app-styles">
     <div v-for="story in page.stories" :key="story" class="container">
       <story-viewer :story-name="story" view-mode="docs" />
     </div>
