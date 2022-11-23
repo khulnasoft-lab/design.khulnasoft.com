0563d3b1c6916a962889647e694994b023b69a70
sdejonge <sdejonge@gitlab.com>
2022-11-23 17:51:37 +1100

    chore: Update mobile navigation styles
    Create `<navbar>` component for mobile header with logo & toggle button
    Add `<navbar>` to `<layout>`
    Create `<logo>` and `<toggle-button>` components
    Remove `sidebar__overlay` maerkup and styles
    Remove added `padding-top` to `<h1>` on small screens
    Add `sidebar--open` class to `<main>` to remove scroll when open
    Add `padding-top` with `$navbar-height` to offset on small screens

---
+++ components/logo.vue
@@ -0,0 +1,6 @@
+<template>
+  <nuxt-link to="/" class="logo">
+    <img src="/pajamas-logo.svg" alt="" width="24" height="24" />
+    <span>Pajamas Design System</span>
+  </nuxt-link>
+</template>
---
+++ components/toggle_button.vue
@@ -0,0 +1,37 @@
+<script>
+import { mapState, mapMutations } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['sidebarOpen']),
+  },
+  methods: {
+    ...mapMutations(['toggleSidebar']),
+  },
+};
+</script>
+
+<template>
+  <button
+    aria-label="Toggle menu"
+    type="button"
+    class="toggle-button"
+    :aria-expanded="sidebarOpen"
+    @click="toggleSidebar"
+  >
+    <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+      <path
+        v-if="sidebarOpen"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M4.28 3.22a.75.75 0 0 0-1.06 1.06L6.94 8l-3.72 3.72a.75.75 0 1 0 1.06 1.06L8 9.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L9.06 8l3.72-3.72a.75.75 0 0 0-1.06-1.06L8 6.94 4.28 3.22Z"
+      />
+      <path
+        v-else
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M0 3.75A.75.75 0 0 1 .75 3h14.5a.75.75 0 0 1 0 1.5H.75A.75.75 0 0 1 0 3.75ZM0 8a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H.75A.75.75 0 0 1 0 8Zm.75 3.5a.75.75 0 0 0 0 1.5h14.5a.75.75 0 0 0 0-1.5H.75Z"
+      />
+    </svg>
+  </button>
+</template>
