6a5be2f98a0c2822c89c14a6b46cb755d00872fd
Scott de Jonge <sdejonge@gitlab.com>
2022-12-20 03:24:32 +0000

    chore: Pajamas navigation refinements

--- components/logo.vue
+++ components/logo.vue
@@ -1,6 +1,9 @@
 <template>
   <nuxt-link to="/" class="logo">
-    <img src="/pajamas-logo.svg" alt="" width="24" height="24" />
-    <span>Pajamas Design System</span>
+    <img src="/pajamas-logo.svg" alt="" width="14" height="24" />
+    <span>
+      <strong>Pajamas</strong>
+      <small>GitLab Design System</small>
+    </span>
   </nuxt-link>
 </template>
