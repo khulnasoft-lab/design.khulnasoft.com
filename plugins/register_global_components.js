b2f3d43bb73cb6829e5f65064c5d2dd33c41253e
sdejonge <sdejonge@gitlab.com>
2023-01-04 16:25:36 +1100

    chore: Add <figure-img> component
    Update README
    Add <figure-img> component
    Update spacing on <figure> to margin

--- plugins/register_global_components.js
+++ plugins/register_global_components.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import FigureImg from '../components/figure_img.vue';
 import StoryViewer from '../components/story_viewer.vue';
 import Note from '../components/note.vue';
 import Todo from '../components/todo.vue';
@@ -6,6 +7,7 @@ import ColorPalette from '../components/color_palette/color_palette.vue';
 import MermaidDiagram from '../components/mermaid_diagram.vue';
 
 Object.entries({
+  FigureImg,
   StoryViewer,
   Note,
   Todo,
