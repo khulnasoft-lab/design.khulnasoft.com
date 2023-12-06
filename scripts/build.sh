e87405ee21f7cb94e12d15dda5431becb75512be
Lukas Eipert <leipert@gitlab.com>
2023-12-06 16:47:34 +0100

    Add caching and smarter compression (e.g. less compression on MRs)

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,10 +8,20 @@ set -euo pipefail; IFS=$'\n\t'
 
 # Setting the NODE_ENV to production if it isn't set
 export NODE_ENV=${NODE_ENV:-production}
+CI_JOB_NAME=${CI_JOB_NAME:-pages}
+CI_ENVIRONMENT_NAME=${CI_JOB_NAME:-fallback}
 
 if [[ "$NODE_ENV" == "test" ]]; then
     echo "We are not running \`nuxt generate\` when $NODE_ENV is set to 'test'"
 else
     echo "Generating production pages with nuxt"
     yarn run nuxt generate
+    if [[ "$CI_JOB_NAME" == "pages" ]]; then
+      echo "Running in a pages job. Compressing with GZIP"
+      find public -type f -regex '.*\.\(htm\|html\|txt\|text\|js\|json\|css\)$' -exec gzip -f -k {} \;
+    fi
+    if [[ "$CI_ENVIRONMENT_NAME" == "pages" ]]; then
+      echo "Running in a _production_ pages job. Compressing with BROTLI"
+      find public -type f -regex '.*\.\(htm\|html\|txt\|text\|js\|json\|css\)$' -exec brotli -f -k {} \;
+    fi
 fi
