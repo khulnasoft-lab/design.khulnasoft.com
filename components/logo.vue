a9bf11199984a7e47ac9dd29902bcca09e2eb7eb
Lukas Eipert <leipert@gitlab.com>
2023-12-12 17:51:18 +0100

    chore: Fix URL paths in review apps
    We need to fix a few paths in our review apps (and pages for now), as we
    are not running our review apps under the domain root path, but rather
    a sub path

--- components/logo.vue
+++ components/logo.vue
@@ -1,6 +1,6 @@
 <template>
   <nuxt-link to="/" class="logo">
-    <img src="/pajamas-logo.svg" alt="" width="14" height="24" />
+    <img src="../assets/pajamas-logo.svg" alt="" width="14" height="24" />
     <span>
       <strong>Pajamas</strong>
       <small>GitLab Design System</small>
