8207ef186737b7adbb031aab43fb830d41d9f0da
sdejonge <sdejonge@gitlab.com>
2024-08-02 10:44:53 +1000

    chore: update homepage banner
    Remove custom styles and banner image

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,16 @@
 <template>
   <div class="index typography">
     <nuxt-child />
-    <div class="banner">
-      <div class="banner__header gl-text-center">
-        <svg
-          aria-hidden="true"
-          width="66"
-          height="121"
-          fill="none"
-          class="animated fadeInDown"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="m26.052 120.631-14.617-7.95a21.948 21.948 0 0 1-8.344-8.045A21.994 21.994 0 0 1 0 93.457V59.678h30.165v58.496a2.753 2.753 0 0 1-1.336 2.438 2.731 2.731 0 0 1-2.777.019Z"
-            fill="#DEC9F0"
-          />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M45.042 78.614 6.527 88.225C0 89.887 0 92.633 0 92.633V37.16a27.488 27.488 0 0 1 5.81-16.9 27.42 27.42 0 0 1 14.963-9.74L62.4.085a2.738 2.738 0 0 1 3.164 1.513c.177.386.263.808.25 1.233v49.186a27.488 27.488 0 0 1-5.822 16.874 27.42 27.42 0 0 1-14.95 9.724Z"
-            fill="url(#a)"
-          />
-          <defs>
-            <linearGradient
-              id="a"
-              x1="0"
-              y1="46.331"
-              x2="65.815"
-              y2="46.331"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stop-color="#7144C6" />
-              <stop offset=".59" stop-color="#8045C9" />
-              <stop offset="1" stop-color="#8E46CC" />
-            </linearGradient>
-          </defs>
-        </svg>
-        <h1 id="skipTarget" class="banner__header--title f-display">Pajamas</h1>
-        <p class="banner__header--description">
-          Resources, components, and design guidelines behind GitLab
-        </p>
+    <div class="gl-grid gl-place-items-center gl-text-center gl-min-h-48">
+      <div>
+        <img
+          alt=""
+          src="../assets/images/pajamas_logo.svg"
+          class="gl-h-auto gl-w-10 gl-inline-block gl-mb-3"
+        />
+        <h1 id="skipTarget" class="gl-heading-display gl-mb-3! brand-text-gradient">Pajamas</h1>
+        <p class="gl-font-lg">Resources, components, and design guidelines behind GitLab</p>
       </div>
-      <img alt="" src="../assets/images/cover-gradient.png" class="banner__image animated" />
     </div>
     <div class="gl-pt-11 gl-pb-11">
       <div class="container">
