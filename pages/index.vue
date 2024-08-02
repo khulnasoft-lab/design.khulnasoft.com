408c68c7810e71b3a08b21fdf3e87efcdd96947a
sdejonge <sdejonge@gitlab.com>
2024-08-02 12:35:21 +1000

    refactor: homepage and component overview remove custom styles
    Improve HTML markup for accessibility

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="index typography">
     <nuxt-child />
-    <div class="gl-grid gl-place-items-center gl-text-center gl-min-h-48">
-      <div>
-        <img
-          alt=""
-          src="../assets/images/pajamas_logo.svg"
-          class="gl-h-auto gl-w-10 gl-inline-block gl-mb-3"
-        />
-        <h1 id="skipTarget" class="gl-heading-display gl-mb-3! brand-text-gradient">Pajamas</h1>
-        <p class="gl-font-lg">Resources, components, and design guidelines behind GitLab</p>
-      </div>
-    </div>
-    <div class="gl-pt-11 gl-pb-11">
-      <div class="container">
-        <div class="two-column two-column--alt">
-          <div class="two-column-start">
+    <div class="container">
+      <section
+        aria-labelledby="skipTarget"
+        class="gl-grid gl-place-items-center gl-text-center gl-min-h-48"
+      >
+        <div>
+          <img
+            alt=""
+            src="../assets/images/pajamas_logo.svg"
+            class="gl-h-auto gl-w-10 gl-inline-block gl-mb-3"
+          />
+          <h1 id="skipTarget" class="gl-heading-display gl-mb-3! brand-text-gradient">
+            Pajamas design system
+          </h1>
+          <p class="gl-text-lg">Resources, components, and design guidelines behind GitLab</p>
+        </div>
+      </section>
+
+      <section aria-labelledby="everyone-can-contribute" class="gl-my-10">
+        <div class="gl-grid lg:gl-grid-cols-2 gl-gap-5 gl-place-items-center">
+          <div class="lg:gl-order-last">
             <img
               alt=""
               class="two-column-image"
@@ -23,9 +29,9 @@
               srcset="../assets/images/contribute@2x.png 2x"
             />
           </div>
-          <div class="two-column-end">
-            <h2>Everyone can contribute</h2>
-            <p>
+          <div>
+            <h2 id="everyone-can-contribute" class="gl-heading-2">Everyone can contribute</h2>
+            <p class="gl-text-base gl-leading-20 gl-mb-3">
               GitLab is an
               <a
                 target="_blank"
@@ -35,48 +41,46 @@
               product and has over 4,000 community contributors. When everyone can contribute,
               consumers become contributors and we greatly increase the rate of human progress.
             </p>
-            <p>
+            <p class="gl-text-base gl-leading-20">
               To ensure that everyone can contribute to GitLab with confidence we provide everyone
               with the right resources and know-how. The Pajamas design system is the single source
               of truth for everything anyone needs to know about contributing to GitLab.
             </p>
           </div>
         </div>
-      </div>
-    </div>
-    <div
-      class="gl-bg-subtle gl-border-default gl-border-solid gl-border-0 gl-border-t-1 gl-border-b-1 gl-pt-9 gl-pb-9"
-    >
-      <div class="container">
-        <div class="two-column">
-          <div class="two-column-start">
+      </section>
+
+      <section aria-labelledby="everything-you-need" class="gl-my-10">
+        <div class="gl-grid lg:gl-grid-cols-2 gl-gap-5 gl-place-items-center">
+          <div>
             <img alt="" class="two-column-image" src="../assets/images/content.svg" />
           </div>
-          <div class="two-column-end">
-            <h2>Everything you need</h2>
-            <p>
+          <div>
+            <h2 id="everything-you-need" class="gl-heading-2">Everything you need</h2>
+            <p class="gl-text-base gl-leading-20 gl-mb-3">
               Pajamas is here to make it as comfortable as possible for anyone to contribute towards
               GitLab. You will find information such as our principles, components, usage
               guidelines, branding, research methodologies, and more!
             </p>
-            <p>Start by looking into the component documentation, including live examples.</p>
+            <p class="gl-text-base gl-leading-20">
+              Start by looking into the component documentation, including live examples.
+            </p>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="gl-pt-11 gl-pb-11">
-      <div class="container">
-        <div class="two-column two-column--alt">
-          <div class="two-column-start">
+      </section>
+
+      <section aria-labelledby="contributing" class="gl-my-10">
+        <div class="gl-grid lg:gl-grid-cols-2 gl-gap-5 gl-place-items-center">
+          <div class="lg:gl-order-last">
             <img
               alt=""
               class="two-column-image two-column-image--small"
               src="../assets/images/pajamas_stars.svg"
             />
           </div>
-          <div class="two-column-end">
-            <h2>Contributing to the design system</h2>
-            <p>
+          <div>
+            <h2 id="contributing" class="gl-heading-2">Contributing to the design system</h2>
+            <p class="gl-text-base gl-leading-20">
               We encourage any feedback and ideas towards the design system itself as well! If you
               are interested in contributing, check out our
               <a href="/get-started/contributing">guidelines</a> to get started. Have any questions?
@@ -89,61 +93,60 @@
             </p>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="gl-pt-9 gl-pb-9">
-      <div class="container">
-        <div class="row gl-mt-9">
-          <div class="col col-33">
+      </section>
+
+      <nav class="gl-my-10">
+        <ul class="gl-grid gl-grid-cols-3 gl-gap-3 gl-list-none gl-m-0 gl-p-0">
+          <li>
             <a
               href="https://gitlab.com/gitlab-org/gitlab-services/design.gitlab.com/issues"
               target="_blank"
-              class="gl-text-center"
+              class="gl-block gl-rounded-base gl-p-3 gl-text-center"
             >
               <img
-                width="48"
-                height="48"
+                width="32"
+                height="32"
                 src="../assets/images/resources/r-solid.png"
-                class="gl-mx-auto gl-display-block"
+                class="gl-mx-auto gl-block"
                 alt=""
               />
-              <span class="gl-display-block gl-p-3"> View the issue tracker </span>
+              <span class="gl-block gl-mt-3">View the issue tracker</span>
             </a>
-          </div>
-          <div class="col col-33">
+          </li>
+          <li>
             <a
               href="https://gitlab.com/gitlab-org/gitlab-services/design.gitlab.com"
               target="_blank"
-              class="gl-text-center"
+              class="gl-block gl-rounded-base gl-p-3 gl-text-center"
             >
               <img
-                width="48"
-                height="48"
+                width="32"
+                height="32"
                 src="../assets/images/resources/r-outline.png"
-                class="gl-mx-auto gl-display-block"
+                class="gl-mx-auto gl-block"
                 alt=""
               />
-              <span class="gl-display-block gl-p-3"> Explore the repository </span>
+              <span class="gl-block gl-mt-3">Explore the repository</span>
             </a>
-          </div>
-          <div class="col col-33">
+          </li>
+          <li>
             <a
               href="https://www.figma.com/community/file/781156790581391771/"
               target="_blank"
-              class="gl-text-center"
+              class="gl-block gl-rounded-base gl-p-3 gl-text-center"
             >
               <img
-                width="48"
-                height="48"
+                width="32"
+                height="32"
                 src="../assets/images/resources/r-figma.png"
-                class="gl-mx-auto gl-display-block"
+                class="gl-mx-auto gl-block"
                 alt=""
               />
-              <span class="gl-display-block gl-p-3"> Duplicate our Figma UI Kit </span>
+              <span class="gl-block gl-mt-3">Duplicate our Figma UI Kit</span>
             </a>
-          </div>
-        </div>
-      </div>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
