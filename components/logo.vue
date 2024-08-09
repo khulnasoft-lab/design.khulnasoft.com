78979c5e7b410faecb28bede99cd684b00dd8884
Scott de Jonge <sdejonge@gitlab.com>
2024-08-09 01:48:43 +0000

    Draft: chore: update homepage banner

--- components/logo.vue
+++ components/logo.vue
@@ -1,9 +1,9 @@
 <template>
-  <nuxt-link to="/" class="logo">
+  <nuxt-link
+    to="/"
+    class="logo gl-flex gl-items-center gl-gap-3 gl-p-3 gl-rounded-base gl-font-base gl-font-bold gl-text-current gl-text-decoration-none"
+  >
     <img src="../assets/pajamas-logo.svg" alt="" width="14" height="24" />
-    <span>
-      <strong>Pajamas</strong>
-      <small>GitLab Design System</small>
-    </span>
+    <span>Pajamas Design System</span>
   </nuxt-link>
 </template>
