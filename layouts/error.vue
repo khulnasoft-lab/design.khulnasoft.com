5c5db1f922510fe5a2995d4e9b2b4069662a9dcd
Lukas Eipert <leipert@gitlab.com>
2024-09-17 15:17:49 +0200

    feat: Add landing page for missing review apps
    The idea is rather simple, our review apps are deployed with a prefix
    `/review-mr-1234` (for MR 1234). We simply add a new error page for all
    `/review-mr-*` URLs and show instructions on how to re-deploy the review
    app.

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,13 @@
 <script>
 import NotFound from '../components/error/not_found.vue';
 import Default from '../components/error/default.vue';
+import ReviewAppNotFound from '../components/error/review_app_not_found.vue';
 
 export default {
   components: {
     NotFound,
     Default,
+    ReviewAppNotFound,
   },
   layout: 'no_footer',
   props: {
@@ -16,10 +18,31 @@ export default {
     },
   },
   computed: {
+    reviewAppMRIid() {
+      if (process.server) {
+        return null;
+      }
+
+      const match = window.location.toString().match(/review-mr-(\d+)/);
+      if (match) {
+        return match[1];
+      }
+      return null;
+    },
+    reviewAppMRUrl() {
+      if (!this.reviewAppMRIid) {
+        return null;
+      }
+      return `https://gitlab.com/gitlab-org/gitlab-services/design.gitlab.com/-/merge_requests/${this.reviewAppMRIid}/pipelines`;
+    },
     details() {
       return [this.error?.message, this.error?.stack].filter(Boolean).join('\n\n');
     },
     showDefaultError() {
+      // Deal with a missing review app
+      if (this.reviewAppMRIid) {
+        return false;
+      }
       // 404s in production, we want to render our 404 page
       if (this.error?.statusCode === 404 && process.env.NODE_ENV === 'production') {
         return false;
@@ -33,5 +56,10 @@ export default {
   <default v-if="showDefaultError">
     <template v-if="details" #details>{{ details }}</template>
   </default>
+  <review-app-not-found
+    v-else-if="reviewAppMRIid"
+    :merge-request-iid="reviewAppMRIid"
+    :merge-request-url="reviewAppMRUrl"
+  />
   <not-found v-else />
 </template>
