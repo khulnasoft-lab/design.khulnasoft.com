49b9a032b32837ad33257752b1dcd573a92d6bf7
sdejonge <sdejonge@gitlab.com>
2024-09-19 15:15:03 +1000

    feat(CollectionPages): make description optional
    Update link markup to both be <span> elements with gl-block class

--- components/collection.vue
+++ components/collection.vue
@@ -3,13 +3,13 @@ export default {
   props: {
     heading: {
       type: String,
-      default: '',
       required: true,
+      default: '',
     },
-    content: {
+    description: {
       type: String,
+      required: false,
       default: '',
-      required: true,
     },
     items: {
       type: Array,
@@ -26,8 +26,8 @@ export default {
       <h1 id="skipTarget" class="gl-heading-display gl-mt-0! gl-mb-4!" tabindex="-1">
         {{ heading }}
       </h1>
-      <p class="gl-text-lg gl-leading-24">
-        {{ content }}
+      <p v-if="description" class="gl-text-lg gl-leading-24">
+        {{ description }}
       </p>
     </div>
     <ul class="gl-grid lg:gl-grid-cols-3 gl-gap-5 gl-list-none gl-m-0 gl-p-0!">
@@ -36,10 +36,10 @@ export default {
           class="!gl-no-underline !gl-text-default gl-rounded-base gl-bg-default gl-p-5 gl-w-full gl-border-1 gl-border-solid gl-border-default hover:gl-border-strong"
           :to="item.path"
         >
-          <span class="gl-font-weight-bold">{{ item.name }}</span>
-          <p class="gl-text-base gl-leading-20 gl-mb-0 gl-mt-2">
+          <span class="gl-block gl-font-weight-bold">{{ item.name }}</span>
+          <span v-if="item.description" class="gl-block gl-text-base gl-leading-20 gl-mb-0 gl-mt-2">
             {{ item.description }}
-          </p>
+          </span>
         </nuxt-link>
       </li>
     </ul>
--- pages/product-foundations/index.vue
+++ pages/product-foundations/index.vue
@@ -22,7 +22,7 @@ export default {
 <template>
   <collection
     heading="Foundations"
-    content="Foundational items communicate the opinionated way in which basic visual design attributes, elements, and concepts come together to create a distinct GitLab personality that's expressed in the UI. For example, color, typography, and iconography."
+    description="Foundational items communicate the opinionated way in which basic visual design attributes, elements, and concepts come together to create a distinct GitLab personality that's expressed in the UI. For example, color, typography, and iconography."
     :items="foundationsList"
   />
 </template>
