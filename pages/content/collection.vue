b8ba4649d141d840b3b94f1d369c92d487941c5b
Jeremy Elder <jelder@gitlab.com>
2024-09-18 21:59:15 -0500

    feat(CollectionPages): Apply patch and include data viz
    Patch applied to simplify reuse and to include the data visualization
    section as a collection.

--- pages/content/collection.vue
+++ pages/content/collection.vue
@@ -1,5 +1,10 @@
 <script>
+import Collection from '../../components/collection.vue';
+
 export default {
+  components: {
+    Collection,
+  },
   async asyncData({ $content }) {
     const contentList = await $content('content')
       .only(['name', 'description', 'path'])
@@ -15,28 +20,9 @@ export default {
 </script>
 
 <template>
-  <div class="container gl-py-7">
-    <div class="gl-mb-5">
-      <h1 id="skipTarget" class="gl-heading-display gl-mt-0! gl-mb-4!" tabindex="-1">
-        Product content
-      </h1>
-      <p class="gl-text-lg gl-leading-24">
-        Guidelines for crafting clear, consistent, and universal content. It addresses key elements
-        of language and localization in user interface design and content creation for GitLab.
-      </p>
-    </div>
-    <ul class="gl-grid lg:gl-grid-cols-3 gl-gap-5 gl-list-none gl-m-0 gl-p-0!">
-      <li v-for="content in contentList" :key="content.path" class="gl-flex">
-        <nuxt-link
-          class="!gl-no-underline !gl-text-default gl-rounded-base gl-bg-default gl-p-5 gl-w-full gl-border-1 gl-border-solid gl-border-default hover:gl-border-strong"
-          :to="content.path"
-        >
-          <span class="gl-font-weight-bold">{{ content.name }}</span>
-          <p class="gl-text-base gl-leading-20 gl-mb-0 gl-mt-2">
-            {{ content.description }}
-          </p>
-        </nuxt-link>
-      </li>
-    </ul>
-  </div>
+  <collection
+    heading="Product content"
+    content="Guidelines for crafting clear, consistent, and universal content. It addresses key elements of language and localization in user interface design and content creation for GitLab."
+    :items="contentList"
+  />
 </template>
--- pages/usability/collection.vue
+++ pages/usability/collection.vue
@@ -1,5 +1,10 @@
 <script>
+import Collection from '../../components/collection.vue';
+
 export default {
+  components: {
+    Collection,
+  },
   async asyncData({ $content }) {
     const usabilityList = await $content('usability')
       .only(['name', 'description', 'path'])
@@ -15,27 +20,9 @@ export default {
 </script>
 
 <template>
-  <div class="container gl-py-7">
-    <div class="gl-mb-5">
-      <h1 id="skipTarget" class="gl-heading-display gl-mt-0! gl-mb-4!" tabindex="-1">
-        Usability and interaction
-      </h1>
-      <p class="gl-text-lg gl-leading-24">
-        Guidelines that apply to the user experience of GitLab.
-      </p>
-    </div>
-    <ul class="gl-grid lg:gl-grid-cols-3 gl-gap-5 gl-list-none gl-m-0 gl-p-0!">
-      <li v-for="usability in usabilityList" :key="usability.path" class="gl-flex">
-        <nuxt-link
-          class="!gl-no-underline !gl-text-default gl-rounded-base gl-bg-default gl-p-5 gl-w-full gl-border-1 gl-border-solid gl-border-default hover:gl-border-strong"
-          :to="usability.path"
-        >
-          <span class="gl-font-weight-bold">{{ usability.name }}</span>
-          <p class="gl-text-base gl-leading-20 gl-mb-0 gl-mt-2">
-            {{ usability.description }}
-          </p>
-        </nuxt-link>
-      </li>
-    </ul>
-  </div>
+  <collection
+    heading="Usability and interaction"
+    content="Guidelines that apply to the user experience of GitLab."
+    :items="usabilityList"
+  />
 </template>
