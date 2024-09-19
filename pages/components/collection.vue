0fb42160deae001882e61d0fc8c80b77988479ed
Jeremy Elder <jelder@gitlab.com>
2024-09-18 21:59:15 -0500

    feat(CollectionPages): Apply patch and include data viz
    Patch applied to simplify reuse and to include the data visualization
    section as a collection.

--- pages/components/collection.vue
+++ pages/components/collection.vue
@@ -1,5 +1,10 @@
 <script>
+import Collection from '../../components/collection.vue';
+
 export default {
+  components: {
+    Collection,
+  },
   async asyncData({ $content }) {
     const componentList = await $content('components')
       .only(['name', 'description', 'path'])
@@ -15,29 +20,9 @@ export default {
 </script>
 
 <template>
-  <div class="container gl-py-7">
-    <div class="gl-mb-5">
-      <h1 id="skipTarget" class="gl-heading-display gl-mt-0! gl-mb-4!" tabindex="-1">
-        Pajamas components
-      </h1>
-      <p class="gl-text-lg gl-leading-24">
-        A component is a UI element that serves a singular purpose or function. Two or more simple
-        components can combine to form a composite component that still has a single function. For
-        example, an avatar, button, or combobox.
-      </p>
-    </div>
-    <ul class="gl-grid lg:gl-grid-cols-3 gl-gap-5 gl-list-none gl-m-0 gl-p-0!">
-      <li v-for="component in componentList" :key="component.path" class="gl-flex">
-        <nuxt-link
-          class="!gl-no-underline !gl-text-default gl-rounded-base gl-bg-default gl-p-5 gl-w-full gl-border-1 gl-border-solid gl-border-default hover:gl-border-strong"
-          :to="component.path"
-        >
-          <span class="gl-font-weight-bold">{{ component.name }}</span>
-          <p class="gl-text-base gl-leading-20 gl-mb-0 gl-mt-2">
-            {{ component.description }}
-          </p>
-        </nuxt-link>
-      </li>
-    </ul>
-  </div>
+  <collection
+    heading="Pajamas components"
+    content="A component is a UI element that serves a singular purpose or function. Two or more simple components can combine to form a composite component that still has a single function. For example, an avatar, button, or combobox."
+    :items="componentList"
+  />
 </template>
