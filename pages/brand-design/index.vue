2b23a90b62cc159427ee0b9245751649000a9098
danmh <dmizzi-harris@gitlab.com>
2024-09-23 15:45:22 +0100

    feat(CollectionPages): Add Brand design collection page

---
+++ pages/brand-design/index.vue
@@ -0,0 +1,28 @@
+<script>
+import Collection from '../../components/collection.vue';
+
+export default {
+  components: {
+    Collection,
+  },
+  async asyncData({ $content }) {
+    const brandDesignList = await $content('brand-design')
+      .only(['name', 'description', 'path'])
+      .sortBy('name')
+      .fetch();
+
+    return { brandDesignList };
+  },
+  head: {
+    title: 'Brand design collection',
+  },
+};
+</script>
+
+<template>
+  <collection
+    heading="Brand design"
+    description="Create awareness, trust, and affinity for the GitLab brand."
+    :items="brandDesignList"
+  />
+</template>
