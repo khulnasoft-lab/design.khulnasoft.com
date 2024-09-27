8112489efb03e993bfbd186a682d92dc0ed730f4
danmh <dmizzi-harris@gitlab.com>
2024-09-27 12:17:00 +0100

    refactor: update collection to use tailwind classes

--- components/collection.vue
+++ components/collection.vue
@@ -23,21 +23,21 @@ export default {
 <template>
   <div class="container gl-py-7">
     <div class="gl-mb-5">
-      <h1 id="skipTarget" class="gl-heading-display gl-mt-0! gl-mb-4!" tabindex="-1">
+      <h1 id="skipTarget" class="gl-mt-0! gl-mb-4! gl-heading-display" tabindex="-1">
         {{ heading }}
       </h1>
       <p v-if="description" class="gl-text-lg gl-leading-24">
         {{ description }}
       </p>
     </div>
-    <ul class="gl-grid lg:gl-grid-cols-3 gl-gap-5 gl-list-none gl-m-0 gl-p-0!">
+    <ul class="gl-m-0 gl-grid gl-list-none gl-gap-5 !gl-p-0 lg:gl-grid-cols-3">
       <li v-for="item in items" :key="item.path" class="gl-flex">
         <nuxt-link
-          class="!gl-no-underline !gl-text-default gl-rounded-base gl-bg-default gl-p-5 gl-w-full gl-border-1 gl-border-solid gl-border-default hover:gl-border-strong"
+          class="gl-w-full gl-rounded-base gl-border-1 gl-border-solid gl-border-default gl-bg-default gl-p-5 !gl-text-default !gl-no-underline hover:gl-border-strong"
           :to="item.path"
         >
-          <span class="gl-block gl-font-weight-bold">{{ item.name }}</span>
-          <span v-if="item.description" class="gl-block gl-text-base gl-leading-20 gl-mb-0 gl-mt-2">
+          <span class="gl-block gl-font-bold">{{ item.name }}</span>
+          <span v-if="item.description" class="gl-mb-0 gl-mt-2 gl-block gl-text-base gl-leading-20">
             {{ item.description }}
           </span>
         </nuxt-link>
