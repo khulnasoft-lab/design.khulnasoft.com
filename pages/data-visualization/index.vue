b5b63d97cb2f007c97faee142d1b8925fe347234
danmh <dmizzi-harris@gitlab.com>
2024-10-10 12:02:53 +0100

    refactor: Add data-viz collection page
    Init collection page, including logic for
    'Overview' page to appear first in the list
    
    chore: Apply suggestion(s)

---
+++ pages/data-visualization/index.vue
@@ -0,0 +1,35 @@
+<script>
+import Collection from '../../components/collection.vue';
+
+export default {
+  components: {
+    Collection,
+  },
+  async asyncData({ $content }) {
+    const dataVizList = await $content('data-visualization')
+      .only(['name', 'summary', 'description', 'path'])
+      .sortBy('name')
+      .fetch();
+
+    // Move 'Overview' to the beginning if it exists
+    const overviewIndex = dataVizList.findIndex((item) => item.name === 'Overview');
+    if (overviewIndex !== -1) {
+      const [overview] = dataVizList.splice(overviewIndex, 1);
+      dataVizList.unshift(overview);
+    }
+
+    return { dataVizList };
+  },
+  head: {
+    title: 'Data visualization collection',
+  },
+};
+</script>
+
+<template>
+  <collection
+    heading="Data visualization"
+    description="A data visualization pulls insights from a data set into a narrative, allowing a user to explore data themselves and discover their own insights."
+    :items="dataVizList"
+  />
+</template>
